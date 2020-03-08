e3f15cce8f5e621b93fdb2c26da5916dca441213
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-03-08 20:14:50 +0800

    Update 26. Remove Duplicates from Sorted Array.py

--- array/26. Remove Duplicates from Sorted Array.py
+++ array/26. Remove Duplicates from Sorted Array.py
@@ -1,8 +1,7 @@
-”“”
+"""
 Runtime: 84 ms, faster than 76.69% of Python3 online submissions for Remove Duplicates from Sorted Array.
 Memory Usage: 14.5 MB, less than 98.36% of Python3 online submissions for Remove Duplicates from Sorted Array.
-“”“
-
+"""
 
 def removeDuplicates(nums):    
     nums[:] = sorted(set(nums))
