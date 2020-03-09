6d41e62313f2949233c33876d84a6f9ec1df3652
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-03-10 01:16:46 +0800

    Update 66. Plus One.py

--- array/66. Plus One.py
+++ array/66. Plus One.py
@@ -1,22 +1,4 @@
 """
-Recursion
-
-Runtime: 32 ms, faster than 48.52% of Python3 online submissions for Plus One.
-Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Plus One.
-"""
-
-def plusOne(self, List):
-    if List[-1] != 9:
-        List[-1] = List[-1] + 1
-    elif set(List) == {9}: 
-        List = [1] + [0] * len(List)
-    else: 
-        List[-1], List[:-1] = 0, self.plusOne(List[:-1])
-    return List
-
-
-
-"""
 Runtime: 28 ms, faster than 79.18% of Python3 online submissions for Plus One.
 Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Plus One.
 """
@@ -30,4 +12,22 @@ def plusOne(List):
         #find the index of the last non-nine digit
         i = len(List) - 1 - next((i for i, x in enumerate(reversed(List)) if x != 9), None)
         List[:i], List[i], List[i+1:] = List[:i], List[i] + 1, [0] * (len(List) - 1 - i)
+    return List
+
+
+
+"""
+Recursion
+
+Runtime: 32 ms, faster than 48.52% of Python3 online submissions for Plus One.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Plus One.
+"""
+
+def plusOne(self, List):
+    if List[-1] != 9:
+        List[-1] = List[-1] + 1
+    elif set(List) == {9}: 
+        List = [1] + [0] * len(List)
+    else: 
+        List[-1], List[:-1] = 0, self.plusOne(List[:-1])
     return List
