9d90a8570c41a3911915e4cddfcd722946ba2099
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-03-10 21:26:17 +0800

    Create 1351. Count Negative Numbers in a Sorted Matrix.py

---
+++ array/1351. Count Negative Numbers in a Sorted Matrix.py
@@ -0,0 +1,7 @@
+def countNegatives(grid):
+        count = 0
+        for i in range(len(grid)-1, -1,-1):
+            for j in range(len(grid[0])-1,-1, -1):
+                if grid[i][j]<0:
+                    count +=1
+        return(count)
