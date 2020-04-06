4efba09ea9c446b371bd904833e2ecc288048460
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-04-07 00:07:51 +0800

    Update 0268. Missing Number.py

--- array/0268. Missing Number.py
+++ array/0268. Missing Number.py
@@ -1,4 +1,10 @@
-## Credit to @StefanPochmann
+"""
+Credit to @StefanPochmann
+
+Runtime: 136 ms, faster than 82.59% of Python3 online submissions for Missing Number.
+Memory Usage: 15.2 MB, less than 6.45% of Python3 online submissions for Missing Number.
+
+"""
 
 def missingNumber(self, nums):
     n = len(nums)
