60497b312edb251f0fcff229be093c5005654f28
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-04-15 14:08:27 +0800

    Update 0448. Find All Numbers Disappeared.py

--- array/0448. Find All Numbers Disappeared.py
+++ array/0448. Find All Numbers Disappeared.py
@@ -2,9 +2,11 @@
 Credit to @leetcodeftw
 
 Explanation: 
-- All the numbers are positive. 
-- Once we visit a number, we make it negative.
-- Return the indexes of the positive numbers we have not visitied.
+To solve the question without extra space, we cannot create a new list to store values.
+1. since all numbers are positive, if a number exists, mark the index by flipping the number to negative
+2. if a number repeats, no change
+3. return the index of positive numbers we have not visited
+
 
 Demo: http://www.pythontutor.com/visualize.html#code=def%20findDisappearedNumbers%28nums%29%3A%0A%20%20%20%20for%20num%20in%20nums%3A%0A%20%20%20%20%20%20%20%20index%20%3D%20abs%28num%29%20-%201%0A%20%20%20%20%20%20%20%20nums%5Bindex%5D%20%3D%20-abs%28nums%5Bindex%5D%29%0A%20%20%20%20%20%20%20%20%20%20%20%20%0A%20%20%20%20return%20%5Bi%20%2B%201%20for%20i,%20num%20in%20enumerate%28nums%29%20if%20num%20%3E%200%5D%0A%20%20%20%20%0A%20%20%20%20%0Anums%20%3D%20%5B4,3,2,7,8,2,3,1%5D%0Amissing%20%3D%20%5B%5D%0A%0AfindDisappearedNumbers%28nums%29&cumulative=false&curInstr=41&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=3&rawInputLstJSON=%5B%5D&textReferences=false
 
