0dcd9c4e6556771da2bbfafd4d93600559539606
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-08-25 20:25:19 +0800

    Update 0024. Swap Nodes in Pairs.py

--- linked_list/0024. Swap Nodes in Pairs.py
+++ linked_list/0024. Swap Nodes in Pairs.py
@@ -12,9 +12,7 @@ def swapPairs(self, head: ListNode) -> ListNode:
     dummy.next = head
     cur = dummy #dummy will not change and can be returned
 
-    if head is None: # and head.next is None: head does not have attribute None
-        return 
-    elif head and head.next is None:
+    if head is None or head.next is None:
         return head 
     else:
         while cur.next and cur.next.next:
