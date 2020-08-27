7c77559110273053ae29a86bacf7c372d7bde9aa
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-08-27 18:40:40 +0800

    Create 0092. Reverse Linked List II.py

---
+++ linked_list/0092. Reverse Linked List II.py
@@ -0,0 +1,21 @@
+"""
+
+Runtime: 24 ms, faster than 97.05% of Python3 online submissions for Reverse Linked List II.
+Memory Usage: 13.8 MB, less than 91.58% of Python3 online submissions for Reverse Linked List II.
+
+https://leetcode.com/problems/reverse-linked-list-ii/discuss/30709/Talk-is-cheap-show-me-the-code-(and-DRAWING)
+
+"""
+
+def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+    dummy = ListNode(-1)
+    dummy.next = head
+    pre, cur = dummy, head
+    
+    for _ in range(m - 1):
+        cur, pre = cur.next, pre.next
+    
+    for _ in range(n - m):
+        cur.next.next, pre.next, cur.next = pre.next, cur.next, cur.next.next
+
+    return dummy.next
