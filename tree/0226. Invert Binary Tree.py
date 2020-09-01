e55d55740f2f6f1b3a0e67e5895141d8aa4e1018
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-09-01 10:38:43 +0800

    Update 0226. Invert Binary Tree.py

--- tree/0226. Invert Binary Tree.py
+++ tree/0226. Invert Binary Tree.py
@@ -3,6 +3,8 @@
 Runtime: 28 ms, faster than 84.98% of Python3 online submissions for Invert Binary Tree.
 Memory Usage: 13.6 MB, less than 95.23% of Python3 online submissions for Invert Binary Tree.
 
+https://leetcode.com/problems/invert-binary-tree/discuss/62705/Python-solutions-(recursively-dfs-bfs).
+
 """
 
 def invertTree(self, root: TreeNode) -> TreeNode:
