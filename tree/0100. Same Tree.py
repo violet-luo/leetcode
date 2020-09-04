0d4cc9dbe5931c18d4f8245f6a361d1873491e9c
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-09-04 20:13:48 +0800

    Update 0100. Same Tree.py

--- tree/0100. Same Tree.py
+++ tree/0100. Same Tree.py
@@ -1,5 +1,39 @@
+# 1. recursion
+
 def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
     if p and q:
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
     else:
         return p == q
+
+# 2. DFS iterative
+def isSameTree2(self, p, q):
+    stack = [(p, q)]
+    while stack:
+        node1, node2 = stack.pop() #remove the top most element
+        if not node1 and not node2:
+            continue
+        elif None in [node1, node2]:
+            return False
+        else:
+            if node1.val != node2.val:
+                return False
+            stack.append((node1.right, node2.right))
+            stack.append((node1.left, node2.left))
+    return True
+
+# 3. BFS
+def isSameTree(self, p, q):
+    queue = [(p, q)]
+    while queue:
+        node1, node2 = queue.pop(0)
+        if not node1 and not node2:
+            continue
+        elif None in [node1, node2]:
+            return False
+        else:
+            if node1.val != node2.val:
+                return False
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+    return True
