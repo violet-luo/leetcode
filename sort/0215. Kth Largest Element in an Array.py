d64acf4ffe46d795e62bac23a791a04bb6b5064d
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-09-09 13:01:01 +0800

    Update 0215. Kth Largest Element in an Array.py

--- sort/0215. Kth Largest Element in an Array.py
+++ sort/0215. Kth Largest Element in an Array.py
@@ -7,29 +7,29 @@ https://www.jiuzhang.com/solution/kth-largest-element/#tag-lang-python
 
 """
 
-def findKthLargest(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        # convert the kth largest to the kth smallest
-        k = n - k
-        return self.partition(nums, 0, n - 1, k)
-    
-    def partition(self, nums, start, end, k):
-        left, right = start, end
-        pivot = nums[left]
-        
-        while left <= right:
-            while left <= right and nums[left] < pivot:
-                left += 1
-            while left <= right and nums[right] > pivot:
-                right -= 1
-            if left <= right:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-                right -= 1
-        
-        # if the kth smallest is on the right side, search the right side
-        if k <= right:
-            return self.partition(nums, start, right, k)
-        if k >= left:
-            return self.partition(nums, left, end, k)
-        return nums[k]
+def kthLargestElement(self, k, nums):
+    n = len(nums)
+    # convert the kth largest to the kth smallest
+    k = n - k
+    return self.partition(nums, 0, n - 1, k)
+
+def partition(self, nums, start, end, k):
+    left, right = start, end
+    pivot = nums[left]
+
+    while left <= right:
+        while left <= right and nums[left] < pivot:
+            left += 1
+        while left <= right and nums[right] > pivot:
+            right -= 1
+        if left <= right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+    # if the kth smallest is on the right side, search the right side
+    if k <= right:
+        return self.partition(nums, start, right, k)
+    if k >= left:
+        return self.partition(nums, left, end, k)
+    return nums[k]
