15dbfcaaadb40d814caa9c60bb7a38461b8d975a
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-09-22 13:45:00 +0800

    Create 0225. Implement Stack using Queues.py

---
+++ stack/0225. Implement Stack using Queues.py
@@ -0,0 +1,26 @@
+"""
+
+1. One queue, 队列添加元素后，反转前n-1个元素，栈顶元素始终保留在队首 
+
+Runtime: 36 ms, faster than 26.40% of Python3 online submissions for Implement Stack using Queues.
+Memory Usage: 13.8 MB, less than 58.53% of Python3 online submissions for Implement Stack using Queues.
+"
+
+def __init__(self):
+    self.q = []
+
+def push(self, x: int) -> None:
+    self.q.append(x)
+    q_length = len(self.q)
+    while q_length > 1:
+        self.q.append(self.q.pop(0)) #反转前n-1个元素，栈顶元素始终保留在队首
+        q_length -= 1
+
+def pop(self) -> int:
+    return self.q.pop(0)
+
+def top(self) -> int:
+    return self.q[0]
+
+def empty(self) -> bool:
+    return len(self.q) == 0
