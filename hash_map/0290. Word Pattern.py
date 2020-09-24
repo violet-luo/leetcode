845c816e2f1c2be414a9580a4448d7685e10c202
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-09-24 21:26:06 +0800

    Update 0290. Word Pattern.py

--- hash_map/0290. Word Pattern.py
+++ hash_map/0290. Word Pattern.py
@@ -6,20 +6,20 @@ Memory Usage: 13.6 MB, less than 97.17% of Python3 online submissions for Word P
 """
 
 def wordPattern(self, pattern: str, s: str) -> bool:
-        dic = {}
-        
-        s = s.split()
+    dic = {}
 
-        if len(pattern) != len(s):
-            return False
-        
-        for i in range(len(pattern)):
-            if pattern[i] not in dic:
-                # pattern = "abba" s = "dog dog dog dog"
-                if s[i] in dic.values():
-                    return False
-                dic[pattern[i]] = s[i]
-            else:
-                if dic[pattern[i]] != s[i]:
-                    return False
-        return True
+    s = s.split()
+
+    if len(pattern) != len(s):
+        return False
+
+    for i in range(len(pattern)):
+        if pattern[i] not in dic:
+            # pattern = "abba" s = "dog dog dog dog"
+            if s[i] in dic.values():
+                return False
+            dic[pattern[i]] = s[i]
+        else:
+            if dic[pattern[i]] != s[i]:
+                return False
+    return True
