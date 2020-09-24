d776da7112fe076d4bb3f9365d69520f2a020915
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-09-24 19:50:58 +0800

    Create 0242. Valid Anagram.py

---
+++ hash_map/0242. Valid Anagram.py
@@ -0,0 +1,10 @@
+def isAnagram1(self, s, t):
+    dic1, dic2 = {}, {}
+    for item in s:
+        dic1[item] = dic1.get(item, 0) + 1
+    for item in t:
+        dic2[item] = dic2.get(item, 0) + 1
+    return dic1 == dic2
+    
+def isAnagram2(self, s, t):
+    return sorted(s) == sorted(t)
