b46cb246715a2b14598c10b187bd6acbca689c35
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-10-14 15:04:21 +0800

    Create 0036. Valid Sudoku.py

---
+++ hash_map/0036. Valid Sudoku.py
@@ -0,0 +1,35 @@
+"""
+
+Runtime: 96 ms, faster than 80.97% of Python3 online submissions for Valid Sudoku.
+Memory Usage: 14.1 MB, less than 100.00% of Python3 online submissions for Valid Sudoku.
+
+"""
+
+def isValidSudoku(self, board: List[List[str]]) -> bool:
+    return (self.is_row_valid(board) and
+            self.is_col_valid(board) and
+            self.is_square_valid(board))
+
+def is_row_valid(self, board):
+    for row in board:
+        if not self.is_unit_valid(row):
+            return False
+    return True
+
+def is_col_valid(self, board):
+    for col in zip(*board):
+        if not self.is_unit_valid(col):
+            return False
+    return True
+
+def is_square_valid(self, board):
+    for i in (0, 3, 6):
+        for j in (0, 3, 6):
+            square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+            if not self.is_unit_valid(square):
+                return False
+    return True
+
+def is_unit_valid(self, unit):
+    unit = [i for i in unit if i != '.']
+    return len(set(unit)) == len(unit)
