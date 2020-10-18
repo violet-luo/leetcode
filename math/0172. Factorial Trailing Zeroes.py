ec700cc7638ba1fb940a82dcad9f482e244c4c89
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-10-18 15:36:05 +0800

    Update 0172. Factorial Trailing Zeroes.py

--- math/0172. Factorial Trailing Zeroes.py
+++ math/0172. Factorial Trailing Zeroes.py
@@ -4,15 +4,14 @@
 2. Find how many 25
 3. No. of 5 = n / 5 + n / 25 + n / 125 ……
 
-Runtime: 52 ms, faster than 24.46% of Python3 online submissions for Factorial Trailing Zeroes.
-Memory Usage: 14.1 MB, less than 100.00% of Python3 online submissions for Factorial Trailing Zeroes.
-
+Runtime: 32 ms, faster than 62.11% of Python3 online submissions for Factorial Trailing Zeroes.
+Memory Usage: 14 MB, less than 100.00% of Python3 online submissions for Factorial Trailing Zeroes.
 """
 
 def trailingZeroes(self, n: int) -> int:
     cnt_five = 0
 
-    while n > 0:
+    while n > 1: // not 0 to not get into 0.24
         cnt_five += int(n / 5)
         n /= 5
 
