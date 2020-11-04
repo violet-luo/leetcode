d08f41de1c7d88d7733a3b692434f3c2306e2a5a
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-11-04 23:35:00 +0800

    Update 0287. Find the Duplicate Number.py

--- binary_search/0287. Find the Duplicate Number.py
+++ binary_search/0287. Find the Duplicate Number.py
@@ -8,23 +8,23 @@ Memory Usage: 16.2 MB, less than 18.23% of Python3 online submissions for Find t
 """
 
 def findDuplicate(self, nums: List[int]) -> int:
-        start, end = 1, len(nums) - 1
-        
-        while start < end:
-            mid = start + (end - start) // 2
-            
-            cnt = 0
-            for num in nums:
-                if num <= mid:
-                    cnt += 1
-            
+    start, end = 1, len(nums) - 1
+
+    while start < end:
+        mid = start + (end - start) // 2
+
+        cnt = 0
+        for num in nums:
+            if num <= mid:
+                cnt += 1
+
             # 根据抽屉原理，小于等于 4 的数的个数如果严格大于 4 个，此时重复元素一定出现在 [1, 4] 区间里
-            if cnt > mid:
-                end = mid
-            else:
-                start = mid + 1
-        
-        return start
+        if cnt > mid:
+            end = mid
+        else:
+            start = mid + 1
+
+    return start
         
 
 """
