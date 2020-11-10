04aa5e450aef5389002d98b539e27a4e0f4ccf9e
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-11-10 17:09:34 +0800

    Update 0167. Two Sum II.py

--- two_pointers/0167. Two Sum II.py
+++ two_pointers/0167. Two Sum II.py
@@ -5,13 +5,13 @@ Memory Usage: 14.3 MB, less than 12.20% of Python3 online submissions for Two Su
 
 """
 def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        low, high = 0, len(numbers) - 1
-        while low < high:
-            total = numbers[low] + numbers[high]
-            if total == target:
-                return[low + 1, high + 1]
-            elif total < target:
-                low += 1
-            else:
-                high -= 1
-        return [-1, 1]
+    low, high = 0, len(numbers) - 1
+    while low < high:
+        total = numbers[low] + numbers[high]
+        if total == target:
+            return[low + 1, high + 1]
+        elif total < target:
+            low += 1
+        else:
+            high -= 1
+    return [-1, 1]
