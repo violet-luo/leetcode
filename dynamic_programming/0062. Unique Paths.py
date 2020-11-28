4a79f6c4bb5e810a748e394806f56eb15f63c224
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-11-28 15:50:55 +0800

    Update 0062. Unique Paths.py

--- dynamic_programming/0062. Unique Paths.py
+++ dynamic_programming/0062. Unique Paths.py
@@ -1,8 +1,20 @@
 """
 
 1. DP
+Runtime: 32 ms, faster than 53.92% of Python3 online submissions for Unique Paths.
+Memory Usage: 14.2 MB, less than 43.52% of Python3 online submissions for Unique Paths.
 
 """
+
+def uniquePaths(self, m: int, n: int) -> int:
+    dp = [[0] * n for _ in range(m)]
+    for i in range(m):
+        for j in range(n):
+            if i == 0 or j == 0:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    return dp[m-1][n-1]
 
 
 
