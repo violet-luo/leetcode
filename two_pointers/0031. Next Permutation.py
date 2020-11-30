dcde1e95524cf3562980d3614aed0123245cea18
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-11-30 21:37:47 +0800

    Create 0031. Next Permutation.py

---
+++ two_pointers/0031. Next Permutation.py
@@ -0,0 +1,21 @@
+"""
+
+Runtime: 44 ms, faster than 32.75% of Python3 online submissions for Next Permutation.
+Memory Usage: 14.2 MB, less than 46.05% of Python3 online submissions for Next Permutation.
+
+"""
+def nextPermutation(self, nums: List[int]) -> None:
+    i = len(nums) - 2
+    while i >= 0 and nums[i] >= nums[i + 1]:
+        i -= 1
+    if i >= 0:
+        j = len(nums) - 1
+        while j >= 0 and nums[i] >= nums[j]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+
+    left, right = i + 1, len(nums) - 1
+    while left < right:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1
