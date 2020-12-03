d755aa56912794efd602e369c42edb04d9a25ba1
Violet Luo <18150621+loveFISHly@users.noreply.github.com>
2020-12-03 22:14:42 +0800

    Create 0289. Game of Life.py

---
+++ array/0289. Game of Life.py
@@ -0,0 +1,37 @@
+"""
+
+Runtime: 24 ms, faster than 97.05% of Python3 online submissions for Game of Life.
+Memory Usage: 14.1 MB, less than 84.32% of Python3 online submissions for Game of Life.
+
+"""
+
+def gameOfLife(self, board: List[List[int]]) -> None:
+    neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+
+    rows = len(board)
+    cols = len(board[0])
+
+    # 从原数组复制一份到 copy_board 中
+    copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
+
+    for row in range(rows):
+        for col in range(cols):
+
+            # 对于每一个细胞统计其八个相邻位置里的活细胞数量
+            live_neighbors = 0
+            for neighbor in neighbors:
+                r = (row + neighbor[0])
+                c = (col + neighbor[1])
+
+                # 查看相邻的细胞是否是活细胞
+                if (r < rows and r >= 0) and (c < cols and c >= 0) and (copy_board[r][c] == 1):
+                    live_neighbors += 1
+
+
+            # 规则 1 或规则 3
+            if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                board[row][col] = 0
+
+            # 规则 4
+            if copy_board[row][col] == 0 and live_neighbors == 3:
+                board[row][col] = 1
