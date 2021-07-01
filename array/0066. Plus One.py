5c24486bba6a0937f59aebf89e86392daee90fb5
Violet Luo <18150621+violet-luo@users.noreply.github.com>
2021-07-01 18:47:12 +0800

    Update 0066. Plus One.py

--- array/0066. Plus One.py
+++ array/0066. Plus One.py
@@ -1,23 +1,32 @@
 """
-Runtime: 28 ms, faster than 79.18% of Python3 online submissions for Plus One.
-Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Plus One.
+Three Scenarios
+1. 24+1=25
+2. 29+1=30
+3. 99+1=100
 """
 
-def plusOne(List):
-    if List[-1] != 9:
-        List[-1] = List[-1] + 1
-    elif set(List) == {9}: 
-        List = [1] + [0] * len(List)
-    else: 
-        #find the index of the last non-nine digit
-        i = len(List) - 1 - next((i for i, x in enumerate(reversed(List)) if x != 9), None)
-        List[:i], List[i], List[i+1:] = List[:i], List[i] + 1, [0] * (len(List) - 1 - i)
-    return List
+"""
+1. 
+
+Runtime: 32 ms, faster than 69.00% of Python3 online submissions for Plus One.
+Memory Usage: 14.4 MB, less than 12.65% of Python3 online submissions for Plus One.
+"""
+
+def plusOne(self, digits: List[int]) -> List[int]:
+    for i in range(len(digits)-1, -1, -1):
+        if digits[i] != 9:
+            digits[i] += 1
+            return digits
+        else:
+            digits[i] = 0
+            if digits[0] == 0:
+                digits.insert(0,1)
+                return digits
 
 
 
 """
-Recursion
+2. Recursion
 
 Runtime: 32 ms, faster than 48.52% of Python3 online submissions for Plus One.
 Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Plus One.
