8b7fa97e01e373a44bcaed9d8ac081f22a0757df
Violet Luo <18150621+violet-luo@users.noreply.github.com>
2021-08-30 09:38:36 -0700

    Update 0125. Valid Palindrome.py

--- two_pointers/0125. Valid Palindrome.py
+++ two_pointers/0125. Valid Palindrome.py
@@ -1,21 +1,13 @@
-"""
-
-Runtime: 40 ms, faster than 89.10% of Python3 online submissions for Valid Palindrome.
-Memory Usage: 14.4 MB, less than 10.54% of Python3 online submissions for Valid Palindrome.
-
-"""
-
-def isPalindrome(self, s: str) -> bool:
-    l, r = 0, len(s) - 1
-    while l < r:
-        if not s[l].isalnum():
-            l += 1
-        elif not s[r].isalnum():
-            r -= 1
-        else:
-            if s[l].lower() != s[r].lower():
-                return False
-            else:
-                l += 1
-                r -= 1
+def isPalindrome(self, s):
+    start, end = 0, len(s) - 1
+    while start < end:
+        # 每次要判断left < right, 不然会越界
+        while start < end and not s[start].isalnum():
+            start += 1
+        while start < end and not s[end].isalnum():
+            end -= 1
+        if start < end and s[start].lower() != s[end].lower():
+            return False
+        start += 1
+        end -= 1
     return True
