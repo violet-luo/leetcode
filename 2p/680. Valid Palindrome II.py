b4d7f7351b16303f1bbf5d3de990dae9eee08f22
Violet Luo <18150621+violet-luo@users.noreply.github.com>
2021-09-01 09:08:49 -0700

    Create 680. Valid Palindrome II.py

---
+++ 2p/680. Valid Palindrome II.py
@@ -0,0 +1,29 @@
+def validPalindrome(self, s):
+        left = 0
+        right = len(s) - 1 
+
+        # edge case "ab" 本身就是回文串
+        if left >= right:
+            return True
+
+        while left <= right:
+            if s[left] == s[right]:
+                left += 1 
+                right -= 1 
+            else:
+                # 碰到一对不符合，删除左边或右边的字符
+                return self.helper(s, left, right - 1) or self.helper(s, left + 1, right)
+        
+        return True
+                
+                
+    def helper(self, s, left, right):
+        while left <= right:
+            # 碰到第二对不符合，直接return False
+            if s[left] != s[right]:
+                return False 
+        
+            left += 1 
+            right -= 1 
+        
+        return True
