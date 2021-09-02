98782ec7fa91340e69bd9a07b86911e4faf47c04
Violet Luo <18150621+violet-luo@users.noreply.github.com>
2021-09-02 10:59:54 -0700

    Update 0015. 3Sum.py

--- 2p/0015. 3Sum.py
+++ 2p/0015. 3Sum.py
@@ -1,44 +1,44 @@
-def threeSum(self, nums):
+def threeSum(self, numbers):
     results = []
     
-    if not nums or len(nums) < 3:
+    if not numbers or len(numbers) < 3:
         return results
         
-    nums.sort()
+    numbers.sort()
     
-    length = len(nums)
+    length = len(numbers)
     # 遍历三元组中的最小数
     for i in range(0, length - 2):
         # 经典去重套路
         # 如果当前元素和左边元素一样，跳过
-        if i > 0 and nums[i] == nums[i-1]:
+        if i > 0 and numbers[i] == numbers[i-1]:
             continue
         # abc, a为i，在 i+1 到 len-1 间寻找 bc    
         left = i + 1
         right = length - 1
-        target = -nums[i]
+        target = -numbers[i]
         # 用经典的two sum
-        self.find_two_sum(nums, left, right, target, results)
+        self.find_two_sum(numbers, left, right, target, results)
     return results
 
 # 带有去重逻辑的相向双指针Two Sum
 # [-7, 2, 2, 3, 4, 5, 5]
-def find_two_sums(nums, left, right, target, results):
+def find_two_sum(self, numbers, left, right, target, results):
     while left < right:
         # [2........5]
-        if nums[left] + nums[right] == target:
-            results.append([-target], nums[left], nums[right]])
+        if numbers[left] + numbers[right] == target:
+            results.append([-target, numbers[left], numbers[right]])
             # 两边同时移动
             right -= 1
             left += 1
             # 如果左指针当前数字和左边数字相同，左指针向中间移动，跳过重复
             # [3, 4]
-            while left < right and nums[left] == nums[left - 1]:
+            while left < right and numbers[left] == numbers[left - 1]:
                 left += 1
-            while left < right and nums[right] == nums[right + 1]:
+            while left < right and numbers[right] == numbers[right + 1]:
                 right -= 1
         # 当前 sum 小于 target, 右移左指针，去找更大的sum
-        elif nums[left] + nums[right] > target:
+        elif numbers[left] + numbers[right] > target:
             right -= 1
         # 当前 sum 大于 target, 左移右指针，去找更小的sum
         else:
