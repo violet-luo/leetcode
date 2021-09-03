a2305d3786d01971a8e16947bb831953c5949056
Violet Luo <18150621+violet-luo@users.noreply.github.com>
2021-09-02 17:33:48 -0700

    Create Lintcode 0031. Partition Array.py

---
+++ 2p/Lintcode 0031. Partition Array.py
@@ -0,0 +1,21 @@
+def partitionArray(self, nums, k):
+   if not nums:
+        return 0
+    
+   left, right = 0, len(nums) - 1
+
+    # if left < right, while 循环结束在left == right
+    # 此时需要多一次 if 语句判断 nums[left] 到底是 <k 还是 >= k
+    # 因此使用 left <= right 可以省去这个判断
+    while left <= right:
+        while left <= right and nums[left] < k:
+            left += 1
+        while left <= right and nums[right] >= k:
+            right -= 1
+            
+        if left <= right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+            
+    return left
