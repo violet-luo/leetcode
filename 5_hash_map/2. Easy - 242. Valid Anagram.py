7882c41080989ba779d6c2b1958d01bf832fdb8a
Violet Luo <18150621+violet-luo@users.noreply.github.com>
2021-09-21 11:18:31 -0700

    Create 2. Easy - 242. Valid Anagram.py

---
+++ 5_hash_map/2. Easy - 242. Valid Anagram.py
@@ -0,0 +1,20 @@
+def isAnagram(self, s, t):
+    dic = {}
+    
+    for i in s:
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i] += 1
+    
+    for j in t:
+        if j not in dic:
+            return False
+        else:
+            dic[j] -= 1
+    
+    for val in dic.values():
+        if val != 0:
+            return False
+    
+    return True
