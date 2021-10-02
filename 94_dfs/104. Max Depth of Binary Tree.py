f061fb4ac7ec045bb5aed1b06f6ea4490a2fd6c9
Violet Luo <luo.violeta@gmail.com>
2021-10-02 12:16:30 -0700

    Create 104. Max Depth of Binary Tree.py

---
+++ 94_dfs/104. Max Depth of Binary Tree.py
@@ -0,0 +1,6 @@
+def maxDepth(self, root):
+    if not root:
+        return 0
+    leftDepth = self.maxDepth(root.left)
+     rightDepth = self.maxDepth(root.right)
+    return max(leftDepth, rightDepth) + 1
