da878062963ce9b78a0b33a74e4ff424491206f8
Violet Luo <luo.violeta@gmail.com>
2021-10-09 10:56:21 -0700

    Create Edit Node in BST.py

---
+++ 94_dfs/Edit Node in BST.py
@@ -0,0 +1,9 @@
+def updateBSTBST(root, target, val):
+    if not root:
+        return  # 未找到target节点
+    if target < root.val:
+        updateBST(root.left, target, val) # target小于根节点值，在左子树中查找哦
+    elif target > root.val:
+        updateBST(root.right, target, val) # target大于根节点值，在右子树中查找
+    else:  # 找到了
+        root.val = val
