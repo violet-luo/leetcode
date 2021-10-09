ddf19868ab379df62264156cac8fdad694b0ae04
Violet Luo <luo.violeta@gmail.com>
2021-10-09 10:55:23 -0700

    Create 700. Search in a BST.py

---
+++ 94_dfs/700. Search in a BST.py
@@ -0,0 +1,9 @@
+def searchBST(root, val):
+    if not root:
+        return None # 未找到值为val的节点
+    if val < root.val:
+        return searchBST(root.left, val) # val小于根节点值，在左子树中查找哦
+    elif val > root.val:
+        return searchBST(root.right, val) # val大于根节点值，在右子树中查找
+    else:
+        return root
