eca4915c15ccbe463aab0c112f556d8208ea49f4
Violet Luo <luo.violeta@gmail.com>
2021-11-06 11:07:53 -0700

    Update 0082. Remove Duplicates from Sorted List II.py

--- 1_linked_list/0082. Remove Duplicates from Sorted List II.py
+++ 1_linked_list/0082. Remove Duplicates from Sorted List II.py
@@ -1,30 +1,16 @@
-"""
-
-Runtime: 36 ms, faster than 95.31% of Python3 online submissions for Remove Duplicates from Sorted List II.
-Memory Usage: 13.8 MB, less than 78.35% of Python3 online submissions for Remove Duplicates from Sorted List II.
-
-https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/discuss/28336/Python-in-place-solution-with-dummy-head-node.
-
-"""
-
-def deleteDuplicates(self, head: ListNode) -> ListNode:
-    dummy = ListNode(-1)
+def deleteDuplicates(self, head):
+    dummy = ListNode(0)
     dummy.next = head
+    cur = dummy
     
-    # set up pre and cur pointers
-    pre, cur = dummy, head
-    
-    while cur:
-        if cur.next and cur.val == cur.next.val:
-            # loop till cur point hits the last dupe
-            while cur and cur.next and cur.val == cur.next.val:
-                cur = cur.next
-            # pre points to the node after the last dupe
-            pre.next = cur.next
+    while cur and cur.next and cur.next.next:
+        if cur.next.val != cur.next.next.val:
+            cur = cur.next
         else:
-            # anyways pre has to move forward
-            pre = pre.next
-        # anyways cur has to move forward
-        cur = cur.next
-    
+            node = cur.next.next
+           # 要run node = node.next 一定要 check while node 不然会 runtime error
+           # cur.next.val是重复数值的第一个数，loop到重复数值的后一个数
+            while node and cur.next.val == node.val: 
+                node = node.next
+            cur.next = node
     return dummy.next
