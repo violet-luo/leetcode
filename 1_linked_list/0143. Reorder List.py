a186a766737d085a309d492643599d09cc1a4622
Violet Luo <luo.violeta@gmail.com>
2021-11-08 15:16:44 -0800

    Update 0143. Reorder List.py

--- 1_linked_list/0143. Reorder List.py
+++ 1_linked_list/0143. Reorder List.py
@@ -11,17 +11,17 @@ def reorderList(self, head: ListNode) -> None:
 
     slow = fast = head
 
-    # 1. find the mid point
+    # 1. 先找到中点
     while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
 
-    # 2. reverse the second half in-place
+    # 2. 把后半段倒过来
     prev, slow.next, slow = None, None, slow.next
     while slow:
         prev, prev.next, slow = slow, prev, slow.next
-
-    # 3. merge lists
+        
+    # 3. 前后交替合并
     slow = head
     while prev:
         slow.next, slow = prev, slow.next
