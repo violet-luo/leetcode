65744ea0beebf8f34d339a4720b4a67784b28943
Violet Luo <luo.violeta@gmail.com>
2021-11-09 11:50:51 -0800

    Update 0138. Copy List with Random Pointer.py

--- 1_linked_list/0138. Copy List with Random Pointer.py
+++ 1_linked_list/0138. Copy List with Random Pointer.py
@@ -28,3 +28,32 @@ class Solution:
              mapping[node].random = mapping[node.random]
              
       return mapping[head]
+
+def copyRandomList1(self, head):
+  if not head:
+      return 
+  
+  # 1. 在每一个结点后复制一个没有指向的结点
+  cur = head
+  while cur:
+      nxt = cur.next
+      cur.next = RandomListNode(cur.label)
+      cur.next.next = nxt
+      cur = nxt
+  
+  # 2. 新结点拷贝老结点的random pointer
+  cur = head
+  while cur: 
+      if cur.random: # 只遍历旧结点
+          cur.next.random = cur.random.next # 新结点拷贝老结点的random pointer
+      cur = cur.next.next
+  
+  # 3. 断开链表
+  second = cur = head.next
+  while cur.next:
+      head.next = cur.next
+      head = head.next
+      cur.next = head.next
+      cur = cur.next
+  head.next = None
+  return second
