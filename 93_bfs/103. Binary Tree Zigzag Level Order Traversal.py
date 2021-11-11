e73b7b9282dd27265944d1af11076db3156930bf
Violet Luo <luo.violeta@gmail.com>
2021-11-11 13:02:07 -0800

    Update 103. Binary Tree Zigzag Level Order Traversal.py

--- 93_bfs/103. Binary Tree Zigzag Level Order Traversal.py
+++ 93_bfs/103. Binary Tree Zigzag Level Order Traversal.py
@@ -1,25 +1,25 @@
 def zigzagLevelOrder(self, root):
-    import collections
-    # write your code here
-    if root is None:
-        return []
-    ans = []
-    q = collections.deque()
-    q.append(root)
+    if not root:
+       return []
+    
+    q = collections.deque([root])
+    res = []
+ 
     #正反向标志
     isForward = 1
-    while len(q) is not 0:
-        row = []
+    
+    while q:
+        level = []
         for i in range(len(q)):
-            if q[0].left:
-                q.append(q[0].left)
-            if q[0].right:
-                q.append(q[0].right)
-            row.append(q[0].val)
-            q.popleft()
+            node = q.popleft()
+            level.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
         #根据标志来确认当前层遍历的方向
-        row = row[::isForward]#翻转
-        ans += [row]
+        level = level[::isForward] #翻转
+        res.append(level)
         #方向反转
         isForward *= -1
-    return ans
+    return res
