f8b513aeeed19846d27e667c68d2bc28c80ba7e5
Violet Luo <luo.violeta@gmail.com>
2021-11-15 17:47:30 -0800

    Update 286. Walls and Gates.py

--- 93_bfs/286. Walls and Gates.py
+++ 93_bfs/286. Walls and Gates.py
@@ -1,44 +1,41 @@
-import collections
-DIRECTIONS = ((0, 1), (0, -1), (1, 0), (-1, 0))
+DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-def wallsAndGates(A):
-    if not A or not A[0]:
-        return A
+class Solution:
     
-    n, m = len(A), len(A[0])
-    queue, visited = collections.deque([]), set()
-    get_sources(A, queue, visited)
-    while queue:
-        x, y = queue.popleft()
-        for dx, dy in DIRECTIONS:
-            next_x, next_y = x + dx, y + dy
-            if is_invalid(A, next_x, next_y, visited):
-                continue
-            queue.append((next_x, next_y))
-            visited.add((next_x, next_y))
-            A[next_x][next_y] = A[x][y] + 1
-    return A
-            
+    def wallsAndGates(self, A):
+        if not A or not A[0]:
+            return A
+        
+        queue = collections.deque([])
+        visited = set()
+        
+        for i in range(len(A)):
+            for j in range(len(A[0])):
+                if A[i][j] == 0:
+                    # 这题和number of islands不同处是不在这里做BFS
+                    queue.append((i, j))
+                    visited.add((i, j))
 
-def get_sources(A, queue, visited):
-    n, m = len(A), len(A[0])
-    for i in range(n):
-        for j in range(m):
-            if A[i][j] == 0:
-                queue.append((i, j))
-                visited.add((i, j))
+        while queue:
+            x, y = queue.popleft()
+            for dx, dy in DIRECTIONS:
+                next_x, next_y = x + dx, y + dy
+                if self.is_invalid(A, next_x, next_y, visited):
+                    continue
+                queue.append((next_x, next_y))
+                visited.add((next_x, next_y))
+                A[next_x][next_y] = A[x][y] + 1
+        return A
                 
-                
-def is_invalid(A, next_x, next_y, visited):
-    n, m = len(A), len(A[0])
-    if next_x < 0 or next_x >= n:
-        return True
-    if next_y < 0 or next_y >= m:
-        return True
-    if (next_x, next_y) in visited:
-        return True
-    if A[next_x][next_y] != 2147483647:
-        return True
-    return False
-
-wallsAndGates([[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]])
+                    
+    def is_invalid(self, A, next_x, next_y, visited):
+        n, m = len(A), len(A[0])
+        if next_x < 0 or next_x >= n:
+            return True
+        if next_y < 0 or next_y >= m:
+            return True
+        if (next_x, next_y) in visited:
+            return True
+        if A[next_x][next_y] != 2147483647:
+            return True
+        return False
