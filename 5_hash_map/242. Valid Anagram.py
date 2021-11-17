84226dfb21147b7cccfca67cb825e348a7764d0f
Violet Luo <luo.violeta@gmail.com>
2021-11-17 12:38:08 -0800

    Update and rename 2. Easy - 242. Valid Anagram.py to 242. Valid Anagram.py

--- 5_hash_map/242. Valid Anagram.py
+++ 5_hash_map/242. Valid Anagram.py
@@ -18,3 +18,14 @@ def isAnagram(self, s, t):
             return False
     
     return True
+
+def isAnagram(self, s, t):
+    dic1, dic2 = {}, {}
+    for item in s:
+        dic1[item] = dic1.get(item, 0) + 1
+    for item in t:
+        dic2[item] = dic2.get(item, 0) + 1
+    return dic1 == dic2
+
+def isAnagram(self, s, t):
+    return sorted(s) == sorted(t)
