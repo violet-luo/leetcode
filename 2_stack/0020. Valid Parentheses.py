787f33532b2b73e7050c307d883a88711c4c3523
Violet Luo <luo.violeta@gmail.com>
2021-12-11 11:57:08 -0800

    Update 0020. Valid Parentheses.py

--- 2_stack/0020. Valid Parentheses.py
+++ 2_stack/0020. Valid Parentheses.py
@@ -23,3 +23,28 @@ def isValidParentheses(self, s):
                 return False
 
     return stack == []
+
+"""
+
+Amazon OA
+给你一个str,里面只有 '('和‘)’,让你数valid pairs一共有多少,如果不是valid就返回-1.
+
+"""
+def isValid(s):
+    stack = []
+    counter = 0
+
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) > 0:
+                stack.pop()
+                counter += 1
+        else:
+            return -1
+        
+    if len(stack) == 0:
+        return counter
+    else:
+        return -1
