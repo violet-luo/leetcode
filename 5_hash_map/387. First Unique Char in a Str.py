97184e066a5d58e7fb3be884a24362c2875e7343
Violet Luo <luo.violeta@gmail.com>
2021-12-10 16:00:40 -0800

    Update 387. First Unique Char in a Str.py

--- 5_hash_map/387. First Unique Char in a Str.py
+++ 5_hash_map/387. First Unique Char in a Str.py
@@ -1,3 +1,18 @@
+def firstUniqChar(s):
+    dic = {}
+
+    for char in s:
+        if char not in dic:
+            dic[char] = 1
+        else:
+            dic[char] += 1
+
+    for i in range(len(s)):
+        char = s[i]
+        if dic[char] == 1:
+            return i
+    return -1
+
 """
 1. get
 """
