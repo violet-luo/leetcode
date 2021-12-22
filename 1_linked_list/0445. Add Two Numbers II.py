cac96914b5c81c92c7459e1c531f56dc7e89527d
Violet Luo <luo.violeta@gmail.com>
2021-12-22 13:19:59 -0800

    Update 0445. Add Two Numbers II.py

--- 1_linked_list/0445. Add Two Numbers II.py
+++ 1_linked_list/0445. Add Two Numbers II.py
@@ -1,3 +1,4 @@
+# 1. reverse
 def addLists(self, l1, l2) -> list:
    dummy = cur = ListNode(0)
    carry = 0
@@ -21,3 +22,27 @@ def reverse(self, head):
   while cur:
       cur.next, prev, cur = prev, cur, cur.next
   return prev
+
+# 2. stack
+def addTwoNumbers(self, l1, l2):
+    st1, st2 = [], []
+    while l1:
+        st1.append(l1.val)
+        l1 = l1.next
+        
+    while l2:
+        st2.append(l2.val)
+        l2 = l2.next
+    
+    carry, head = 0, None
+
+    while st1 or st2 or carry:
+        d1, d2 = 0, 0
+        d1 = st1.pop() if st1 else 0
+        d2 = st2.pop() if st2 else 0
+        carry, digit = divmod(d1 + d2 + carry, 10)
+        head_new = ListNode(digit)
+        head_new.next = head
+        head = head_new
+          
+    return head
