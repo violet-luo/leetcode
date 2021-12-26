acb73239289c1d6e257a88c4bb173e106a913980
Violet Luo <luo.violeta@gmail.com>
2021-12-25 18:42:44 -0800

    Update 0062. Unique Paths.py

--- 12_dp/0062. Unique Paths.py
+++ 12_dp/0062. Unique Paths.py
@@ -1,42 +1,37 @@
-"""
-
-1. DP
-Runtime: 32 ms, faster than 53.92% of Python3 online submissions for Unique Paths.
-Memory Usage: 14.2 MB, less than 43.52% of Python3 online submissions for Unique Paths.
-
-"""
-
-def uniquePaths(self, m: int, n: int) -> int:
+// 1. 自顶向下
+def uniquePaths(self, m, n):
+    # state: dp[i][j] 代表从0,0走到i,j的方案总数
     dp = [[0] * n for _ in range(m)]
+
+    # initialize: 初始化第0行和第0列
     for i in range(m):
-        for j in range(n):
-            if i == 0 or j == 0:
-                dp[i][j] = 1
-            else:
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        dp[i][0] = 1
+    for j in range(n):
+        dp[0][j] = 1
+
+    # function: dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+    # answer
     return dp[m-1][n-1]
 
+// 2. 自底向上
+def uniquePaths(self, m, n):
+    # state: dp[i][j] 代表从i,j走到m-1, n-1的方案总数
+    dp = [[0] * n for _ in range(m)]
 
+    # initialize: 初始化第m-1行和第n-1列
+    for i in range(m):
+        dp[i][n-1] = 1
+    for j in range(n):
+        dp[m-1][j] = 1
 
-"""
+    # function: dp[i][j] = dp[i+1][j] + dp[i][j+1]
+    for i in range(m-2, -1, -1):
+        for j in range(n-2, -1):
+            dp[i][j] = dp[i+1][j] + dp[i][j+1]
 
-2. Combination
-Runtime: 40 ms, faster than 5.03% of Python3 online submissions for Unique Paths.
-Memory Usage: 14.3 MB, less than 7.99% of Python3 online submissions for Unique Paths.
-
-"""
-
-def uniquePaths(self, m: int, n: int) -> int:
-    if (m == 1 or n == 1):
-        return 1 
-
-    # make sure m <= n
-    if (m > n):
-        m, n = n, m
-
-    temp, res = 1, 1
-    for i in range(1, m):
-        temp *= i
-    for i in range(n, m + n - 1):
-        res *= i
-    return res//temp
+    # answer
+    return dp[0][0]
