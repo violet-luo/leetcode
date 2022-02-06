828dbb5396656857bcff54d987a14c959df5dc82
Violet Luo <luo.violeta@gmail.com>
2022-02-05 21:05:27 -0800

    Update Google OA. Longest Substring with Same Letter.py

--- 8_string/Google OA. Longest Substring with Same Letter.py
+++ 8_string/Google OA. Longest Substring with Same Letter.py
@@ -1,21 +1,20 @@
-def solution(S):
-    d = {}
+def solution(s):
+    dict = {}
     # store the furthest index of the letters in S
-    for i in range(len(S) - 1, -1, -1): #{b:7, a:6, c:0}
-        if S[i] not in d:
-            d[S[i]] = i
+    for i in range(len(s) - 1, -1, -1): # {b:7, a:6, c:0}
+        if s[i] not in dict:
+            dict[s[i]] = i
             
-    max_length = float("-infinity")
-    best_index = 0
-    
+    max_len, min_idx = 0, 0
+
     # loop from the beginning of S
-    for i, let in enumerate(S): # (0, 'c')
+    for idx, letter in enumerate(s): # (0, 'c')
         # calculate the distance from current instance of the letter to the last
-        sub_length = d[let] + 1 - i
+        substr_len = dict[letter] + 1 - idx
         # only update if the distance is greater than max
         # this means we always start our answer from the earliest index possible. 
-        if sub_length > max_length:
-            best_index = i
-            max_length = sub_length
+        if substr_len > max_len:
+            min_idx = idx
+            max_len = substr_len
             
-    return S[best_index:d[S[best_index]] + 1]
+    return s[min_idx : dict[s[min_idx]] + 1]
