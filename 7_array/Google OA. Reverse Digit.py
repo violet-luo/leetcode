d720547057a54516731a138c1f0a11ec0a66f2c6
Violet Luo <luo.violeta@gmail.com>
2022-02-06 13:11:25 -0800

    Update Google OA. Reverse Digit.py

--- 7_array/Google OA. Reverse Digit.py
+++ 7_array/Google OA. Reverse Digit.py
@@ -1,7 +1,9 @@
-def reverse_digits2(n):
+def reverse_digits(n):
     num_str = str(n)
-    arr = []
-    for i in range(len(num_str)):
-        for j in range(i+2, len(num_str)+1):
-            arr.append(num_str[:i]+num_str[i:j][::-1]+num_str[j:])
-    return max(n, int(max(arr)))
+    max_num = n
+    for i in range(len(num_str) - 1):
+        for j in range(i+1, len(num_str)):
+            if int(num_str[i]) < int(num_str[j]):
+                reverse_str = num_str[:i]+num_str[i:j+1][::-1]+num_str[j+1:]
+                max_num = max(max_num, int(reverse_str))
+    return max_num
