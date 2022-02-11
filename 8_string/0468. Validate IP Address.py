f64807342ccf5c5729a4ffd6607cb8b6afe601a8
Violet Luo <luo.violeta@gmail.com>
2022-02-11 14:46:58 -0800

    Update 0468. Validate IP Address.py

--- 8_string/0468. Validate IP Address.py
+++ 8_string/0468. Validate IP Address.py
@@ -1,8 +1,13 @@
-# 1. length = 4
+# 1. length = 4 (.123.123.123.123.)
 # 2. no leading zero
 # 3. 0 < IP < 255
+# 4. non numeric
 def validateIPAddress(addressIP):
     ipv4 = addressIP.split('.')
+    for num in ipv4:
+        if not num.isnumeric():
+            return False
+        
     for ip in ipv4:
         if len(ipv4) == 4 and str(int(ip)) == ip and 0 <= int(ip) <= 255:
             continue
