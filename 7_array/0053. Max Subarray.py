ad6a53ced5af249e9443da054fb4872a263d5a54
Violet Luo <luo.violeta@gmail.com>
2022-02-12 09:04:49 -0800

    Update 0053. Max Subarray.py

--- 7_array/0053. Max Subarray.py
+++ 7_array/0053. Max Subarray.py
@@ -1,6 +1,6 @@
 def max_subarray(nums):
     prefix_sum = 0 #记录前i个数的和
-   max_sum = -sys.maxsize #全局最大值
+   max_sum = -sys.maxsize #全局最大值，如果全是负数就不能设为0
    min_prefix_sum = 0 #记录前i个数中0-k的最小值
     
     for num in nums:
