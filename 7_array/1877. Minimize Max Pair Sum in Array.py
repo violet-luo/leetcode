5cae2957a499eb825cec1a96a93f6ed848191089
Violet Luo <luo.violeta@gmail.com>
2022-02-13 13:07:33 -0800

    Create 1877. Minimize Max Pair Sum in Array.py

---
+++ 7_array/1877. Minimize Max Pair Sum in Array.py
@@ -0,0 +1,12 @@
+def minPairSum(nums):
+    nums.sort()
+    i = 0
+    j = len(nums) - 1
+    res = 0
+
+    while i < j:
+        res = max(res, nums[i] + nums[j])
+        i += 1
+        j -= 1
+
+    return res
