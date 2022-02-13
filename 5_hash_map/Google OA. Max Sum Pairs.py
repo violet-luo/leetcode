9d28b5f66e799e579c8fb3a05ad5cecd7af6de01
Violet Luo <luo.violeta@gmail.com>
2022-02-13 11:08:04 -0800

    Update Google OA. Max Sum Pairs.py

--- 5_hash_map/Google OA. Max Sum Pairs.py
+++ 5_hash_map/Google OA. Max Sum Pairs.py
@@ -1,13 +1,15 @@
+# [10,8,2,1,9]
 def max_sum_pair(A):
     hash = {}
     for i in range(len(A)):
         for j in range(i+1,len(A)):
-            if A[i] + A[j] not in hash:
+            if A[i] + A[j] not in hash: #{18: [0,1]}
                 hash[A[i] + A[j]] = [i,j]
             elif i not in hash[A[i] + A[j]] and j not in hash[A[i] + A[j]]:
-                hash[A[i] + A[j]].append(i)
+                hash[A[i] + A[j]].append(i) #{11: [0,3,2,4]}
                 hash[A[i] + A[j]].append(j)
-                
+    
+    # 找最长的value
     max_len = 0
     for i in hash:
         if len(hash[i]) > max_len:
