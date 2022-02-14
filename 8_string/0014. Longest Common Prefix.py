aa5ffea1de27708404424d26fbe4ccff8c953242
Violet Luo <luo.violeta@gmail.com>
2022-02-13 22:13:13 -0800

    Update 0014. Longest Common Prefix.py

--- 8_string/0014. Longest Common Prefix.py
+++ 8_string/0014. Longest Common Prefix.py
@@ -1,15 +1,8 @@
-"""
-
-Runtime: 32 ms, faster than 79.53% of Python3 online submissions for Longest Common Prefix.
-Memory Usage: 14.3 MB, less than 5.14% of Python3 online submissions for Longest Common Prefix.
-
-"""
-
-def longestCommonPrefix(self, strs: List[str]) -> str:
+def longestCommonPrefix(strs):
     if not strs:
         return ""
-    shortest = min(strs,key=len)
-    for i, ch in enumerate(shortest):
+    shortest = min(strs, key=len) #'flow'
+    for i, ch in enumerate(shortest): # o, 'f'
         for other in strs:
             if other[i] != ch:
                 return shortest[:i]
