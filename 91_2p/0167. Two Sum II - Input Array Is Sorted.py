d413ede7f473ad52a9f5835f6f7a4e32f2dcd6db
Violet Luo <luo.violeta@gmail.com>
2022-02-14 11:44:51 -0800

    Update 0167. Two Sum II - Input Array Is Sorted.py

--- 91_2p/0167. Two Sum II - Input Array Is Sorted.py
+++ 91_2p/0167. Two Sum II - Input Array Is Sorted.py
@@ -1,15 +1,19 @@
-def twoSum(nums, target):
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    if not numbers:
+        return [-1, 1]
+
+    nums = [(number, index) for index, number in enumerate(numbers)]
     # nlogn
     nums.sort()
 
     # n
     left, right = 0, len(nums) - 1
-    while left < right: #没有等于
-        if nums[left] + nums[right] > target:
-            right-= 1
-        elif nums[left] + nums[right] < target:
+    while left < right: # 需要两个数字所以没有等于
+        if nums[left][0] + nums[right][0] > target:
+            right -= 1
+        elif nums[left][0] + nums[right][0] < target:
             left += 1
         else:
-            return [nums[left], nums[right]]
+            return [nums[left][1], nums[right][1]]
 
     return [-1, 1]
