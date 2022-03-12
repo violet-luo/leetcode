dbed1f62e08e97dc5638652a0551913cda22bbb9
Violet Luo <luo.violeta@gmail.com>
2022-03-12 12:53:33 -0800

    Update 509. Fibonacci.py

--- 94_dfs/509. Fibonacci.py
+++ 94_dfs/509. Fibonacci.py
@@ -1,6 +1,18 @@
-def fibonacci(self, n):
-    if n == 1:
+# 1. dfs
+def fib(self, n: int) -> int:
+    if n == 0:
         return 0
-    if n == 2:
+    if n == 1:
         return 1
-    return fibonacci(n-1) + fibonacci(n-2)
+    return self.fib(n-1) + self.fib(n-2)
+
+# 2. dp
+def fib(self, n: int) -> int:
+    arr = (n+1) * [0]
+    
+    for i in range(0, n+1):
+        if i == 1:
+            arr[1] = 1
+        if i > 1:
+            arr[i] = arr[i-1] + arr[i-2]
+    return arr[-1]
