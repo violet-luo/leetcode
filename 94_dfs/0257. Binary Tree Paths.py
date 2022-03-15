b19f5e2cc8a948b801e08fb47d39fd99ee5b7b85
Violet Luo <luo.violeta@gmail.com>
2022-03-14 21:17:26 -0700

    Update 0257. Binary Tree Paths.py

--- 94_dfs/0257. Binary Tree Paths.py
+++ 94_dfs/0257. Binary Tree Paths.py
@@ -1,15 +1,15 @@
 def binaryTreePaths(self, root):
-    paths = []
-    if not root:
-        return paths
-    if not root.left and not root.right: #递归出口
-        return[str(root.val)]
-    
+    if root is None:
+        return []
+        
+    if root.left is None and root.right is None:
+        return [str(root.val)] #递归出口
+
     leftPaths = self.binaryTreePaths(root.left) #2 #["5"]
     rightPaths = self.binaryTreePaths(root.right) #3 #None
     
     paths = []
     for path in leftPaths + rightPaths:
-         paths.append(str(root.val) + '->' + path) #["2->5", "3"]
+        paths.append(str(root.val) + '->' + path) #["2->5", "3"]
         
-    return path
+    return paths
