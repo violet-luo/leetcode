d77e521fc0005dd7b4f35b51f3ee2a0bd4c5608b
Violet Luo <luo.violeta@gmail.com>
2022-03-16 20:37:55 -0700

    Create 1469. Find All The Lonely Nodes.py

---
+++ 94_dfs/1469. Find All The Lonely Nodes.py
@@ -0,0 +1,14 @@
+def getLonelyNodes(self, root):
+    res = []
+    self.dfs(root, res)
+    return res
+    
+def dfs(self, root, res):
+    if not root: #不能省略
+        return
+    if root.left and not root.right:
+        res.append(root.left.val)
+    if not root.left and root.right:
+        res.append(root.right.val)
+    self.dfs(root.left, res)
+    self.dfs(root.right, res)
