f3c218b159fe26c94b118d675e71d5c3f9010cc8
Violet Luo <luo.violeta@gmail.com>
2022-03-22 23:23:43 -0700

    Create 0105. Preorder and Inorder Traversal to Binary Tree.py

---
+++ 94_dfs/0105. Preorder and Inorder Traversal to Binary Tree.py
@@ -0,0 +1,8 @@
+def buildTree(self, preorder, inorder):
+    if not inorder: 
+        return None # inorder is empty
+    root = TreeNode(preorder[0]) # 前序的第一个为根
+    rootPos = inorder.index(preorder[0]) # 在中序中找到根的位置
+    root.left = self.buildTree(preorder[1 : 1 + rootPos], inorder[ : rootPos])
+    root.right = self.buildTree(preorder[rootPos + 1 : ], inorder[rootPos + 1 : ])
+    return root
