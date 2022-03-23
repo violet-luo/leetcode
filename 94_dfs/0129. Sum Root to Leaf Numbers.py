32e7e0c650ba14e81b4e470103f0b009d3aacb75
Violet Luo <luo.violeta@gmail.com>
2022-03-23 16:37:33 -0700

    Create 0129. Sum Root to Leaf Numbers.py

---
+++ 94_dfs/0129. Sum Root to Leaf Numbers.py
@@ -0,0 +1,12 @@
+def sumNumbers(self, root):
+    return self.dfs(root, 0);
+
+def dfs(self, root, prev):
+    if(root == None) :
+        return 0;
+
+    sum = root.val + prev * 10;
+    if(root.left == None and root.right == None) :
+        return sum;
+
+    return self.dfs(root.left, sum) + self.dfs(root.right, sum)
