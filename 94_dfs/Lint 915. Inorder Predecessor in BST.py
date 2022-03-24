2ce35ee6e642fd56b5e968c2871c366c99ad228e
Violet Luo <luo.violeta@gmail.com>
2022-03-23 20:58:03 -0700

    Create Lint 915. Inorder Predecessor in BST.py

---
+++ 94_dfs/Lint 915. Inorder Predecessor in BST.py
@@ -0,0 +1,23 @@
+# 1. DFS递归
+def inorderPredecessor(self, root, p):
+    if root.val > p.val:
+        return self.inorderPredecessor(root.left, p)
+    elif root.val == p.val:
+        return root.left
+    else: # root.val < p.val
+        node = self.inorderPredecessor(root.right, p)
+        if node is None:
+            return root
+        else:
+            return node
+
+# 2. DFS非递归
+def inorderPredecessor(self, root, p):
+    predecessor = None
+    while root:
+        if root.val >= p.val:
+            root = root.left
+        else:
+            predecessor = root
+            root = root.right
+    return predecessor
