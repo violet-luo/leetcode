1586a250f5271c39b5f2301e62d5a0c18099390f
Violet Luo <luo.violeta@gmail.com>
2022-03-25 14:24:20 -0700

    Update 0222. Count Complete Tree Nodes.py

--- 94_dfs/0222. Count Complete Tree Nodes.py
+++ 94_dfs/0222. Count Complete Tree Nodes.py
@@ -1,16 +1,16 @@
 def countNodes(self, root):
-    leftdepth = self.getdepth(root, True)
-    rightdepth = self.getdepth(root, False)
+    l_depth = self.getHeight(root, True)
+    r_depth = self.getHeight(root, False)
 
-    if leftdepth == rightdepth:
-        return 2 ** leftdepth - 1
+    if l_depth == r_depth:
+        return 2 ** l_depth - 1
     else:
         return 1 + self.countNodes(root.left) + self.countNodes(root.right)
 
-def getdepth(self, root, isLeft):
+def getHeight(self, root, isLeft):
     if root is None:
         return 0
     if isLeft:
-        return 1 + self.getdepth(root.left, isLeft)
+        return 1 + self.getHeight(root.left, isLeft)
     else:
-        return 1 + self.getdepth(root.right, isLeft)
+        return 1 + self.getHeight(root.right, isLeft)
