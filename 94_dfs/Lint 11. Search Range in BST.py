2b7dfae4215292a0d27064e31354733cd38b706d
Violet Luo <luo.violeta@gmail.com>
2022-03-24 20:41:33 -0700

    Update Lint 11. Search Range in BST.py

--- 94_dfs/Lint 11. Search Range in BST.py
+++ 94_dfs/Lint 11. Search Range in BST.py
@@ -1,14 +1,16 @@
 def searchRange(self, root, k1, k2):
-    result = []
-    self.dfs(root, k1, k2, result)
-    return result
+    res = []
+    self.dfs(root, k1, k2, res)
+    return res
 
-def dfs(self, root, k1, k2, result):
-    if root is None:
+def dfs(self, root, k1, k2, res):
+    if not root:
         return
+    # 剪枝，如果当前节点小于等于k1，不必访问左子树
     if root.val > k1:
-        self.dfs(root.left, k1, k2, result)
+        self.dfs(root.left, k1, k2, res)
     if k1 <= root.val and root.val <= k2:
-        result.append(root.val)
+        res.append(root.val)
+    # 剪枝，如果当前节点大于等于k2，不必访问右子树
     if root.val < k2:
-        self.dfs(root.right, k1, k2, result)
+        self.dfs(root.right, k1, k2, res)
