39707b6d4888ffba197972c873d626d1d61e6e36
Violet Luo <luo.violeta@gmail.com>
2022-03-27 15:49:05 -0700

    Update and rename 104. Max Depth of Binary Tree.py to 0104. Max Depth of Binary Tree.py

--- 94_dfs/0104. Max Depth of Binary Tree.py
+++ 94_dfs/0104. Max Depth of Binary Tree.py
@@ -2,5 +2,5 @@ def maxDepth(self, root):
     if not root:
         return 0
     leftDepth = self.maxDepth(root.left)
-     rightDepth = self.maxDepth(root.right)
+    rightDepth = self.maxDepth(root.right)
     return max(leftDepth, rightDepth) + 1
