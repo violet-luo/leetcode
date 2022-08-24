f15703553086ef171775a19e14a1f7b5da2b4c8a
Violet Luo <luo.violeta@gmail.com>
2022-08-24 15:52:58 -0700

    Update 0001. Two Sum.py

--- 91_2p/0001. Two Sum.py
+++ 91_2p/0001. Two Sum.py
@@ -1,19 +1,22 @@
-def twoSum(self, numbers: List[int], target: int) -> List[int]:
+def twoSum(numbers, target):
     if not numbers:
-        return [-1, 1]
-
-    nums = [(number, index) for index, number in enumerate(numbers)]
-    # nlogn
-    nums.sort()
-
-    # n
-    left, right = 0, len(nums) - 1
-    while left < right: # 需要两个数字所以没有等于
-        if nums[left][0] + nums[right][0] > target:
-            right -= 1
-        elif nums[left][0] + nums[right][0] < target:
-            left += 1
+        return [-1, -1]
+    
+    # enumerate(["eat", "sleep") => [(0, 'eat'), (1, 'sleep')]
+   # 这里的顺序不能变为(idx, num)因为sorted sort tuple的第一个element也就是num
+    nums = [(num, idx) for idx, num in enumerate(numbers)]
+   # O(nlogn)
+    nums = sorted(nums)
+    
+    l, r = 0, len(nums) - 1
+   
+   # O(n)
+   # 需要两个数字所以没有等于
+    while l < r:
+        two_sum = nums[l][0] + nums[r][0]
+        if two_sum == target:
+            return sorted([nums[l][1], nums[r][1]])
+        elif two_sum < target:
+            l += 1
         else:
-            return [nums[left][1], nums[right][1]]
-
-    return [-1, 1]
+            r -= 1
