3f4f8c4551c302735c798203eb5cec18fc1b7826
Violet Luo <luo.violeta@gmail.com>
2022-08-26 15:37:51 -0700

    Update 0027. Remove Element.py

--- 91_2p/0027. Remove Element.py
+++ 91_2p/0027. Remove Element.py
@@ -1,6 +1,6 @@
 def removeElement(nums, val):
     l = 0
-    for j in range(len(nums)):
+    for r in range(len(nums)): # 从0开始，因为第一个也可能要被Remove
         if nums[r] != val:
             nums[l] = nums[r]
             l += 1
