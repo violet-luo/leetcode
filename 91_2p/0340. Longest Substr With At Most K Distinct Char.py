0f3c079f072312030697bddd409cd10d9d0d8e2f
Violet Luo <luo.violeta@gmail.com>
2022-09-06 18:23:41 -0700

    Create 0340. Longest Substr With At Most K Distinct Char.py

---
+++ 91_2p/0340. Longest Substr With At Most K Distinct Char.py
@@ -0,0 +1,15 @@
+def lengthOfarr_lenSubstringKDistinct(self, s, k):
+    counter = {}
+    l = 0
+    arr_len = 0
+    
+    for r in range(len(s)):
+        counter[s[r]] = counter.get(s[r], 0) + 1
+        while l <= r and len(counter) > k:
+            counter[s[l]] -= 1
+            if counter[s[l]] == 0:
+                del counter[s[l]]
+            l += 1
+        
+        arr_len = max(arr_len, r - l + 1)
+    return arr_len
