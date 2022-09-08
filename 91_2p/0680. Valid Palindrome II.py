5db856b23be2f9e067e2fc28aada8282c3f1dc6e
Violet Luo <luo.violeta@gmail.com>
2022-09-08 16:27:04 -0700

    Update 0680. Valid Palindrome II.py

--- 91_2p/0680. Valid Palindrome II.py
+++ 91_2p/0680. Valid Palindrome II.py
@@ -1,29 +1,20 @@
 def validPalindrome(self, s):
-        left = 0
-        right = len(s) - 1 
+    l, r = 0, len(s) - 1 # -1 所以s[r]不越界
 
-        # edge case "a" 本身就是回文串
-        if left >= right:
-            return True
-
-        while left <= right:
-            if s[left] == s[right]:
-                left += 1 
-                right -= 1 
-            else:
-                # 碰到一对不符合，删除左边或右边的字符
-                return self.helper(s, left, right - 1) or self.helper(s, left + 1, right)
-        
-        return True
-                
-                
-    def helper(self, s, left, right):
-        while left <= right:
-            # 碰到第二对不符合，直接return False
-            if s[left] != s[right]:
-                return False 
-        
-            left += 1 
-            right -= 1 
-        
-        return True
+    while l < r:
+        if s[l] == s[r]:
+            l += 1 
+            r -= 1 
+        else:
+            # 碰到第一对不符合，删除左边或右边的字符
+            return self.helper(s, l, r - 1) or self.helper(s, l + 1, r)
+    return True          
+            
+def helper(self, s, l, r):
+    while l < r:
+        # 碰到第二对不符合，直接return False
+        if s[l] != s[r]:
+            return False
+        l += 1 
+        r -= 1
+    return True
