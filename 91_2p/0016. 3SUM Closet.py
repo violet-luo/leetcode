7e6f8ec6e23268425e409db235b4cda35e0cbc6a
Violet Luo <luo.violeta@gmail.com>
2022-09-09 12:59:41 -0700

    Update 0016. 3SUM Closet.py

--- 91_2p/0016. 3SUM Closet.py
+++ 91_2p/0016. 3SUM Closet.py
@@ -1,18 +1,22 @@
-def threeSumClosest(self, nums: List[int], target: int) -> int:
+def threeSumClosest(self, nums, target):
+    n = len(nums)
+    if n < 3:
+        return 
+    
     nums.sort()
     res = nums[0] + nums[1] + nums[2]
     
-    for i in range(len(nums)-2):
-        l, r = i+1, len(nums) - 1
+    for i in range(n):
+        l, r = i + 1, n - 1
         while l < r:
             s = nums[i] + nums[l] + nums[r]
             if s == target:
                 return s
+            # 如果找不到exact match, return the closest match
             if abs(s - target) < abs(res - target):
                 res = s
             if s < target:
                 l += 1
             elif s > target:
                 r -= 1
-        
     return res
