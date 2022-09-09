2c77c302966d72674db772060c4d33d0689c50ef
Violet Luo <luo.violeta@gmail.com>
2022-09-09 14:35:09 -0700

    Create 0031. Next Permutation.py

---
+++ 91_2p/0031. Next Permutation.py
@@ -0,0 +1,17 @@
+def nextPermutation(self, nums):
+    n = len(nums)
+    for i in range(n - 1, 0, -1):
+        # find the index of the last peak
+        if nums[i] > nums[i - 1]:
+            # i = 3, [1,2,3,6,5,4] -> [1,2,3,4,5,6]
+            nums[i:] = sorted(nums[i:])
+
+            # swap the pre-last peak index with the value just larger than it
+            # [1,2,3,4,5,6] -> [1,2,4,3,5,6]
+            for j in range(i, n):
+                if nums[i-1] < nums[j]:
+                    nums[j], nums[i-1] = nums[i-1], nums[j]
+                    return nums
+                    
+    # [3,2,1] -> [1,2,3]
+    return nums.reverse()
