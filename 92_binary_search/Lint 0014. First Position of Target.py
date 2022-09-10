9822a3b90b6692c5d02f2342e2380c21f0327cb2
Violet Luo <luo.violeta@gmail.com>
2022-09-10 13:31:55 -0700

    Create Lint 0014. First Position of Target.py

---
+++ 92_binary_search/Lint 0014. First Position of Target.py
@@ -0,0 +1,18 @@
+def binarySearch(nums, target):
+    # 通用二分模板
+    if not nums or not target:
+        return -1
+    l, r = 0, len(nums) - 1
+    
+    while l + 1 < r :
+        mid = (l + r) // 2
+        if nums[mid] < target:
+            l = mid
+        else:
+            r = mid
+    
+    if nums[l] == target:
+        return l
+    elif nums[r] == target:
+        return r
+    return -1
