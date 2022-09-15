7b108bb959533593e096d8ef55d1f341b8359ae7
Violet Luo <luo.violeta@gmail.com>
2022-09-15 11:43:48 -0700

    Update 0034. Find First and Last Position of Element in Sorted Array.py

--- 92_binary_search/0034. Find First and Last Position of Element in Sorted Array.py
+++ 92_binary_search/0034. Find First and Last Position of Element in Sorted Array.py
@@ -1,33 +1,27 @@
-"""
-
-Runtime: 84 ms, faster than 65.21% of Python3 online submissions for Find First and Last Position of Element in Sorted Array.
-Memory Usage: 15 MB, less than 100.00% of Python3 online submissions for Find First and Last Position of Element in Sorted Array.
-
-"""
-
-def searchRange(self, nums: List[int], target: int) -> List[int]:
-    # two binary searches for the begining and end of the range
+def searchRange(nums, target):
     if not nums:
         return [-1, -1]
-
+        
+    # 第一次二分查找第一个>= target的位置
     def bisect_left(nums, target):
         l, r = 0, len(nums) - 1
         while l < r:
-            m = (l + r) // 2
+            mid = (l + r) // 2 #上取整
             if nums[m] < target:
-                l = m + 1
+                l = mid + 1
             else:
-                r = m
+                r = mid
         return l if nums[l] == target else -1
-
+    
+    # 第二次二分查找最后一个 <= target的位置
     def bisect_right(nums, target):
         l, r = 0, len(nums) - 1
         while l < r:
-            m = (l + r) // 2 + 1
-            if nums[m] > target:
-                r = m - 1
+            mid = (l + r) // 2 + 1 # 下取整，防止死循环
+            if nums[mid] > target:
+                r = mid - 1
             else:
-                l = m
+                l = mid
         return l if nums[l] == target else -1
 
     return [bisect_left(nums, target), bisect_right(nums, target)]
