01f6f453c282022d0336e84fd93cc9d1ddddaedc
Violet Luo <luo.violeta@gmail.com>
2022-09-15 20:56:32 -0700

    Update 0074. Search a 2D Matrix.py

--- 92_binary_search/0074. Search a 2D Matrix.py
+++ 92_binary_search/0074. Search a 2D Matrix.py
@@ -1,25 +1,17 @@
-"""
-
-Runtime: 40 ms, faster than 83.55% of Python3 online submissions for Search a 2D Matrix.
-Memory Usage: 14.4 MB, less than 24.85% of Python3 online submissions for Search a 2D Matrix.
-
-"""
-
-def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-    m = len(matrix)
-    if m == 0:
+def searchMatrix(matrix, target):
+    if not matrix or not matrix[0]:
         return False
-    n = len(matrix[0])
+    n, m = len(matrix), len(matrix[0])
 
-    left, right = 0, m * n - 1
-    while left <= right:
-        mid = (left + right) // 2
-        element = matrix[mid // n ][mid % n]
+    l, r = 0, n * m - 1
+    while l <= r:
+        mid = (l + r) // 2
+        element = matrix[mid // m][mid % m]
         if target == element:
             return True
         elif target < element:
-            right = mid - 1
+            r = mid - 1
         else:
-            left = mid + 1
+            l = mid + 1
 
     return False
