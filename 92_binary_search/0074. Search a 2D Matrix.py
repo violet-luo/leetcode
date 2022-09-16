3092020082bd61b91885bffc0682a58984288cfd
Violet Luo <luo.violeta@gmail.com>
2022-09-16 15:04:18 -0700

    Update 0074. Search a 2D Matrix.py

--- 92_binary_search/0074. Search a 2D Matrix.py
+++ 92_binary_search/0074. Search a 2D Matrix.py
@@ -1,17 +1,17 @@
 def searchMatrix(matrix, target):
     if not matrix or not matrix[0]:
         return False
+        
     n, m = len(matrix), len(matrix[0])
-
     l, r = 0, n * m - 1
     while l <= r:
         mid = (l + r) // 2
         element = matrix[mid // m][mid % m]
-        if target == element:
+        if element == target:
             return True
-        elif target < element:
-            r = mid - 1
-        else:
+        elif element < target:
             l = mid + 1
+        else:
+            r = mid - 1
 
     return False
