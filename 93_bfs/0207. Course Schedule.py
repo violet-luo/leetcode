4bd6b0d54319943e8826693bd9a1450adbeb5ebc
Violet Luo <luo.violeta@gmail.com>
2022-09-28 20:37:47 -0700

    Update 0207. Course Schedule.py

--- 93_bfs/0207. Course Schedule.py
+++ 93_bfs/0207. Course Schedule.py
@@ -1,29 +1,34 @@
-def canFinish(self, numCourses, prerequisites):
+def canFinish(self, numCourses, prereq):
+    # 构建图，代表先修课 -> 后修课的映射
     graph = [[] for i in range(numCourses)]
 
     # 1. 统计每个点的入度，并构建图 
-    in_degree = [0] * numCourses
-    for node_in, node_out in prerequisites:
-        graph[node_out].append(node_in) #存储每节课对应的后续课
-        in_degree[node_in] += 1 #每节课加入度
+    indegree = [0] * numCourses # [0, 0, 0, 0]
+    for course, precourse in prereq:
+        # graph 存储每节课对应的后续课
+        graph[precourse].append(course) # [[1,2], [3], [3], []]
+        # 每节课加入度
+        indegree[course] += 1 # [0, 1, 1, 2]
     
     # 2. 将每个入度为0的点放入队列 queue 中作为起始节点
     queue = collections.deque()
     for i in range(numCourses):
-        if in_degree[i] == 0:
+        if indegree[i] == 0:
             queue.append(i)
 
     # 记录已修课程的数量
-    num_choose = 0
+    num_taken = 0
     
     # 3. 不断从队列中拿出一个点，去掉这个点的所有连边，其他点的相应入度-1
     while queue:
-        now_pos = queue.popleft()
-        num_choose += 1
-        # 将每条邻边删去，如果入度降为 0，再加入队列
-        for j in graph[now_pos]:
-            in_degree[j] -= 1
-            if in_degree[j] == 0:
+        cur_course = queue.popleft()
+        num_taken += 1
+        # 当前课的所有后续课入度-1
+        for j in graph[cur_course]: #当前课
+            indegree[j] -= 1
+            # 4. 一旦发现新的入度为0的点，丢回队列中
+            # 表示一门后修课的所有先修课已经完成，可以被修了
+            if indegree[j] == 0:
                 queue.append(j)
     
-    return num_choose == numCourses
+    return num_taken == numCourses
