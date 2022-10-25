6abb472e2acdf48346f321c7f405a05cf2f3bd72
Violet Luo <luo.violeta@gmail.com>
2022-10-24 20:15:07 -0700

    Update 0543. Diameter of Binary Tree.py

--- 94_dfs/0543. Diameter of Binary Tree.py
+++ 94_dfs/0543. Diameter of Binary Tree.py
@@ -1,7 +1,7 @@
 def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-    return self.dfs(root.left) + self.dfs(root.right)
+    return self.getHeight(root.left) + self.getHeight(root.right)
 
-def dfs(self, node):
-    if node is None:
+def getHeight(self, node):
+    if not node:
         return 0
-    return max(self.dfs(node.left), self.dfs(node.right)) + 1
+    return max(self.getHeight(node.left), self.getHeight(node.right)) + 1
