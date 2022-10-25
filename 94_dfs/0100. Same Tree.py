363a25baaf1592f8ed9236e67f0ee709bb284e16
Violet Luo <luo.violeta@gmail.com>
2022-10-24 18:15:14 -0700

    Update 0100. Same Tree.py

--- 94_dfs/0100. Same Tree.py
+++ 94_dfs/0100. Same Tree.py
@@ -1,21 +1,8 @@
-❌❌❌❌❌❌❌❌❌
 def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    if (p is None and q is not None) or (p is not None and q is None) or p.val != q.val:
+    if not p and not q:
+        return True
+    elif not p or not q:
+        return False
+    if p.val != q.val:
         return False
     return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-
-✅✅✅✅✅✅✅✅✅
-def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    if p == None and q == None: 
-        return True
-    if p and q and p.val == q.val:
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-    return False
-
-def isSameTree(self, p, q):
-    if (not p and q) or (not q and p) or (p and q and p.val != q.val):
-        return False               
-    elif p and q:
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
-    else:
-        return True
