db20a214dcd4134347aa344eaa3848de5ca45e8b
Violet Luo <luo.violeta@gmail.com>
2022-10-24 19:05:27 -0700

    Update 0572. Subtree of Another Tree.py

--- 94_dfs/0572. Subtree of Another Tree.py
+++ 94_dfs/0572. Subtree of Another Tree.py
@@ -1,15 +1,17 @@
 def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
     if not root and not subRoot:
-       return True
+        return True
     if (not root and subRoot) or (root and not subRoot):
         return False
     if root.val == subRoot.val and self.isSameTree(root, subRoot):
         return True
-    return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot) #not and
 
 def isSameTree(self, p, q):
-    if p == None and q == None: 
+    if not p and not q:
         return True
-    if p and q and p.val == q.val:
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-    return False
+    elif not p or not q:
+        return False
+    if p.val != q.val: #出口
+        return False
+    return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
