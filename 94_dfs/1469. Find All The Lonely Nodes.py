391961d204983d2a196afbefa31a99a30a399450
Violet Luo <luo.violeta@gmail.com>
2022-10-25 14:45:25 -0700

    Update 1469. Find All The Lonely Nodes.py

--- 94_dfs/1469. Find All The Lonely Nodes.py
+++ 94_dfs/1469. Find All The Lonely Nodes.py
@@ -2,7 +2,8 @@ def getLonelyNodes(self, root):
     res = []
     self.dfs(root, res)
     return res
-    
+
+# 因为需要多传一个参数，所以需要新建一个dfs
 def dfs(self, root, res):
     if not root: #不能省略
         return
