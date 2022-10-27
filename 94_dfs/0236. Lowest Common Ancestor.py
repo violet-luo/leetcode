7bf9c4410f07d25044aaac727ddbf9fda4801214
Violet Luo <luo.violeta@gmail.com>
2022-10-26 17:55:19 -0700

    Update 0236. Lowest Common Ancestor.py

--- 94_dfs/0236. Lowest Common Ancestor.py
+++ 94_dfs/0236. Lowest Common Ancestor.py
@@ -1,14 +1,24 @@
-def lowestCommonAncestor(self, root, A, B):
-    if root is None:
+def lca(self, root, A, B):
+    if not root:
         return None
     if root == A or root == B:
-        return root
-    left = self.lowestCommonAncestor(root.left, A, B)
-    right = self.lowestCommonAncestor(root.right, A, B)
-    if left and right:
         return root 
+    
+    # 找 A 和 B
+    left = self.lca(root.left, A, B)
+    right = self.lca(root.right, A, B)
+
+    # A 和 B 一边一个
+    if left and right: 
+        return root
+    
+    # 左子树有一个点或者左子树有LCA
     if left:
         return left
+    
+    # 右子树有一个点或者右子树有LCA
     if right:
-        return right 
+        return right
+        
+    # 左右子树啥都没有
     return None
