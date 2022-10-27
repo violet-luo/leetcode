914869e13d752658fa763cf6b705ee0b79668854
Violet Luo <luo.violeta@gmail.com>
2022-10-26 20:15:48 -0700

    Update and rename 112. Path Sum.py to 0112. Path Sum.py

--- 94_dfs/0112. Path Sum.py
+++ 94_dfs/0112. Path Sum.py
@@ -1,6 +1,6 @@
 def hasPathSum(self, root, sum):
-    if not root: # 这里加not targetSum会报错
+    if not root: # 出口2，这里加not sum会死循环，报错
         return False
-    if root.val == sum and not root.left and not root.right:
+    if root.val == sum and not root.left and not root.right: # 出口1
         return True
     return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
