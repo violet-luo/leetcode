8fbdea3f077b60b154bdd32324f222eff587bfa4
Violet Luo <luo.violeta@gmail.com>
2022-10-27 12:13:51 -0700

    Update and rename Lint 596. Min Subtree.py to Lint 0596. Min Subtree.py

--- 94_dfs/Lint 0596. Min Subtree.py
+++ 94_dfs/Lint 0596. Min Subtree.py
@@ -1,3 +1,22 @@
+def findSubTree(self, root)：
+    self.min_sum = sys.maxint
+    self.res = root
+    self.get_sum(root)
+    return self.res
+
+def get_sum(self, root):
+    if not root:
+        return 0
+    left = self.get_sum(root.left)
+    right = self.get_sum(root.right)
+    sum = root.val + left + right
+    if sum < self.min_sum:
+        self.min_sum = sum
+        self.res = node
+    return sum
+
+//
+
 class Solution:
     """
     @param root: the root of binary tree
