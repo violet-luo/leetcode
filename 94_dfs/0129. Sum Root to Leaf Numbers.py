707c8e02cebe146fa783e3dd5fef4dd2204708ec
Violet Luo <luo.violeta@gmail.com>
2022-10-26 20:01:04 -0700

    Update 0129. Sum Root to Leaf Numbers.py

--- 94_dfs/0129. Sum Root to Leaf Numbers.py
+++ 94_dfs/0129. Sum Root to Leaf Numbers.py
@@ -1,11 +1,11 @@
 def sumNumbers(self, root):
-    return self.dfs(root, 0)
+    return self.helper(root, 0)
 
-# 因为需要上一个节点的信息，所以需要重新建立一个dfs
-def dfs(self, root, prev):
+# 因为需要上一个节点的信息，所以需要重新建立一个helper
+def helper(self, root, prev):
     if not root:
         return 0
     sum = root.val + prev * 10
     if not root.left and not root.right: # 出口
         return sum
-    return self.dfs(root.left, sum) + self.dfs(root.right, sum)
+    return self.helper(root.left, sum) + self.helper(root.right, sum)
