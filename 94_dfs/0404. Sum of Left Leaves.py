d4da9a4f74dc0bb99a91fd6133a4d7fec009f582
Violet Luo <luo.violeta@gmail.com>
2022-10-26 19:37:55 -0700

    Update 0404. Sum of Left Leaves.py

--- 94_dfs/0404. Sum of Left Leaves.py
+++ 94_dfs/0404. Sum of Left Leaves.py
@@ -1,12 +1,12 @@
-def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+def sumOfLeftLeaves(self, root):
     if not root:
         return 0
-    sum_left = 0
+    left_sum = 0
     if root.left: 
         if not root.left.left and not root.left.right: #出口：左叶子节点
-            sum_left += root.left.val
+            left_sum += root.left.val
         else:
-            sum_left += self.sumOfLeftLeaves(root.left)
-    if root.right:
-        sum_left += self.sumOfLeftLeaves(root.right)
-    return sum_left
+            left_sum += self.sumOfLeftLeaves(root.left)
+    if root.right: #如果不加这个条件就走不到15
+        left_sum += self.sumOfLeftLeaves(root.right)
+    return left_sum
