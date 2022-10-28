25eccd2f7f6ae1fae5b732c54d159debfec7a5c6
Violet Luo <luo.violeta@gmail.com>
2022-10-28 13:06:23 -0700

    Update 0669. Trim a BST.py

--- 94_dfs/0669. Trim a BST.py
+++ 94_dfs/0669. Trim a BST.py
@@ -1,11 +1,11 @@
-def trimBST(self, root, min, max):
+def trimBST(self, root, low, high):
     if root is None:
-        return root
-    if root.val < min :
-        return self.trimBST(root.right, min, max)
-    elif root.val > max :
-        return self.trimBST(root.left, min, max)
+        return
+    if root.val < low:
+        return self.trimBST(root.right, low, high)
+    elif root.val > high:
+        return self.trimBST(root.left, low, high)
     else: # min < root < max
-        root.left = self.trimBST(root.left, min, max)
-        root.right = self.trimBST(root.right, min, max)
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
     return root
