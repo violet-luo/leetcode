fdb835c58b91186c61bb975d56e942ad38ed3477
Violet Luo <luo.violeta@gmail.com>
2022-11-03 14:47:29 -0700

    Update 0530. Min Absolute Diff in BST.py

--- 94_dfs/0530. Min Absolute Diff in BST.py
+++ 94_dfs/0530. Min Absolute Diff in BST.py
@@ -1,15 +1,17 @@
-class Solution(object):
-    pre, res = -float('inf'), float('inf')
-    
-    # [1,3,4,5,6,7,8]
-    def minDiffInBST(self, root): #minDiff(3) #minDiff(4)
-        # 前序遍历
-        if root.left:
-            self.minDiffInBST(root.left)
-        # 中序遍历
-        self.res = min(self.res, root.val - self.pre) # 3-1=2 #4-3=1
-        self.pre = root.val #之前minDiff走过的结点
-        # 后序遍历
-        if root.right:
-            self.minDiffInBST(root.right)
-        return self.res
+def getMinimumDifference(self, root):
+    self.res = float('inf')
+    self.pre = None
+    self.inorder(root)
+    return self.res
+
+def inorder(self, node):
+    if not node:
+        return
+
+    self.inorder(node.left)
+    if self.pre is not None:
+        self.res = min(self.res, node.val - self.pre)
+
+    self.pre = node.val
+    if node.right: 
+        self.inorder(node.right)
