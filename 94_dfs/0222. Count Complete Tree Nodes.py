c616703a01992b8517f8e9059d8ada0e28438103
Violet Luo <luo.violeta@gmail.com>
2022-11-03 19:58:31 -0700

    Update 0222. Count Complete Tree Nodes.py

--- 94_dfs/0222. Count Complete Tree Nodes.py
+++ 94_dfs/0222. Count Complete Tree Nodes.py
@@ -1,23 +1,6 @@
-def countNodes(self, root):
-    if root == None:
-        return 0
-
-    leftHeight = self.getHeight(root.left)
-    rightHeight = self.getHeight(root.right)
-    
-    # 如果左子树的深度 = 右子树的深度，左子树为满二叉树
-    # 节点数 = 左子树的深度 + 右子树的深度 + 根节点
-    if leftHeight == rightHeight:
-        return (2 ** leftHeight - 1) + self.countNodes(root.right) + 1
-    # 如果左子树的深度 ＞ 右子树的深度，右子树为满二叉树
-    # 节点数 = 左子树的深度 + 右子树的深度 + 根节点
-    else:
-        return (2 ** rightHeight - 1) + self.countNodes(root.left) + 1
-
-def getHeight(self, root:TreeNode):
-    height = 0
-    while root:
-        root = root.left
-        height += 1
-
-    return height
+def invertTree(self, root):
+    if not root:
+        return
+    root.left = self.invertTree(root.right)
+    root.right = self.invertTree(root.left)
+    return root
