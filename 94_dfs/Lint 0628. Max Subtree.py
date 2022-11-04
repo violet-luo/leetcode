e9f3df1918133cf2669a49c4c3ac8a45e926acd9
Violet Luo <luo.violeta@gmail.com>
2022-11-03 21:41:20 -0700

    Update Lint 0628. Max Subtree.py

--- 94_dfs/Lint 0628. Max Subtree.py
+++ 94_dfs/Lint 0628. Max Subtree.py
@@ -1,5 +1,5 @@
 def findSubTree(self, root)：
-    self.max_sum = -sys.maxint
+    self.max_sum = float('-inf')
     self.res = root
     self.get_sum(root)
     return self.res
@@ -14,31 +14,3 @@ def get_sum(self, root):
         self.max_sum = sum
         self.res = node
     return sum
-
-//
-
-class Solution:
-    # @param {TreeNode} root the root of binary tree
-    # @return {int} the maximum weight node
-    import sys
-    maximum_weight = 0
-    result = None
-
-    def findSubtree(self, root):
-        # Write your code here
-        self.helper(root)
-
-        return self.result
-
-    def helper(self, root):
-        if root is None:
-            return 0
-
-        left_weight = self.helper(root.left)
-        right_weight = self.helper(root.right)
-        
-        if left_weight + right_weight + root.val >= self.maximum_weight or self.result is None:
-            self.maximum_weight = left_weight + right_weight + root.val
-            self.result = root
-
-        return left_weight + right_weight + root.val
