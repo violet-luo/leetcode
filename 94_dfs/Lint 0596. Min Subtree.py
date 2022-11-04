89d9c62365154630dbbb4f74c3eec56fd68ca319
Violet Luo <luo.violeta@gmail.com>
2022-11-03 21:41:46 -0700

    Update Lint 0596. Min Subtree.py

--- 94_dfs/Lint 0596. Min Subtree.py
+++ 94_dfs/Lint 0596. Min Subtree.py
@@ -1,5 +1,5 @@
 def findSubTree(self, root)：
-    self.min_sum = sys.maxint
+    self.min_sum = float('inf')
     self.res = root
     self.get_sum(root)
     return self.res
@@ -14,31 +14,3 @@ def get_sum(self, root):
         self.min_sum = sum
         self.res = node
     return sum
-
-//
-
-class Solution:
-    """
-    @param root: the root of binary tree
-    @return: the root of the minimum subtree
-    """
-    import sys
-    min_weight = 0
-    result = None
-
-    def findSubtree(self, root):
-        self.helper(root)
-        return self.result
-
-    def helper(self, root):
-        if root is None:
-            return 0 
-        
-        left_weight = self.helper(root.left)
-        right_weight = self.helper(root.right)
-        
-        if left_weight + right_weight + root.val <= self.min_weight or self.result is None:
-            self.min_weight = left_weight + right_weight + root.val
-            self.result = root
-
-        return left_weight + right_weight + root.val
