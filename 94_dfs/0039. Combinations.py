57adeaeddc6dc0810422114f84f3b1358a0f677d
Violet Luo <luo.violeta@gmail.com>
2022-11-04 16:08:01 -0700

    Create 0039. Combinations.py

---
+++ 94_dfs/0039. Combinations.py
@@ -0,0 +1,19 @@
+def combinationSum(self, candidates, target):
+    res, subset = [], []
+    candidates = sorted(candidates)  #需要排序
+    self.backtrack(candidates, target, 0, 0, res, subset)
+    return res
+
+def backtrack(self, candidates, target, sum, start_idx, res, subset):
+    if sum > target: 
+        return 
+    if sum == target: 
+        return res.append(subset[:])
+    for i in range(start_idx, len(candidates)):
+        if sum + candidates[i] > target: 
+            return
+        sum += candidates[i] 
+        subset.append(candidates[i])
+        self.backtrack(candidates, target, sum, i, res, subset)  #start_idx = i:表示可以重复读取当前的数
+        sum -= candidates[i]  #回溯
+        subset.pop()  #回溯
