65c2593f985507a6a8bf26d5c4ac83246d39bc34
Violet Luo <luo.violeta@gmail.com>
2022-11-05 12:59:10 -0700

    Update 0046. Permutations.py

--- 94_dfs/0046. Permutations.py
+++ 94_dfs/0046. Permutations.py
@@ -1,26 +1,15 @@
 def permute(self, nums):
-    if not nums:
-        return [[]]
-        
-    permutations = []
-    self.dfs(nums, [], set(), permutations)
-    return permutations
+    res = []  #存放符合条件结果的集合
+    subset = []  #用来存放符合条件的结果
+    self.backtrack(nums, res, subset)
+    return res
 
-# 递归的定义：找到所有 permutation 开头的 permutations
-def dfs(self, nums, permutation, visited, permutations):
-    # 递归的出口，当n个数都记录到了，记录下来
-    if len(nums) == len(permutation):
-        permutations.append(list(permutation))
-        return
-    
-    # 递归的拆解
-    # [] => [1] [2] [3]
-    # [1] => [1,2], [1,3], [1,4]
-    for num in nums:
-        if num in visited:
+def backtrack(self, nums, res, subset):
+    if len(subset) == len(nums): #递归出口
+        return res.append(subset[:]) 
+    for i in range(len(nums)):
+        if nums[i] in subset:  #subset里已经收录的元素，直接跳过
             continue
-        permutation.append(num)
-        visited.add(num)
-        self.dfs(nums, permutation, visited, permutations)
-        visited.remove(num)
-        permutation.pop()
+        subset.append(nums[i])
+        self.backtrack(nums, res, subset) 
+        subset.pop() 
