ad76bb5ba5d6a9f7e6e16a35c38278f1c7772c94
Violet Luo <luo.violeta@gmail.com>
2022-11-04 17:23:16 -0700

    Create 0017. Letter Combinations of a Phone Number.py

---
+++ 94_dfs/0017. Letter Combinations of a Phone Number.py
@@ -0,0 +1,20 @@
+map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+
+class Solution:    
+    def letterCombinations(self, digits):
+        char, res = '', []
+        if digits == '':
+            return res
+        self.backtrack(digits, 0, char, res)
+        return res
+
+    def backtrack(self, digits, index, char, res):
+        if index == len(digits):
+            res.append(char)
+            return
+        
+        letters = map[digits[index]]
+        for letter in letters:
+            char += letter
+            self.backtrack(digits, index + 1, char, res)
+            char = char[:-1] # 回溯
