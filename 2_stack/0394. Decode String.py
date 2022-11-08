030542b3741d455f74f6a2588351244e08432125
Violet Luo <luo.violeta@gmail.com>
2022-11-08 10:59:10 -0800

    Update 0394. Decode String.py

--- 2_stack/0394. Decode String.py
+++ 2_stack/0394. Decode String.py
@@ -1,29 +1,14 @@
 def decodeString(s):
-    res = []
+    stack, res, num = [], "", 0
     for c in s:
-        # loop till ']' and add ele to ['3', '[', 'a', '2', '[']
-        if c != ']':
-            res.append(c)
-            continue
-        
-        # retrieve letters #['c'] #['cc', 'a']
-        letters = []
-        while res and res[-1] != '[':
-            letters.append(res.pop())
-
-        # skip '['
-        res.pop()
-        
-        num = 0
-        base = 1
-        while res and res[-1].isdigit():
-            num += (ord(res.pop()) - ord('0')) * base
-            base *= 10
-            # 不能这样写，100会变成1
-            # cur_num = int(res.pop())
-            # num += num*10 + cur_num
-        # 因为是stack所以要reverse
-        res.append(''.join(reversed(letters)) * num)
-
-    return ''.join(res)
-decodeString('3[a2[c]]')
+        if c == '[': # [[3, ""]]
+            stack.append([num, res])
+            res, num = "", 0 # 归零
+        elif c == ']':
+            cur_num, last_res = stack.pop()
+            res = last_res + cur_num * res
+        elif '0' <= c <= '9':
+            num = num * 10 + int(c)            
+        else: # char为字母
+            res += c
+    return res
