76ba53b3c1dd4913dd78d7ba2bed6bfb1cc59bf5
Violet Luo <luo.violeta@gmail.com>
2022-11-08 11:05:07 -0800

    Update 0394. Decode String.py

--- 2_stack/0394. Decode String.py
+++ 2_stack/0394. Decode String.py
@@ -1,14 +1,14 @@
 def decodeString(s):
     stack, res, num = [], "", 0
-    for c in s:
-        if c == '[': # [[3, ""]]
+    for ch in s:
+        if ch == '[': # [[3, ""]]
             stack.append([num, res])
             res, num = "", 0 # 归零
-        elif c == ']':
+        elif ch == ']':
             cur_num, last_res = stack.pop()
             res = last_res + cur_num * res
-        elif '0' <= c <= '9':
-            num = num * 10 + int(c)            
+        elif ch.isdigit():
+            num = num * 10 + int(ch)            
         else: # char为字母
-            res += c
+            res += ch
     return res
