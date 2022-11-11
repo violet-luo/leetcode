752ab60ef98842ee6fbb6fe1c68e0a35816d1705
Violet Luo <luo.violeta@gmail.com>
2022-11-11 10:24:27 -0800

    Update 0409. Longest Palindrome.py

--- 5_hash_map/0409. Longest Palindrome.py
+++ 5_hash_map/0409. Longest Palindrome.py
@@ -1,32 +1,14 @@
 def longestPalindrome(self, s):
-    dic = {}
-
-    for char in s:
-        if char in dic:
-            del dic[char] # 偶数个
-        else:
-            dic[char] = 1 # 奇数个
-
-    if len(dic) > 0:
-    # 偶数个 + 1个奇数
-        return len(s) - len(dic) + 1
-    else:
-        # 无奇数，直接return原string
-        return len(s)
-      
-def longestPalindrome(self, s):
-    hash = set()
+    res = set()
 
     for c in s:
-        if c in hash:
-            hash.remove(c) # 偶数个
+        if c in res:
+            res.remove(c) # 偶数个
         else:
-            hash.add(c) # 奇数个
+            res.add(c) # 奇数个
 
-    # len(hash)存储了所有奇数
-    if len(hash) > 0:
-        # 偶数个 + 1个奇数
-        return len(s) - len(hash) + 1
-    else:
-        # 无奇数，直接return原string
+    # len(res)存储了所有奇数
+    if len(res) > 0: # 偶数个 + 1个奇数
+        return len(s) - len(res) + 1
+    else: # 无奇数，直接return原string
         return len(s)
