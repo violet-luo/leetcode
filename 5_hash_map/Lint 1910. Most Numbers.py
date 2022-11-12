31d62bcc053423ebaee8c6f0cbe8efc4b73d3dfe
Violet Luo <luo.violeta@gmail.com>
2022-11-11 18:08:56 -0800

    Update Lint 1910. Most Numbers.py

--- 5_hash_map/Lint 1910. Most Numbers.py
+++ 5_hash_map/Lint 1910. Most Numbers.py
@@ -1,13 +1,13 @@
 def findNumber(self, array):
     counter = collections.Counter(array) 
-    answer = 0
-    count = 0
+    res = 0
+    max_count = 0
 
     for num, count in counter.items():
         if count > max_count:
             max_count = count
-            answer = num
-        elif count == max_count and num < answer:
-            answer = num
+            res = num
+        elif count == max_count and num < res:
+            res = num
 
-    return answer
+    return res
