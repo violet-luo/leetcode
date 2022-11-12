7a2f2328d97dc745ba4585ebdddfc9af1b0ba10d
Violet Luo <luo.violeta@gmail.com>
2022-11-12 15:11:17 -0800

    Update 0454. 4Sum II.py

--- 5_hash_map/0454. 4Sum II.py
+++ 5_hash_map/0454. 4Sum II.py
@@ -1,17 +1,14 @@
 def fourSumCount(self, A, B, C, D):
-    # key为(a + b)之和，value为出现频次
-    dictionary = {}
+    dic = {} # key为(a + b)之和，value为出现频次
+    cnt = 0
     
     for a in A:
         for b in B:
             total = a + b
-            # 如果dict中没有key sum, 返回默认频次0
-            dictionary[total] = dictionary.get(total, 0) + 1
+            dic[total] = dic.get(total, 0) + 1 # 如果dict中没有key sum, 返回默认频次0
     
-    cnt = 0
     for c in C:
         for d in D:
             total = c + d
-            # 如果sum出现在AB之和中，累加频次，否则加默认频次0
-            cnt += dictionary.get(-total, 0)
+            cnt += dic.get(-total, 0) # 如果sum出现在AB之和中，累加频次，否则加默认频次0
     return cnt
