d529ea36d90f4d732e58d64a6a48d2f451bfa6c4
Violet Luo <luo.violeta@gmail.com>
2022-11-12 13:40:46 -0800

    Update 0169. Majority Element.py

--- 5_hash_map/0169. Majority Element.py
+++ 5_hash_map/0169. Majority Element.py
@@ -1,16 +1,16 @@
 def majorityNumber(self, nums):
-    storedNum = None
+    majority_ele = None
     counter = 1 #从1起步是因为1第一次进去会被-1
 
     for num in nums:
-        if num == storedNum:
+        if num == majority_ele:
             counter += 1
         else:
             # 对冲两个数的出现次数
             counter -= 1
             # 2出现次数比1多
-            if counter == 0:
-                storedNum = num
+            if counter == 0: # 也可以不缩进，但是会慢一点
+                majority_ele = num
                 counter = 1
-
-    return storedNum
+    
+    return majority_ele
