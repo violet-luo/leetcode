b6eccd1eca914b429496c9b820fd67fb9a371100
Violet Luo <luo.violeta@gmail.com>
2022-11-14 21:12:24 -0800

    Update 0092. Reverse Linked List II.py

--- 1_linked_list/0092. Reverse Linked List II.py
+++ 1_linked_list/0092. Reverse Linked List II.py
@@ -1,22 +1,13 @@
-"""
-
-Runtime: 24 ms, faster than 97.05% of Python3 online submissions for Reverse Linked List II.
-Memory Usage: 13.8 MB, less than 91.58% of Python3 online submissions for Reverse Linked List II.
-
-code: https://leetcode.com/problems/reverse-linked-list-ii/discuss/30672/Python-one-pass-iterative-solution
-drawing: https://leetcode.com/problems/reverse-linked-list-ii/discuss/30709/Talk-is-cheap-show-me-the-code-(and-DRAWING)
-
-"""
-
-def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
-    dummy = ListNode(-1)
+def reverseBetween(self, head, left, right):
+    dummy = ListNode(0)
     dummy.next = head
-    pre, cur = dummy, head
-    
-    for _ in range(m - 1):
-        cur, pre = cur.next, pre.next
-    
-    for _ in range(n - m):
-        cur.next.next, pre.next, cur.next = pre.next, cur.next, cur.next.next
+    cur = head
+    prev = dummy # 而不是None
+
+    for _ in range(left - 1): # range(1) #0, 1
+        cur, prev = cur.next, prev.next
+
+    for _ in range(right - left): #range(2) # 2,3
+        cur.next.next, prev.next, cur.next = prev.next, cur.next, cur.next.next
 
     return dummy.next
