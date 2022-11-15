adca580c93d6ea2dc73793505c7b7f86bb42c782
Violet Luo <luo.violeta@gmail.com>
2022-11-14 20:59:55 -0800

    Update 0206. Reverse Linked List.py

--- 1_linked_list/0206. Reverse Linked List.py
+++ 1_linked_list/0206. Reverse Linked List.py
@@ -1,5 +1,5 @@
 def reverseList(self, head):
     cur, prev = head, None
     while cur:
-        cur.next, prev, cur = prev, cur, cur.next
+        cur.next, prev, cur = prev, cur, cur.next # 3,1,2 = 1,2,3
     return prev
