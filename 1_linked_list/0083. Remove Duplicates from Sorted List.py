11795b095a42e36bfc7769e5f47e0758139beba4
Violet Luo <luo.violeta@gmail.com>
2022-11-14 19:34:30 -0800

    Update 0083. Remove Duplicates from Sorted List.py

--- 1_linked_list/0083. Remove Duplicates from Sorted List.py
+++ 1_linked_list/0083. Remove Duplicates from Sorted List.py
@@ -1,17 +1,8 @@
-"""
-Given a sorted linked list, delete all duplicates such that each element appear only once.
-Input: 1->1->2
-Output: 1->2
-
-
-Runtime: 44 ms, faster than 44.57% of Python3 online submissions for Remove Duplicates from Sorted List.
-Memory Usage: 14 MB, less than 6.45% of Python3 online submissions for Remove Duplicates from Sorted List.
-"""
-
 def deleteDuplicates(self, head):
     if not head:
-        return head
+        return
     cur = head
+    
     while cur.next:
         if cur.val == cur.next.val:
             cur.next = cur.next.next
