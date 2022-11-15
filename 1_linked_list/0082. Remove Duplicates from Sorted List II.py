ac23a9f8d65cd0c1e37966ce5ccccdd0a6412f4b
Violet Luo <luo.violeta@gmail.com>
2022-11-14 20:01:39 -0800

    Update 0082. Remove Duplicates from Sorted List II.py

--- 1_linked_list/0082. Remove Duplicates from Sorted List II.py
+++ 1_linked_list/0082. Remove Duplicates from Sorted List II.py
@@ -1,16 +1,17 @@
 def deleteDuplicates(self, head):
     dummy = ListNode(0)
     dummy.next = head
-    cur = dummy
-    
+    cur = dummy 
+
     while cur and cur.next and cur.next.next:
-        if cur.next.val != cur.next.next.val:
-            cur = cur.next
-        else:
-            node = cur.next.next
-           # 要run node = node.next 一定要 check while node 不然会 runtime error
-           # cur.next.val是重复数值的第一个数，loop到重复数值的后一个数
-            while node and cur.next.val == node.val: 
+        if cur.next.val == cur.next.next.val:
+            node = cur.next.next # node = cur.next也可以
+            # 要run node = node.next 一定要 check while node 不然会 runtime error
+            # cur.next.val是重复数值的第一个数，loop到重复数值的后一个数
+            while node and node.val == cur.next.val:
                 node = node.next
             cur.next = node
+        else:
+            cur = cur.next
+
     return dummy.next
