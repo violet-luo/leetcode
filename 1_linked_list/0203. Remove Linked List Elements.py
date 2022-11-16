e9eb2cc2a3118fd9c923e7ab8be7d80456b94288
Violet Luo <luo.violeta@gmail.com>
2022-11-15 22:18:26 -0800

    Update 0203. Remove Linked List Elements.py

--- 1_linked_list/0203. Remove Linked List Elements.py
+++ 1_linked_list/0203. Remove Linked List Elements.py
@@ -1,19 +1,15 @@
-"""
+def removeElements(self, head, val):
+    if not head:
+        return
 
-Runtime: 76 ms, faster than 72.26% of Python3 online submissions for Remove Linked List Elements.
-Memory Usage: 16.9 MB, less than 51.73% of Python3 online submissions for Remove Linked List Elements.
-
-"""
-
-def removeElements(self, head: ListNode, val: int) -> ListNode:
-    dummy_head = ListNode(-1)
-    dummy_head.next = head 
-    current_node = dummy_head 
-
-    while current_node.next != None:
-        if current_node.next.val == val:
-            current_node.next = current_node.next.next 
+    dummy = ListNode(0)
+    dummy.next = head 
+    cur = dummy
+    
+    while cur.next:
+        if cur.next.val == val:
+            cur.next = cur.next.next 
         else:
-            current_node = current_node.next
-
-    return dummy_head.next
+            cur = cur.next
+            
+    return dummy.next
