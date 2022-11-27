b2d01199287eb4a67c5fe24a81209c687bcd4610
Violet Luo <luo.violeta@gmail.com>
2022-11-27 15:17:30 -0800

    Update 0148. Sort List.py

--- 95_sort/0148. Sort List.py
+++ 95_sort/0148. Sort List.py
@@ -1,63 +1,45 @@
-"""
+def sortList(self, head):
+    if not head or not head.next:
+        return head
+    #把链表分成左右两半    
+    mid = self.find_mid(head)
+    left = head
+    right = mid.next
+    mid.next = None
+    
+    sorted_left = self.sortList(left)
+    sorted_right = self.sortList(right)
+    
+    return self.merge(sorted_left, sorted_right)
+    
+def find_mid(self, head):
+    if not head or not head.next:
+        return head
+    slow = head
+    fast = head
+    while fast.next and fast.next.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+    
+def merge(self, l1, l2): #参见Lintcode#165 Merge Two Sorted Lists
+    if not l1: return l2
+    if not l2: return l1
+    dummy = ListNode(0)
+    cur = dummy
 
-Runtime: 244 ms, faster than 61.73% of Python3 online submissions for Sort List.
-Memory Usage: 22.9 MB, less than 81.28% of Python3 online submissions for Sort List.
-
-https://leetcode.com/problems/sort-list/discuss/46712/Bottom-to-up(not-recurring)-with-o(1)-space-complextity-and-o(nlgn)-time-complextity
-
-"""
-
-class Solution(object):
-    def sortList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        if head is None: return None
+    while l1 and l2:
+        if l1.val < l2.val:
+            cur.next = l1
+            cur = cur.next
+            l1 = l1.next
+        else:
+            cur.next = l2
+            cur = cur.next
+            l2 = l2.next
+    if l1:
+        cur.next = l1
+    if l2:
+        cur.next = l2
         
-        def getSize(head):
-            counter = 0
-            while(head is not None):
-                counter +=1
-                head = head.next
-            return counter
-        
-        def split(head, step):
-            i = 1
-            while (i < step and head):
-                head = head.next
-                i += 1
-                
-            if head is None: return None
-            #disconnect
-            temp, head.next = head.next, None
-            return temp
-        
-        def merge(l, r, head):
-            cur = head
-            while(l and r):
-                if l.val < r.val:
-                    cur.next, l = l, l.next
-                else:
-                    cur.next, r = r, r.next
-                cur = cur.next
-            
-            cur.next = l if l is not None else r
-            while cur.next is not None: cur = cur.next
-            return cur
-
-        size = getSize(head)
-        bs = 1
-        dummy = ListNode(0)
-        dummy.next = head
-        l, r, tail = None, None, None
-        while (bs < size):
-            cur = dummy.next
-            tail = dummy
-            while cur:
-                l = cur
-                r = split(l, bs)
-                cur = split(r, bs)
-                tail = merge(l, r, tail)
-            bs <<= 1
-        return dummy.next
+    return dummy.next
