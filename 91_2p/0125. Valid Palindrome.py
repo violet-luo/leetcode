88dc2c6a1941d9dc98e13e916f97b74cc8a8aef3
Violet Luo <luo.violeta@gmail.com>
2022-12-02 12:44:53 -0800

    Update 0125. Valid Palindrome.py

--- 91_2p/0125. Valid Palindrome.py
+++ 91_2p/0125. Valid Palindrome.py
@@ -1,3 +1,20 @@
+def isPalindrome(self, s: str) -> bool:
+    s = ''.join(ch for ch in s if ch.isalnum())
+    s = s.lower()
+    
+    left, right = 0, len(s) - 1
+    
+    while left < right: # <= 也可
+        if s[left] != s[right]:
+            return False
+        else:
+            left += 1
+            right -= 1
+    
+    return True
+
+##
+
 def isPalindrome(self, s):
     l, r = 0, len(s) - 1
     while l < r:
