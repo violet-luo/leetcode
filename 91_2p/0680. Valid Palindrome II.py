b3e5c62b648a282d0731ad2dbbcca8188d444e6e
Violet Luo <luo.violeta@gmail.com>
2022-12-02 14:22:17 -0800

    Update 0680. Valid Palindrome II.py

--- 91_2p/0680. Valid Palindrome II.py
+++ 91_2p/0680. Valid Palindrome II.py
@@ -1,20 +1,45 @@
+❌❌❌
 def validPalindrome(self, s):
-    l, r = 0, len(s) - 1 # -1 所以s[r]不越界
-
-    while l < r:
-        if s[l] == s[r]:
-            l += 1 
-            r -= 1 
+    left, right = 0, len(s) - 1
+    
+    # 找到第一个不一样的，删除
+    while left < right:
+        if s[left] != s[right]:
+            # 不能这样，'abca'要测试'aba'和'aca'
+            s = s[:left] + s[left+1:]             
         else:
-            # 碰到第一对不符合，删除左边或右边的字符
-            return self.helper(s, l, r - 1) or self.helper(s, l + 1, r)
-    return True          
-            
-def helper(self, s, l, r):
-    while l < r:
-        # 碰到第二对不符合，直接return False
-        if s[l] != s[r]:
+            left += 1
+            right -= 1
+
+    # 重走一遍，即125.Valid Palindrome
+    while left < right:
+        if s[left] != s[right]:
             return False
-        l += 1 
-        r -= 1
+        else:
+            left += 1
+            right -= 1
+
+    return True
+
+
+✅✅✅
+def validPalindrome(self, s):
+    left, right = 0, len(s) - 1
+
+    # 找到第一个不一样的，删除
+    while left < right:
+        if s[left] == s[right]:
+            left += 1
+            right -= 1
+        else:
+            return self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1)
+    return True # 一定要加这一行，'aba'才是True，不然会return None
+
+def isPalindrome(self, s, left, right):
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        else:
+            left += 1
+            right -= 1
     return True
