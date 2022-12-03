742fc1691954939116c2354dfdc9e96f45b071c4
Violet Luo <luo.violeta@gmail.com>
2022-12-02 19:38:43 -0800

    Update 0189. Rotate Array.py

--- 91_2p/0189. Rotate Array.py
+++ 91_2p/0189. Rotate Array.py
@@ -1,11 +1,12 @@
-def rotate(self, nums, k):
-    k = k % len(nums)
-    reverse(nums, 0, len(nums)-1)
-    reverse(nums, 0, k-1)
-    reverse(nums, k, len(nums)-1)
+def rotate(self, nums, k): # 1 2 3 4 5 6 7
+    n = len(nums)
+    k = k % n
+    self.reverse(nums, 0, n - 1) # 7 6 5 4 3 2 1
+    self.reverse(nums, 0, k - 1) # reverse the first k num, 5 6 7 4 3 2 1
+    reverse(nums, k, n - 1) # reverse last n - k num, 5 6 7 1 2 3 4
 
-def reverse(self, s, l, r):
-    while l < r:
-        s[l], a[r] = a[r], a[l]
-        l += 1
-        r -= 1
+def reverse(self, s, left, right):
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
