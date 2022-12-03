99767dde6f427839c5698649f479e13032b9386b
Violet Luo <luo.violeta@gmail.com>
2022-12-02 18:30:12 -0800

    Update 0561. Partition Array.py

--- 95_sort/0561. Partition Array.py
+++ 95_sort/0561. Partition Array.py
@@ -1,10 +1,8 @@
 def arrayPairSum(self, nums: List[int]) -> int:
     res = 0
     nums.sort()
-    i = 0
     
-    while i < len(nums):
+    for i in range(0, len(nums), 2):
         res += nums[i]
-        i += 2
         
     return res
