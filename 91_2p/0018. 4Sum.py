9f53dcdae001f6406888a7114d55120e63b62c3f
Violet Luo <luo.violeta@gmail.com>
2022-12-03 13:44:25 -0800

    Update 0018. 4Sum.py

--- 91_2p/0018. 4Sum.py
+++ 91_2p/0018. 4Sum.py
@@ -1,42 +1,31 @@
-def fourSum(self, numbers, target):
+def fourSum(self, nums, target):
+    nums.sort()
     res = []
-    if not numbers:
-        return res
+    n = len(nums)
 
-    nums = sorted(numbers)
-
-    for i in range(len(nums)):
-        # 经典去重套路
-        # 如果当前元素和左边元素一样，跳过
-        if i > 0 and nums[i] == nums[i-1]:
+    for i in range(0, n - 3):
+        # i去重，去重模版，如果当前元素和左边元素一样，跳过
+        if i and nums[i] == nums[i - 1]:
             continue
-        for j in range(i + 1, len(nums)):
-            if j > i + 1 and nums[j] == nums[j - 1]:
+
+        for j in range(i + 1, n - 2):
+            # j去重，当j和i相等的时候，只记录第一个j, 试例[2,2,2,2,2], 8
+            if j != i + 1 and nums[j] == nums[j - 1]:
                 continue
-
-            # 在 j+1 到 len(nums)-1 的区间寻找 (c,d)
-            pairs = self.find_two_sum_pairs(
-                nums, 
-                j+1, 
-                len(nums) - 1, 
-                target - nums[i] - nums[j]
-            )
-
-            for (c, d) in pairs:
-                res.append([nums[i], nums[j], c, d])
-
+            two_sum = target - nums[i] - nums[j]
+            # 同 three sum
+            left, right = j + 1, n - 1
+            while left < right:
+                if nums[left] + nums[right] == two_sum:
+                    res.append([nums[i], nums[j], nums[left], nums[right]])
+                    right -= 1
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif nums[left] + nums[right] > two_sum:
+                    right -= 1
+                else:
+                    left += 1
     return res
-
-def find_two_sum_pairs(self, nums, l, r, target):
-    pairs = []
-    while l < r:
-        if nums[l] + nums[r] < target:
-            l += 1
-        elif nums[l] + nums[r] > target:
-            r -= 1
-        else:
-            if not pairs or (nums[l], nums[r]) != pairs[-1]:
-                pairs.append((nums[l], nums[r]))
-            l += 1
-            r -= 1
-    return pairs
