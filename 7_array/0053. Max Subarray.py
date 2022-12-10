35c9c51945437e1be27b2927bbc4e044c8a22dcb
Violet Luo <luo.violeta@gmail.com>
2022-12-10 13:32:06 -0800

    Update 0053. Max Subarray.py

--- 7_array/0053. Max Subarray.py
+++ 7_array/0053. Max Subarray.py
@@ -1,12 +1,11 @@
-def max_subarray(nums):
-    prefix_sum = 0 #记录前i个数的和
-   max_sum = -sys.maxsize #全局最大值，如果全是负数就不能设为0
-   min_prefix_sum = 0 #记录前i个数中0-k的最小值
-    
+def maxSubArray(self, nums):
+    prefix_sum, max_sum = 0, float('-inf')
+
     for num in nums:
         prefix_sum += num
-       # 因为min_prefix_sum会出现在两个运算中，所以后更新
-        max_sum = max(max_sum, prefix_sum - min_prefix_sum)
-        min_prefix_sum = min(min_prefix_sum, prefix_sum)
-        
+        max_sum = max(max_sum, prefix_sum)
+        # If sum is positive, it's possible to make the next value bigger, so we keep it
+        # If sum is negative, it has no use to the next element, so we replace with 0
+        prefix_sum = max(prefix_sum, 0)
+
     return max_sum
