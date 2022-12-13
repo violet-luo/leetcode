99aec3c01b485a95dd622f60f6f7ca10e19a9ddf
Violet Luo <luo.violeta@gmail.com>
2022-12-13 15:19:00 -0800

    Update 0077. Combinations.py

--- 94_dfs/0077. Combinations.py
+++ 94_dfs/0077. Combinations.py
@@ -1,3 +1,22 @@
+def combine(n, k):
+    res = []
+    subset = []
+
+    def backtrack(n, k, start_index):
+        if len(subset) == k: # 与78的区别
+            res.append(subset[:]) 
+            return
+
+        for i in range(start_index, n - (k - len(subset)) + 2): # 剪枝
+            subset.append(i) # 与78的区别，不需要append所有的子集
+            backtrack(n, k, i + 1)
+            subset.pop()
+            
+    backtrack(n, k, 1)
+    return res 
+
+###
+
 def combine(self, n, k):
     res, subset = [], []
     self.backtrack(n, k, 1, res, subset)
