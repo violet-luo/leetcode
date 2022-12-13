0ae633d73f7e5fa8c32344727519b943066a6c0c
Violet Luo <luo.violeta@gmail.com>
2022-12-13 15:45:32 -0800

    Update 0090. Subsets II.py

--- 94_dfs/0090. Subsets II.py
+++ 94_dfs/0090. Subsets II.py
@@ -1,3 +1,23 @@
+def subsets(self, nums):
+    res = []
+    subset = []
+    nums = sorted(nums) #去重需要排序
+
+    def backtrack(nums, start_index):
+        res.append(subset[:]) # 不能是res.append(subset)
+        # 因为是组合不是集合，所以不是从0而是从start_index开始遍历
+        for i in range(start_index, len(nums)):
+            if i > start_index and nums[i] == nums[i - 1]: # 与78区别，去重
+                continue
+            subset.append(nums[i])
+            backtrack(nums, i+1)
+            subset.pop()
+
+    backtrack(nums, 0)
+    return res
+
+###
+
 def subsetsWithDup(self, nums):
     res, subset = [], [] 
     nums = sorted(nums)  # 去重需要排序
