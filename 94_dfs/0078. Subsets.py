166a508e3db3f39da363ddf04677de83f0d750dd
Violet Luo <luo.violeta@gmail.com>
2022-12-13 13:55:48 -0800

    Update 0078. Subsets.py

--- 94_dfs/0078. Subsets.py
+++ 94_dfs/0078. Subsets.py
@@ -1,4 +1,21 @@
 def subsets(self, nums):
+    res = []
+    subset = []
+
+    def backtrack(nums, start_index):
+        res.append(subset[:]) # 不能是res.append(subset)
+        # 因为是组合不是集合，所以不是从0而是从start_index开始遍历
+        for i in range(start_index, len(nums)):
+            subset.append(nums[i])
+            backtrack(nums, i+1)
+            subset.pop()
+
+    backtrack(nums, 0)
+    return res
+
+###
+
+def subsets(self, nums):
     res, subset = [], []
     self.backtrack(nums, 0, res, subset)
     return res
