8bae1155978fe79857645ce6a79647fb84a8b47b
Violet Luo <luo.violeta@gmail.com>
2022-12-13 18:21:43 -0800

    Update 0040. Combination Sum II.py

--- 94_dfs/0040. Combination Sum II.py
+++ 94_dfs/0040. Combination Sum II.py
@@ -1,4 +1,29 @@
-def combinationSum2(self, candidates, target)]:
+def combinationSum(self, candidates, target):
+    res = []
+    subset = []
+    sum = 0
+
+    def backtrack(candidates, target, sum, start_index):
+        if sum == target:
+            return res.append(subset[:])
+        for i in range(start_index, len(candidates)):
+            if i > start_index and candidates[i] == candidates[i - 1]: #与39区别，去重
+                continue
+            if sum + candidates[i] > target:
+                return 
+            sum += candidates[i]
+            subset.append(candidates[i])
+            backtrack(candidates, target, sum, i)
+            sum -= candidates[i]
+            subset.pop()
+
+    candidates = sorted(candidates) # 不知道为什么不sort过不了
+    backtrack(candidates, target, 0, 0)
+    return res
+
+###
+
+def combinationSum2(self, candidates, target):
     res, subset = [], []
     candidates = sorted(candidates)  # 给candidates排序，让相同的元素都挨在一起
     self.backtrack(candidates, target, 0, 0, res, subset)
