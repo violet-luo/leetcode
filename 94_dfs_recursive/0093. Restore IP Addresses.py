2e08316d286e78fd4d2261c184e2f6c9017eaec0
Violet Luo <luo.violeta@gmail.com>
2022-12-13 19:41:50 -0800

    Update 0093. Restore IP Addresses.py

--- 94_dfs_recursive/0093. Restore IP Addresses.py
+++ 94_dfs_recursive/0093. Restore IP Addresses.py
@@ -1,4 +1,34 @@
 def restoreIpAddresses(self, s):
+    res = []
+    subset = [] 
+
+    def is_valid(ip):
+        if len(ip) > 1 and ip[0] == '0': return False
+        if 0 <= int(ip) < 256: return True
+        return False
+
+    def backtrack(s, start_index):
+        if len(s) > 12: return
+        if len(subset) == 4 and start_index == len(s):
+            res.append(".".join(subset[:]))
+            return
+
+        for i in range(start_index, len(s)):
+            if len(s) - start_index > 3*(4 - len(subset)): continue # 剪枝，剩下的字符串大于允许的最大长度则跳过
+            substr = s[start_index : i + 1]
+            if is_valid(substr):
+                subset.append(substr)
+            else: 
+                continue
+            backtrack(s, i + 1)
+            subset.pop()
+
+    backtrack(s, 0)
+    return res
+
+###
+
+def restoreIpAddresses(self, s):
     res, subset = [], []
     self.backtrack(s, 0, res, subset)
     return res
