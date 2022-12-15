f04dbef3ad682165b8efad8b7bf198e12f3ef1ac
Violet Luo <luo.violeta@gmail.com>
2022-12-14 19:24:15 -0800

    Update 0490. The Maze.py

--- 93_bfs/0490. The Maze.py
+++ 93_bfs/0490. The Maze.py
@@ -1,3 +1,31 @@
+def hasPath(maze, start, destination):
+    if not maze or not maze[0]: return False
+
+    queue = collections.deque([start])
+    visited = set() # 不加会TLE
+
+    while queue:
+        x, y = queue.popleft()
+        if [x, y] == destination: return True # 到达终点
+        visited.add((x,y))
+
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            x_next, y_next = x + dx, y + dy
+            if (x_next, y_next) in visited:
+                continue
+            visited.add((x_next, y_next))
+
+            # 与number of islands不同的是，如果是valid继续，invalid才停下
+            while 0 <= x_next < len(maze) and 0 <= y_next < len(maze[0]) and maze[x_next][y_next] == 0:
+                x_next += dx
+                y_next += dy
+            # 直到invalid撞墙，将撞墙的前一格加入queue    
+            queue.append([x_next - dx, y_next - dy])
+    
+    return False
+
+###
+
 class Solution:
     def hasPath(self, maze, start, destination): 
         if not maze: 
