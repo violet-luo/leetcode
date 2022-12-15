d5de7b71fa4171a63b33fde1e75062fa2a85d51c
Violet Luo <luo.violeta@gmail.com>
2022-12-14 16:42:01 -0800

    Update 0200. Number of Islands.py

--- 93_bfs/0200. Number of Islands.py
+++ 93_bfs/0200. Number of Islands.py
@@ -1,3 +1,34 @@
+def numIslands(self, grid: List[List[str]]) -> int:
+    if not grid or not grid[0]: return 0
+    n, m = len(grid), len(grid[0])
+
+    def bfs(x, y):
+        queue = deque([(x, y)])
+        grid[x][y] = '0'
+        while queue:
+            x, y = queue.popleft()
+            for delta_x, delta_y in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
+                next_x = x + delta_x
+                next_y = y + delta_y
+                if not is_valid(next_x, next_y):
+                    continue
+                queue.append((next_x, next_y))
+                grid[next_x][next_y] = '0'
+
+    def is_valid(x, y):
+        return 0 <= x < n and 0 <= y < m and grid[x][y] == '1'
+
+    islands = 0
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == '1':
+                bfs(i, j)
+                islands += 1
+
+    return islands
+
+###
+
 def numIslands(self,grid):
     if not grid or not grid[0]: #grid是[[]]
         return 0
