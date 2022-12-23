4224331681cef180aae8877650f18af390567c22
Violet Luo <luo.violeta@gmail.com>
2022-12-23 14:37:08 -0800

    Update 0305. Number of Islands.py

--- union_find/0305. Number of Islands.py
+++ union_find/0305. Number of Islands.py
@@ -1,3 +1,49 @@
+## LC
+class UnionFind:
+    def __init__(self):
+        self.parent = defaultdict()
+        self.count = 0
+
+    def add(self, r, c, m, n):
+        x = r * n + c
+        if x not in self.parent:
+            self.parent[x] = x
+            self.count += 1
+
+        # union with neighbor
+        for dr, dc in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
+            nxt_r, nxt_c = r + dr, c + dc
+            if 0 <= nxt_r < m and 0 <= nxt_c < n:
+                y = nxt_r * n + nxt_c
+                if y in self.parent:
+                    self.union(x, y)
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        parent_x, parent_y = self.find(x), self.find(y)
+        if parent_x != parent_y:
+            self.parent[parent_x] = parent_y
+            self.count -= 1
+
+    def get_count(self):
+        return self.count
+
+
+class Solution:
+    def numIslands2(self, m, n, positions):
+        uf = UnionFind()
+        res = [0] * len(positions)
+        for i, (r, c) in enumerate(positions):
+            uf.add(r, c, m, n)
+            res[i] = uf.count
+
+        return res
+    
+## Lint 
 def numIslands2(self, n, m, operators):
     if not operators:
         return []
