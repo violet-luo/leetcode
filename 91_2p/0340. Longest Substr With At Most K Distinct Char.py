7d66180f8e52a7c2ad5ce00d9d164b8268818ff5
Violet Luo <luo.violeta@gmail.com>
2022-12-25 13:21:05 -0800

    Update 0340. Longest Substr With At Most K Distinct Char.py

--- 91_2p/0340. Longest Substr With At Most K Distinct Char.py
+++ 91_2p/0340. Longest Substr With At Most K Distinct Char.py
@@ -1,15 +1,16 @@
-def lengthOfarr_lenSubstringKDistinct(self, s, k):
-    counter = {}
-    l = 0
-    arr_len = 0
-    
-    for r in range(len(s)):
-        counter[s[r]] = counter.get(s[r], 0) + 1
-        while l <= r and len(counter) > k:
-            counter[s[l]] -= 1
-            if counter[s[l]] == 0:
-                del counter[s[l]]
-            l += 1
+from collections import Counter
+def lengthOfLongestSubstringKDistinct(s, k):
+    max_len, counter = 0, Counter()
+    n = len(s)
+    left = 0
+
+    for right in range(n):
+        counter[s[right]] += 1
+        while right < n and len(counter) > k:
+            counter[s[left]] -= 1
+            if counter[s[left]] == 0:
+                del counter[s[left]]
+            left += 1
+        max_len = max(max_len, right - left + 1)
         
-        arr_len = max(arr_len, r - l + 1)
-    return arr_len
+    return max_len
