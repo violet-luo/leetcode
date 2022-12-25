9f91dc8073b138cb19ea9a2700ba6b52d253265d
Violet Luo <luo.violeta@gmail.com>
2022-12-25 14:01:11 -0800

    Update 0424.  Longest Repeating Character Replacement.py

--- 91_2p/0424.  Longest Repeating Character Replacement.py
+++ 91_2p/0424.  Longest Repeating Character Replacement.py
@@ -1,28 +1,15 @@
-def characterReplacement(self, s, k):
-    if not s:
-        return 0
-
-    # j指向满足条件的子字符串的后面一个位置
-    j = 0
-    maxFreq = 0
-    counter = {}
-    answer = 0
-    for i in range(len(s)): #O(n)
-        # 找到i ~ j-1这一段是最长的以i开头，满足不超过k次替换的子字符
-        while j < len(s) and j - i - maxFreq <= k: #O(n)
-            counter[s[j]] = counter.get(s[j], 0) + 1
-            maxFreq = max(maxFreq, counter[s[j]])
-            j += 1
-
-        if j - i - maxFreq > k:
-            # i ~ j-1 的 substring 需要 k+1
-            # i ~ j-2 的 substring 只需要 k
-            answer = max(answer, j - i - 1)
-        else:
-            # i ~ j-1 的 substring <= k 替换
-            answer = max(answer, j - i)
-
-        counter[s[i]] -= 1
-        maxFreq = max(counter.values())
-
-    return answer
+from collections import Counter
+def characterReplacement(s, k):
+    max_len, counter, res = 0, Counter(), 0
+    left = 0
+    
+    for right in range(len(s)):
+        counter[s[right]] += 1
+        max_len = max(max_len, counter[s[right]])
+        # 如果要替换的次数超过k, 移动左边界，直到不超出
+        while right - left + 1 - max_len > k:
+            counter[s[left]] -= 1
+            left += 1
+        res = max(res, right - left + 1)
+    
+    return res
