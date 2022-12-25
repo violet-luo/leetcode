ddd5e66c1610637ce98169028c7f65510edb086e
Violet Luo <luo.violeta@gmail.com>
2022-12-24 16:17:04 -0800

    Update 1099. Two Sum - Less Than K.py

--- 91_2p/1099. Two Sum - Less Than K.py
+++ 91_2p/1099. Two Sum - Less Than K.py
@@ -1,13 +1,14 @@
 def twoSumLessThanK(nums, k):
     nums.sort()
+    res = float('-inf')
     left, right = 0, len(nums) - 1
-    max_two_sum = -1
-    
+
     while left < right:
         two_sum = nums[left] + nums[right]
-        if two_sum >= k:
-            right -= 1
-        else:
+        if two_sum < k:
+            res = max(res, two_sum)
             left += 1
-            max_two_sum = max(two_sum, max_two_sum) 
-    return max_two_sum
+        else:
+            right -= 1
+
+    return res if res != float('-inf') else -1
