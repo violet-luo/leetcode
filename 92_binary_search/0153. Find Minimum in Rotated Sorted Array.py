5e67958a46d3c5f190722ec02c8a53d0cfce84a6
Violet Luo <luo.violeta@gmail.com>
2022-12-26 14:20:51 -0800

    Update 0153. Find Minimum in Rotated Sorted Array.py

--- 92_binary_search/0153. Find Minimum in Rotated Sorted Array.py
+++ 92_binary_search/0153. Find Minimum in Rotated Sorted Array.py
@@ -1,17 +1,10 @@
-def findMin(self, nums):
-    if not nums:
-        return -1
-
+def findMin(self, nums): 
     start, end = 0, len(nums) - 1
-    
-    # find the first element <= target
     while start + 1 < end:
         mid = (start + end) // 2
-        if nums[mid] > nums[end]:
-            # 如果mid位置上的数字小于等于最右端的数字，区间向左移动
-            start = mid 
-        # 注意：题目声明不存在相等元素，所以这里else 等价于 mid < end
+        if nums[mid] > nums[end]: 
+            start = mid
         else:
             end = mid
 
-    return min(nums[start], nums[end])
+    return(min(nums[start], nums[end]))
