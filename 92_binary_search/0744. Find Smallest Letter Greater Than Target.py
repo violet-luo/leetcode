c9797254d0677042da43b3536881c41a8e2a54c8
Violet Luo <luo.violeta@gmail.com>
2022-12-26 13:27:31 -0800

    Update 0744. Find Smallest Letter Greater Than Target.py

--- 92_binary_search/0744. Find Smallest Letter Greater Than Target.py
+++ 92_binary_search/0744. Find Smallest Letter Greater Than Target.py
@@ -1,18 +1,25 @@
-def nextGreatestLetter(nums, target):
+def nextGreatestLetter(nums, target): # [1,2,2,3], 2
     if target >= nums[-1]:
         return nums[0]
     
+    # 标准二分模版找last pos of target
     l, r = 0, len(nums) - 1
     while l + 1 < r:
         mid = (l + r) // 2
-        # 这里要加 =
-        # ["e","e","n"]，"e" => "n"
         if nums[mid] <= target: 
             l = mid
-        else: 
+        elif nums[mid] > target:
             r = mid
-    
-    if nums[l] > target:
+        else:
+            l = mid
+
+    """
+    1 2 2 3
+    l m   r
+      l m r
+        l r
+    """
+    if target < nums[l]: # [2,3]
         return nums[l]
     
     return nums[r]
