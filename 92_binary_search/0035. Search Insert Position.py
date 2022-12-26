a4e256aeec7dec30df65b368ab5448deae4852f1
Violet Luo <luo.violeta@gmail.com>
2022-12-26 11:24:30 -0800

    Update 0035. Search Insert Position.py

--- 92_binary_search/0035. Search Insert Position.py
+++ 92_binary_search/0035. Search Insert Position.py
@@ -1,6 +1,7 @@
 def searchInsert(nums, target):
     l, r = 0, len(nums) - 1
     
+    # 标准二分模版
     while l + 1 < r:
         mid = (l + r) // 2
         if nums[mid] < target:
@@ -8,8 +9,8 @@ def searchInsert(nums, target):
         else:
             r = mid
     
-    if nums[l] >= target:
+    if nums[l] >= target: # [1,2], 0
         return l
-    if nums[r] >= target:
+    if nums[r] >= target: # [1,2], 1
         return r
-    return len(nums)
+    return len(nums) # [1, 2], 3
