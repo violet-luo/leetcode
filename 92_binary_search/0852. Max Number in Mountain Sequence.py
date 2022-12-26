0a042a79512c8e7ea5c31531c8aa3a17e130d332
Violet Luo <luo.violeta@gmail.com>
2022-12-26 14:06:21 -0800

    Update 0852. Max Number in Mountain Sequence.py

--- 92_binary_search/0852. Max Number in Mountain Sequence.py
+++ 92_binary_search/0852. Max Number in Mountain Sequence.py
@@ -1,18 +1,14 @@
-def mountainSequence(nums):
-    if not nums:
-        return -1
-        
-    # 找到第一个符合条件nums[i] > nums[i+1]的i
-    l, r = 0, len(nums) - 1
-    while l + 1 < r:
-        mid = (l + r) // 2
-        # mid + 1一定不会越界
-        # 因为 while 循环退出条件是 l + 1 < r
-        # 所以一定有 l < mid + 1 < r
-        if nums[mid] > nums[mid + 1]:
-            r = mid
+def peakIndexInMountainArray(self, nums):
+    start, end = 0, len(nums) - 1
+    while start + 1 < end:
+        mid = (start + end) // 2
+        if nums[mid] < nums[mid + 1]: 
+            start = mid
+        elif nums[mid] > nums[mid + 1]:
+            end = mid
         else:
-            l = mid
+            start = mid # end = mid也可以
 
-    # 返回l和r中较大的值，则为山顶
-    return max(nums[l], nums[r])
+    if nums[start] > nums[end]: 
+        return start
+    return end
