f13cb3fb2bfa7ef48cc0371b46b517107a4c9a08
Violet Luo <luo.violeta@gmail.com>
2022-12-26 17:06:02 -0800

    Create 0378. Kth Smallest Element in a Sorted Matrix.py

---
+++ 92_binary_search/0378. Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,26 @@
+def kthSmallest(self, matrix, k):
+    m, n = len(matrix), len(matrix[0])
+    start, end = matrix[0][0], matrix[-1][-1]
+    while start + 1 < end:
+        mid = start + (end - start) // 2
+        count = self.num_of_less_or_equal(matrix, mid)
+        if count < k:
+            start = mid 
+        else:
+            end = mid 
+    
+    if self.num_of_less_or_equal(matrix, start) >= k:
+        return start 
+    return end  
+        
+def num_of_less_or_equal(self, matrix, target):
+    m, n = len(matrix), len(matrix[0])
+    i, j = m-1, 0
+    count = 0
+    while i >= 0 and j < n:
+        if matrix[i][j] <= target:
+            count += i+1 
+            j += 1 
+        else:
+            i -= 1 
+    return count
