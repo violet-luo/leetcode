b58db47fd5af04f52b7f8c7dabbc6dc6b1c45dee
Violet Luo <luo.violeta@gmail.com>
2022-12-26 18:12:19 -0800

    Update 0162. Find Peak Element.py

--- 92_binary_search/0162. Find Peak Element.py
+++ 92_binary_search/0162. Find Peak Element.py
@@ -1,3 +1,20 @@
+def findPeakElement(nums):
+    l, r = 0, len(nums) - 1
+    while l + 1 < r:
+        mid = (l + r) // 2
+        if nums[mid] <= nums[mid + 1]:
+            l = mid 
+        elif nums[mid] > nums[mid + 1]:
+            r = mid
+        else:
+            return mid
+
+    if nums[l] > nums[r]:
+        return l 
+    return r
+
+###
+
 def findPeak(nums):
     l, r = 0, len(nums) - 1
     while l < r:
