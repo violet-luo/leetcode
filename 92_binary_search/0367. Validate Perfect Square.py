c7d14c996fc0ccd8891bbbbead1db347e752910e
Violet Luo <luo.violeta@gmail.com>
2022-12-26 18:25:51 -0800

    Update 0367. Validate Perfect Square.py

--- 92_binary_search/0367. Validate Perfect Square.py
+++ 92_binary_search/0367. Validate Perfect Square.py
@@ -1,5 +1,23 @@
 def isPerfectSquare(num):
     l, r = 0, num
+
+    while l + 1 < r:
+        mid = (l + r) // 2
+        if mid * mid < num:
+            l = mid
+        elif mid * mid > num:
+            r = mid
+        else:
+            return True
+    
+    if l * l == num or r * r == num:
+        return True
+    return False
+
+###
+
+def isPerfectSquare(num):
+    l, r = 0, num
     
     # 如果没有等号, l 和 r 不会相交, 最后一次一定是 m 收敛到 l, 而取不到r
     while l <= r: 
