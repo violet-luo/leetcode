4fcfb1211a0adf0c01443934a7d566f2c285c222
Violet Luo <luo.violeta@gmail.com>
2022-12-28 21:23:16 -0800

    Update 0039. Combination Sum.py

--- 96_backtracking/0039. Combination Sum.py
+++ 96_backtracking/0039. Combination Sum.py
@@ -1,24 +1,23 @@
 def combinationSum(self, candidates, target):
-    res = []
-    subset = []
-    sum = 0
+    res, subset = [], []
+    candidates = sorted(candidates) # 不知道为什么不sort过不了
 
-    def backtrack(candidates, target, sum, start_index):
+    def backtrack(start_index, sum):
         if sum > target:
             return 
         if sum == target:
             return res.append(subset[:])
+        
         for i in range(start_index, len(candidates)):
             if sum + candidates[i] > target:
                 return 
             sum += candidates[i]
             subset.append(candidates[i])
-            backtrack(candidates, target, sum, i)
+            backtrack(i, sum)
             sum -= candidates[i]
             subset.pop()
     
-    candidates = sorted(candidates) # 不知道为什么不sort过不了
-    backtrack(candidates, target, 0, 0)
+    backtrack(0, 0)
     return res
 
 ###
