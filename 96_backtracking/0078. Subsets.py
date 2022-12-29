36168d3c500567d54127097c9731abdccda73d15
Violet Luo <luo.violeta@gmail.com>
2022-12-28 20:50:42 -0800

    Update 0078. Subsets.py

--- 96_backtracking/0078. Subsets.py
+++ 96_backtracking/0078. Subsets.py
@@ -1,15 +1,15 @@
 def subsets(self, nums):
     res, subset = [], []
-
-    def backtrack(nums, start_index):
-        res.append(subset[:]) # 不能是res.append(subset)
+        
+    def backtrack(start_index):
+        res.append(subset[:]) # 硬拷贝不能是res.append(subset)
         # 因为是组合不是集合，所以不是从0而是从start_index开始遍历
         for i in range(start_index, len(nums)):
             subset.append(nums[i])
-            backtrack(nums, i+1)
+            backtrack(i+1)
             subset.pop()
-
-    backtrack(nums, 0)
+            
+    backtrack(0)
     return res
 
 ###
