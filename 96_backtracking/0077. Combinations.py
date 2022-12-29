3690c4fe48015853a49e7241b62d599124aac8f6
Violet Luo <luo.violeta@gmail.com>
2022-12-28 20:54:53 -0800

    Update 0077. Combinations.py

--- 96_backtracking/0077. Combinations.py
+++ 96_backtracking/0077. Combinations.py
@@ -1,16 +1,15 @@
 def combine(n, k):
-    res = []
-    subset = []
+    res, subset = [], []
 
-    def backtrack(n, k, start_index):
+    def backtrack(start_index):
         if len(subset) == k: # 与78的区别
             return res.append(subset[:])
         for i in range(start_index, n - (k - len(subset)) + 2): # 剪枝
-            subset.append(i)
-            backtrack(n, k, i + 1)
+            subset.append(i) # 与78的区别，不需要append所有的子集
+            backtrack(i + 1)
             subset.pop()
             
-    backtrack(n, k, 1)
+    backtrack(1)
     return res 
 
 ###
