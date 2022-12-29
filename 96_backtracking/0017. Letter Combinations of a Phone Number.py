5c72256bd85405034ff35d5497275350f19fcd62
Violet Luo <luo.violeta@gmail.com>
2022-12-28 21:13:37 -0800

    Update 0017. Letter Combinations of a Phone Number.py

--- 96_backtracking/0017. Letter Combinations of a Phone Number.py
+++ 96_backtracking/0017. Letter Combinations of a Phone Number.py
@@ -1,24 +1,22 @@
 map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-
-class Solution:    
+class Solution:
     def letterCombinations(self, digits):
-        char, res = '', []
+        res, subset = [], []
         if not digits:
             return res
 
-        def backtrack(digits, index, char, res):
-            if index == len(digits):
-                res.append(char)
-                return
+        def backtrack(start_index):
+            if start_index == len(digits):
+                return res.append(''.join(subset)) # 要['ad']而不是['a','d']
             
-            letters = map[digits[index]]
+            letters = map[digits[start_index]]
             for letter in letters:
-                char += letter
-                backtrack(digits, index + 1, char, res)
-                char = char[:-1] # 回溯
+                subset.append(letter)
+                backtrack(start_index + 1)
+                subset.pop()
         
-        backtrack(digits, 0, char, res)
-        return res 
+        backtrack(0)
+        return res
 
 ###
 
