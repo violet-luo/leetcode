f6e99f3e7f13b6d7a82c4c19f075cf6e8f8de8b9
Violet Luo <luo.violeta@gmail.com>
2022-12-29 14:24:43 -0800

    Update 0022. Generate Parentheses.py

--- 96_backtracking/0022. Generate Parentheses.py
+++ 96_backtracking/0022. Generate Parentheses.py
@@ -1,4 +1,25 @@
 def generateParenthesis(self, n):
+    res, subset = [], []
+
+    def dfs(left_cnt, right_cnt):
+        if left_cnt == right_cnt == n:
+            res.append(''.join(subset))
+        if left_cnt < n:
+            subset.append('(')
+            dfs(left_cnt + 1, right_cnt)
+            subset.pop()
+        # ["()"]是有效的括号组合，["()",")("]不是，所以左括号个数一定大于等于右括号
+        if right_cnt < left_cnt:
+            subset.append(')')
+            dfs(left_cnt, right_cnt + 1)
+            subset.pop()
+            
+    dfs(0, 0)
+    return res
+
+###
+
+def generateParenthesis(self, n):
     res, subset = [], ''
 
     def dfs(left_count, right_count, subset):
