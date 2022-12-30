e6b34cb982d986b3fbb50eccc4ba8c7cd318e17d
Violet Luo <luo.violeta@gmail.com>
2022-12-29 20:35:04 -0800

    Update 0100. Same Tree.py

--- 93_bfs/0100. Same Tree.py
+++ 93_bfs/0100. Same Tree.py
@@ -6,9 +6,8 @@ def isSameTree(self, p, q):
             continue
         elif not node1 or not node2:
             return False
-        else:
-            if node1.val != node2.val:
-                return False
-            queue.append((node1.right, node2.right))
-            queue.append((node1.left, node2.left))
+        if node1.val != node2.val:
+            return False
+        queue.append((node1.right, node2.right))
+        queue.append((node1.left, node2.left))
     return True
