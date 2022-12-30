780636235767ef0132fde6077109f44516e75f08
Violet Luo <luo.violeta@gmail.com>
2022-12-29 20:32:23 -0800

    Create 0100. Same Tree.py

---
+++ 93_bfs/0100. Same Tree.py
@@ -0,0 +1,14 @@
+def isSameTree(self, p, q):
+    queue = collections.deque([(p, q)])
+    while queue:
+        node1, node2 = queue.popleft()
+        if not node1 and not node2:
+            continue
+        elif not node1 or not node2:
+            return False
+        else:
+            if node1.val != node2.val:
+                return False
+            queue.append((node1.right, node2.right))
+            queue.append((node1.left, node2.left))
+    return True
