6c299b8924ebc54751bc155dfcd849af3bcf5f72
Violet Luo <luo.violeta@gmail.com>
2022-12-30 17:43:57 -0800

    Update 0144. Binary Tree Preorder Traversal.py

--- 95_dfs_iterative/0144. Binary Tree Preorder Traversal.py
+++ 95_dfs_iterative/0144. Binary Tree Preorder Traversal.py
@@ -1,3 +1,22 @@
+def preorderTraversal(self, root): 
+    if not root: 
+        return []
+
+    stack = [root] 
+    res = [] 
+
+    while stack: 
+        node = stack.pop() 
+        res.append(node.val) 
+        if node.right: 
+            stack.append(node.right)
+        if node.left: 
+            stack.append(node.left) 
+
+    return res
+
+###
+
 def preorderTraversal(self, root):
      res, stack = [], []
      node = root 
