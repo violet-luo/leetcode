f8fc7b88fe61852dbf06c55e8471af5d619079be
Violet Luo <luo.violeta@gmail.com>
2022-12-30 18:58:04 -0800

    Update 0094. Binary Tree Inorder Traversal.py

--- 95_dfs_iterative/0094. Binary Tree Inorder Traversal.py
+++ 95_dfs_iterative/0094. Binary Tree Inorder Traversal.py
@@ -1,4 +1,31 @@
 def inorderTraversal(self, root):
+    if not root:
+        return []
+    
+    res = []
+    stack = []
+    
+    # 1. 添加所有最左边节点到栈
+    while root:
+        stack.append(root)
+        root = root.left
+    
+    while stack:
+        # 2. 将左节点加入res
+        node = stack.pop()
+        res.append(node.val)
+        # 3. 如果有右节点，加入stack
+        if node.right:
+            node = node.right
+            while node:
+                stack.append(node)
+                node = node.left
+    
+    return res
+   
+###
+
+def inorderTraversal(self, root):
    res, stack = [], []
    node = root
    while node or stack:
