9361f1aeb841da7160a078b030c119d4cda9337b
Violet Luo <luo.violeta@gmail.com>
2022-12-31 16:24:54 -0800

    Update 0563. Binary Tree Tilt.py

--- 94_dfs_recursive/0563. Binary Tree Tilt.py
+++ 94_dfs_recursive/0563. Binary Tree Tilt.py
@@ -1,4 +1,16 @@
 def findTilt(self, root):
+    if not root:
+        return 0
+    return self.findTilt(root.left) + self.findTilt(root.right) + abs(self.get_sum(root.left) - self.get_sum(root.right))
+
+def get_sum(self, root):
+    if not root: 
+        return 0
+    return self.get_sum(root.left) + self.get_sum(root.right) + root.val
+
+###
+
+def findTilt(self, root):
     res = [0]
     self.dfs(root, res)
     return res[0]
