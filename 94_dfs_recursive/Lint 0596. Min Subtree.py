c084036710c3278265471aaede0e4b483e985442
Violet Luo <luo.violeta@gmail.com>
2022-12-31 17:32:26 -0800

    Update Lint 0596. Min Subtree.py

--- 94_dfs_recursive/Lint 0596. Min Subtree.py
+++ 94_dfs_recursive/Lint 0596. Min Subtree.py
@@ -1,3 +1,24 @@
+def find_subtree(self, root):
+    min_sum = float('inf')
+    res = root
+
+    def get_sum(root):
+        if not root:
+            return 0
+        left = get_sum(root.left)
+        right = get_sum(root.right)
+        sum = root.val + left + right
+        nonlocal min_sum, res
+        if sum < min_sum:
+            min_sum = sum
+            res = root
+        return sum
+
+    get_sum(root)
+    return res
+
+###
+
 def findSubTree(self, root)：
     self.min_sum = float('inf')
     self.res = root
