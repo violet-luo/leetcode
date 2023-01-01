4323ba33a2d35b6420d65a9033163ee328a32917
Violet Luo <luo.violeta@gmail.com>
2023-01-01 15:12:40 -0800

    Update 0530. Min Absolute Diff in BST.py

--- 94_dfs_recursive/0530. Min Absolute Diff in BST.py
+++ 94_dfs_recursive/0530. Min Absolute Diff in BST.py
@@ -1,4 +1,20 @@
 def getMinimumDifference(self, root):
+    def inorder(root):
+        if not root:
+            return
+
+        inorder(root.left) # 先遍历左子树
+        self.min_diff = min(self.min_diff, abs(self.pre - root.val)) # 记录当前节点和前一个节点的差值
+        self.pre = root.val # 当前节点遍历之后就变成了前一个节点
+        inorder(root.right) # 再遍历右子树
+
+    self.min_diff, self.pre = float('inf'), float('inf')
+    inorder(root)
+    return self.min_diff
+
+###
+
+def getMinimumDifference(self, root):
     self.res = float('inf')
     self.pre = None
     self.inorder(root)
