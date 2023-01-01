828f967bc2f00e3c6e719c69a74628f0167de1a7
Violet Luo <luo.violeta@gmail.com>
2022-12-31 17:30:10 -0800

    Update 1120. Max Avg Subtree.py

--- 94_dfs_recursive/1120. Max Avg Subtree.py
+++ 94_dfs_recursive/1120. Max Avg Subtree.py
@@ -1,4 +1,23 @@
 def maximumAverageSubtree(self, root):
+    max_avg = float('-inf')
+
+    def get_sum(root):
+        if not root:
+            return 0, 0
+        l_sum, l_size = get_sum(root.left)
+        r_sum, r_size = get_sum(root.right)
+        sum = l_sum + root.val + r_sum
+        size = l_size + 1 + r_size 
+        nonlocal max_avg
+        max_avg = max(max_avg, sum/size)
+        return sum, size
+
+    get_sum(root)
+    return max_avg
+
+##
+
+def maximumAverageSubtree(self, root):
     self.max_avg = float('-inf')
     self.get_sum(root)
     return self.max_avg
