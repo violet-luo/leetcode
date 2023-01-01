0b921b52bbc5dbcc8522259cb967675374d997a8
Violet Luo <luo.violeta@gmail.com>
2023-01-01 13:12:00 -0800

    Update 0257. Binary Tree Paths.py

--- 94_dfs_recursive/0257. Binary Tree Paths.py
+++ 94_dfs_recursive/0257. Binary Tree Paths.py
@@ -1,4 +1,39 @@
 def binaryTreePaths(self, root):
+    def construct_paths(root, path):
+        if root:
+            path += str(root.val)
+            if not root.left and not root.right:  # 当前节点是叶子节点
+                paths.append(path) 
+            else:
+                path += '->'  # 当前节点不是叶子节点，继续递归遍历
+                construct_paths(root.left, path)
+                construct_paths(root.right, path)
+
+    paths = []
+    construct_paths(root, '')
+    return paths
+
+###
+
+def binaryTreePaths(self, root):
+    if root is None:
+        return []
+        
+    if root.left is None and root.right is None:
+        return [str(root.val)] #递归出口
+
+    leftPaths = self.binaryTreePaths(root.left) #2 #["5"]
+    rightPaths = self.binaryTreePaths(root.right) #3 #None
+    
+    paths = []
+    for path in leftPaths + rightPaths:
+        paths.append(str(root.val) + '->' + path) #["2->5", "3"]
+        
+    return paths
+
+###
+
+def binaryTreePaths(self, root):
     if not root:
         return []
         
@@ -20,21 +55,3 @@ def dfs(self, node, path, res):
         path.append(str(node.right.val))
         self.dfs(node.right, path, res)
         path.pop()
-
-#
-
-def binaryTreePaths(self, root):
-    if root is None:
-        return []
-        
-    if root.left is None and root.right is None:
-        return [str(root.val)] #递归出口
-
-    leftPaths = self.binaryTreePaths(root.left) #2 #["5"]
-    rightPaths = self.binaryTreePaths(root.right) #3 #None
-    
-    paths = []
-    for path in leftPaths + rightPaths:
-        paths.append(str(root.val) + '->' + path) #["2->5", "3"]
-        
-    return paths
