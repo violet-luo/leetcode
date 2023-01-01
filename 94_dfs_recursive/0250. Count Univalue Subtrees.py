e1ef074e7349544e82025adae2d84d57ce913560
Violet Luo <luo.violeta@gmail.com>
2022-12-31 17:28:01 -0800

    Update 0250. Count Univalue Subtrees.py

--- 94_dfs_recursive/0250. Count Univalue Subtrees.py
+++ 94_dfs_recursive/0250. Count Univalue Subtrees.py
@@ -1,4 +1,24 @@
 def countUnivalSubtrees(self, root):
+    count = 0
+
+    def dfs(root):
+        if not root:
+            return True
+        l = dfs(root.left)
+        r = dfs(root.right)
+        # if both children are "True" and root.val is equal to both children's values if exist
+        if l and r and (not root.left or root.left.val == root.val) and (not root.right or root.right.val == root.val):
+            nonlocal count
+            count += 1
+            return True
+        return False
+
+    dfs(root)
+    return count
+
+###
+
+def countUnivalSubtrees(self, root):
     self.count = 0
     self.dfs(root)
     return self.count
