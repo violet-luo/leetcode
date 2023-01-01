7ac4bc96daf4e7ce09fff0452f76ac46ada6a194
Violet Luo <luo.violeta@gmail.com>
2023-01-01 13:01:15 -0800

    Update 0366. Find Leaves of Binary Tree.py

--- 94_dfs_recursive/0366. Find Leaves of Binary Tree.py
+++ 94_dfs_recursive/0366. Find Leaves of Binary Tree.py
@@ -1,4 +1,24 @@
 def findLeaves(self, root):
+    res = collections.defaultdict(list)
+
+    # 自底向上递归
+    def getHeight(root):
+        if not root:
+            return 0
+
+        left_height = getHeight(root.left)
+        right_height = getHeight(root.right)
+        height = max(left_height, right_height) + 1
+
+        res[height].append(root.val)
+        return height
+
+    getHeight(root)
+    return [v for v in res.values()]
+
+###
+
+def findLeaves(self, root):
     res = []
     self.depth = {}
     max_depth = self.getHeight(root)
