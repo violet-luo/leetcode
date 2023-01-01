c517edd6bdce64a33c4e152083037356e507bc16
Violet Luo <luo.violeta@gmail.com>
2022-12-31 20:34:34 -0800

    Update 0671. 2nd Min Node in a Binary Tree.py

--- 94_dfs_recursive/0671. 2nd Min Node in a Binary Tree.py
+++ 94_dfs_recursive/0671. 2nd Min Node in a Binary Tree.py
@@ -1,4 +1,24 @@
 def findSecondMinimumValue(self, root):
+    if not root or not root.left or not root.right:
+        return -1
+    # 左右子节点中不同于 root.val 的那个值可能是第二小的值
+    l, r = root.left.val, root.right.val
+    
+    # 如果左右子节点都等于 root.val，则去左右子树递归寻找第二小的值
+    if root.val == root.left.val:
+        l = self.findSecondMinimumValue(root.left)
+    if root.val == root.right.val:
+        r = self.findSecondMinimumValue(root.right)
+    if l == -1:
+        return r
+    if r == -1:
+        return l
+    # 如果左右子树都找到一个第二小的值，更小的那个是整棵树的第二小元素
+    return min(l, r)
+
+###
+
+def findSecondMinimumValue(self, root):
     res = [float('inf')]
     def traverse(node):
         if not node:
