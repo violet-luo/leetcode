8f9898edb39a997083270aefe6e57b9e6f9a7bec
Violet Luo <luo.violeta@gmail.com>
2023-01-01 14:41:08 -0800

    Update 0501. Find Mode in BST.py

--- 94_dfs_recursive/0501. Find Mode in BST.py
+++ 94_dfs_recursive/0501. Find Mode in BST.py
@@ -1,5 +1,5 @@
 def findMode(self, root):
-    res = {}
+    res = collections.defaultdict(int)
     self.dfs(root, res)
     mode = max(res.values())
     return [key for key in res.keys() if res[key] == mode]
@@ -7,9 +7,6 @@ def findMode(self, root):
 def dfs(self, root, res):
     if not root:
         return
-    if root.val not in res:
-        res[root.val] = 1
-    else:
-        res[root.val] += 1
+    res[root.val] += 1
     self.dfs(root.left, res)
     self.dfs(root.right, res)
