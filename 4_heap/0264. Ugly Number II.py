8bd3493bda7a2bf9713298ea810bf36f1015f6e7
Violet Luo <luo.violeta@gmail.com>
2023-01-03 14:53:11 -0800

    Update 0264. Ugly Number II.py

--- 4_heap/0264. Ugly Number II.py
+++ 4_heap/0264. Ugly Number II.py
@@ -1,16 +1,13 @@
-import heapq
+def nthUglyNumber(self, n):
+    import heapq
+    heap = [1]
+    visited = set([1])
+    
+    for _ in range(n):
+        min_ugly_num = heapq.heappop(heap) # 弹出当前最小丑数
+        for factor in [2, 3, 5]:
+            if min_ugly_num * factor not in visited:
+                visited.add(min_ugly_num * factor)
+                heapq.heappush(heap, min_ugly_num * factor) # heapppush(heap, ele)
 
-class Solution:
-    def nthUglyNumber(self, n):
-        heap = [1]
-        visited = set([1])
-        val = None
-        
-        for _ in range(n):
-            val = heapq.heappop(heap) # 弹出当前最小丑数
-            for factor in [2, 3, 5]:
-                if val * factor not in visited:
-                    visited.add(val * factor)
-                    heapq.heappush(heap, val * factor) # heapppush(heap, ele)
-
-        return val
+    return min_ugly_num
