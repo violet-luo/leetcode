ac76d3a0f296a9ba79d5ce167db83486a77671b9
Violet Luo <luo.violeta@gmail.com>
2023-01-03 14:04:24 -0800

    Update 1209. Remove All Adjacent Duplicates in String II.py

--- 2_stack/1209. Remove All Adjacent Duplicates in String II.py
+++ 2_stack/1209. Remove All Adjacent Duplicates in String II.py
@@ -1,10 +1,10 @@
 def removeDuplicates(self, s, k):
     res = []  # pair of [char, freq]
-    for char in s: # [d, 1]
-        if res and res[-1][0] == char:
+    for char in s: 
+        if res and res[-1][0] == char: # [['d',1], ['e',3]]
             res[-1][1] += 1
         else:
-            res.append([char, 1])
-        if res[-1][1] == k:
+            res.append([char, 1]) # [['d',1]]
+        if res[-1][1] == k: # ['e',3]
             res.pop()
     return "".join(char * cnt for char, cnt in res) # ['a', 2]
