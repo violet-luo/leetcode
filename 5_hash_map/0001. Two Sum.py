8be134771cb436feb536b7ae8f77a74379437d7d
Violet Luo <luo.violeta@gmail.com>
2023-01-02 21:08:26 -0800

    Update 0001. Two Sum.py

--- 5_hash_map/0001. Two Sum.py
+++ 5_hash_map/0001. Two Sum.py
@@ -1,12 +1,10 @@
-def twoSum(self, numbers, target):
-    # key: value, value: index
-    hash = {}
+def twoSum(self, nums, target):
+    num_to_idx = {}
 
-    for i in range(len(numbers)):
-        if target - numbers[i] in hash:
-            # return the first pair of indices
-            return [hash[target - numbers[i]], i] 
+    for i in range(len(nums)):
+        if target - nums[i] in num_to_idx:
+            return [num_to_idx[target - nums[i]], i] 
         else:
-            hash[numbers[i]] = i
-
+            num_to_idx[nums[i]] = i
+ 
     return [-1, -1]
