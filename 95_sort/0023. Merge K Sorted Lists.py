811bff7cac2225ce37e1e4a1095ca255ca3f4a29
Violet Luo <luo.violeta@gmail.com>
2023-01-03 13:38:23 -0800

    Update 0023. Merge K Sorted Lists.py

--- 95_sort/0023. Merge K Sorted Lists.py
+++ 95_sort/0023. Merge K Sorted Lists.py
@@ -1,29 +1,30 @@
-def mergeKLists(self, lists):
-    if lists == []:
-        return None
-    if len(lists) == 1:
-        return lists[0]
-
-    mid = len(lists) // 2
-    left = self.mergeKLists(lists[:mid])
-    right = self.mergeKLists(lists[mid:])
-    return self.merge(left, right)
+def mergekSortedArrays(self, arrays):
+    return self.merge_range_arrays(arrays, 0, len(arrays) - 1)
     
-def merge(self, left, right):
-    dummy = ListNode(None)
-    h = dummy
+# 标准归排
+def merge_range_arrays(self, arrays, start, end):
+    if start == end:
+        return arrays[start]
+    
+    mid = (start + end) // 2
+    left = self.merge_range_arrays(arrays, start, mid)
+    right = self.merge_range_arrays(arrays, mid + 1, end)
+    return self.merge_two_arrays(left, right)
 
-    while left and right:
-        if left.val < right.val:
-            h.next = left
-            left = left.next
+def merge_two_arrays(self, arr1, arr2):
+    i, j = 0, 0
+    array = []
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            array.append(arr1[i])
+            i += 1
         else:
-            h.next = right
-            right = right.next
-        h = h.next
-        
-    if left:
-        h.next = left
-    if right:
-        h.next = right
-    return dummy.next
+            array.append(arr2[j])
+            j += 1
+    while i < len(arr1):
+        array.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        array.append(arr2[j])
+        j += 1
+    return array
