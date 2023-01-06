8fe6bf8b83fcfca4ec0a084e4ceeceeef3d13bc6
Violet Luo <luo.violeta@gmail.com>
2023-01-05 18:44:43 -0800

    Update 0001. Two Sum.py

--- 5_hash_map/0001. Two Sum.py
+++ 5_hash_map/0001. Two Sum.py
@@ -1,10 +1,8 @@
 def twoSum(self, nums, target):
-    num_to_idx = {}
+    num_to_idx = {} # 不用sort
 
-    for i in range(len(nums)):
+    for i in range(len(nums)): # 不用for num in nums 因为最后return idx
         if target - nums[i] in num_to_idx:
-            return [num_to_idx[target - nums[i]], i] 
+            return [num_to_idx[target - nums[i]], i] # [i, num_to_idx[target - nums[i]] 也可以
         else:
             num_to_idx[nums[i]] = i
- 
-    return [-1, -1]
