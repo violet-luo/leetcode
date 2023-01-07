58c7aeeaefcc6b80677f259a2ee852a94831b4db
Violet Luo <luo.violeta@gmail.com>
2023-01-07 13:22:18 -0800

    Update 0026. Remove Duplicate Numbers in Array.py

--- 91_2p/0026. Remove Duplicate Numbers in Array.py
+++ 91_2p/0026. Remove Duplicate Numbers in Array.py
@@ -1,3 +1,17 @@
+def removeDuplicates(self, nums: List[int]) -> int: # [1,2,2,3]
+    slow, fast = 0, 0
+
+    while fast < len(nums): # 走到len(nums) - 1, 即3
+        if nums[slow] == nums[fast]:
+            fast += 1
+        else: # 不要用while
+            nums[slow + 1] = nums[fast] # slow = 2, [1,2,3]
+            slow += 1 # slow = 2
+            fast += 1 # fast = 3
+    return slow + 1
+
+###
+
 def removeDuplicates(nums):
     slow = 0
 
@@ -5,18 +19,4 @@ def removeDuplicates(nums):
         if nums[fast] != nums[slow]:
             slow += 1
             nums[slow] = nums[fast]
-    return slow + 1 # slow是index, 所以数量要+1
-
-###
-
-def removeDuplicates(nums):
-    slow, fast = 0, 1
-
-    while fast < len(nums):
-        if nums[fast] != nums[slow]:
-            nums[slow + 1] = nums[fast]
-            slow += 1
-            fast += 1
-        else:
-            fast += 1
     return slow + 1 # slow是index, 所以数量要+1
