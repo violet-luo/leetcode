fede33590585aef5aa6bf5b61385efcea99211f8
Violet Luo <luo.violeta@gmail.com>
2023-01-07 15:38:47 -0800

    Update 0122. Best Time to Buy and Sell Stock II.py

--- 7_2_greedy/0122. Best Time to Buy and Sell Stock II.py
+++ 7_2_greedy/0122. Best Time to Buy and Sell Stock II.py
@@ -1,5 +1,16 @@
 def maxProfit(self, prices):
     profit = 0
+
+    for i in range(len(prices) - 1):
+        if prices[i] < prices[i + 1]:
+            profit += prices[i + 1] - prices[i]
+
+    return profit
+    
+###
+
+def maxProfit(self, prices):
+    profit = 0
     for i in range(1, len(prices)):
         if prices[i] - prices[i - 1] > 0:
             profit += prices[i] - prices[i - 1]
