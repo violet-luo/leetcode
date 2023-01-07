18afcadfa6f68ac1d07dee5880b80c22c64ff12a
Violet Luo <luo.violeta@gmail.com>
2023-01-06 17:05:21 -0800

    Update 0657. Robot Return to Origin.py

--- 8_string/0657. Robot Return to Origin.py
+++ 8_string/0657. Robot Return to Origin.py
@@ -1,3 +1,15 @@
+def judgeCircle(moves):
+    dic = {'U':(0,1), 'D':(0,-1), 'L':(-1,0), 'R':(1,0)}
+    x, y = 0, 0
+
+    for move in moves:
+        x += dic[move][0]
+        y += dic[move][1]
+
+    return (x, y) == (0,0)
+
+###
+
 def judgeCircle(self, moves):
     x = y = 0
     for move in moves:
