b1e607ea939d78cb9ea4bfbd6d8610633e3988c0
Violet Luo <luo.violeta@gmail.com>
2023-01-07 14:49:02 -0800

    Create 0860. Lemonade Change.py

---
+++ 7_2_greedy/0860. Lemonade Change.py
@@ -0,0 +1,25 @@
+def lemonadeChange(self, bills):
+    five, ten, twenty = 0, 0, 0
+
+    for bill in bills:
+        if bill == 5:
+            five += 1
+        if bill == 10:
+            if five == 0:
+                return False
+            ten += 1
+            five -= 1
+        if bill == 20:
+            # 1. 10 + 5
+            if ten > 0:
+                if five == 0:
+                    return False
+                ten -= 1
+                five -= 1
+            # 2. 5 + 5 + 5
+            else:
+                if five < 3:
+                    return False
+                five -= 3
+
+    return True
