e5cb7c2799dad28ec21f1aefba7601b2ea9877c2
Violet Luo <luo.violeta@gmail.com>
2023-01-07 15:14:30 -0800

    Update 1086. High Five.py

--- 4_heap/1086. High Five.py
+++ 4_heap/1086. High Five.py
@@ -1,17 +1,15 @@
-from collections import defaultdict
-from heapq import heappush, heappop
-
-class Solution:
-    def highFive(self, records):
-        scores = defaultdict(list)
-
-        for record in records:
-            heappush(scores[record.id], record.score)
-            if (len(scores[record.id])) > 5:
-                heappop(scores[record.id])
-
-        scores_avg = {}
-        for id in scores:
-            scores_avg[id] = sum(scores[id]) / 5.0
-            
-        return scores_avg
+def highFive(self, items):
+    dic = collections.defaultdict(list)
+    import heapq 
+    for item in items:
+        heapq.heappush(dic[item[0]], item[1])
+        if len(dic[item[0]]) > 5:
+            heapq.heappop(dic[item[0]])
+    
+    res = []
+    for id, score_list in dic.items():
+        avg = sum(score_list) // len(score_list)
+        res.append([id, avg])
+    
+    res.sort(key = lambda x: x[0])
+    return res
