a887b94d50f27959995b24622ff06cedae576bb2
Violet Luo <luo.violeta@gmail.com>
2023-01-07 19:33:29 -0800

    Update 0905. Partition Array by Odd and Even.py

--- 91_2p/0905. Partition Array by Odd and Even.py
+++ 91_2p/0905. Partition Array by Odd and Even.py
@@ -1,3 +1,18 @@
+def sortArrayByParity(self, nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        while left < right and nums[left] % 2 == 0:
+            left += 1
+        while left < right and nums[right] % 2 == 1:
+            right -= 1
+        if left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+    return nums
+
+###
+
 def partitionArray(self, nums):
     l, r = 0, len(nums) - 1
     while l <= r:
