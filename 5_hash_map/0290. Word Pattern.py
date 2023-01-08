01b76039d47c14543f7012a4006bd2b7f41f300a
Violet Luo <luo.violeta@gmail.com>
2023-01-07 16:21:02 -0800

    Update 0290. Word Pattern.py

--- 5_hash_map/0290. Word Pattern.py
+++ 5_hash_map/0290. Word Pattern.py
@@ -1,3 +1,23 @@
+def wordPattern(self, pattern, s):
+    s = s.split(' ')
+    if len(pattern) != len(s):
+        return False
+
+    p_to_s = collections.defaultdict(str)
+    s_to_p = collections.defaultdict(str)
+
+    for i in range(len(pattern)):
+        if pattern[i] not in p_to_s:
+            p_to_s[pattern[i]] = s[i]
+        if s[i] not in s_to_p:
+            s_to_p[s[i]] = pattern[i]
+        if p_to_s[pattern[i]] != s[i] or s_to_p[s[i]] != pattern[i]:
+            return False
+    
+    return True
+
+###
+
 def wordPattern(self, s, t):
     dic = {}
     t = t.split()
