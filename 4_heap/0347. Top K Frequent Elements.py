efcc767743e1c71f050446f425c7f306c2cef593
Violet Luo <luo.violeta@gmail.com>
2023-01-07 18:53:54 -0800

    Update 0347. Top K Frequent Elements.py

--- 4_heap/0347. Top K Frequent Elements.py
+++ 4_heap/0347. Top K Frequent Elements.py
@@ -1,16 +1,13 @@
 def topKFrequent(self, nums, k):
-    counts = {}
+    counter = collections.Counter(nums)
+    import heapq
+    heap, res = [], []
 
-    for num in nums:
-        counts[num] = counts.get(num, 0) + 1
-
-    heap = []
-    for item in counts.items():
-        heapq.heappush(heap, (-item[1], item[0]))
-
-    results = []
+    for num, cnt in counter.items():
+        heapq.heappush(heap, [-cnt, num])
+    
     for i in range(k):
-        count, value = heapq.heappop(heap)
-        results.append(value)
+        cnt, num = heapq.heappop(heap)
+        res.append(num)
 
-    return results
+    return res
