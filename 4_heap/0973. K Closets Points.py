14bd4822bb582d3b5f582137ed24a871f8bdce4c
Violet Luo <luo.violeta@gmail.com>
2023-01-07 17:34:16 -0800

    Update 0973. K Closets Points.py

--- 4_heap/0973. K Closets Points.py
+++ 4_heap/0973. K Closets Points.py
@@ -1,13 +1,14 @@
 def kClosest(points, k):
-    heap = [] #最小栈，栈顶放最近点
-    res = []
+    heap, res = [], [] #最小栈，栈顶放最近点
 
-    for point in points:
-        dist = point[0] ** 2 + point[1] ** 2
-        heapq.heappush(heap, (dist, point[0], point[1])) # dist/x/y小的优先
+    for x, y in points:
+        heapq.heappush(heap, [x ** 2 + y ** 2, x, y]) # dist/x/y小的优先
 
     for _ in range(k):
         _, x, y = heapq.heappop(heap)
         res.append([x, y])
 
+    # 不可以是以下，因为其实heap并没有排序，要到push时才排序
+    # for i in range(k):
+        # res.append([heap[i][1], heap[i][2]])
     return res
