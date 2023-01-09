fb710ff82333fe55b06dd8146ce8c717afe34f45
Violet Luo <luo.violeta@gmail.com>
2023-01-09 15:59:04 -0800

    Update 0113. Path Sum II.py

--- 94_1_dfs_recursive/0113. Path Sum II.py
+++ 94_1_dfs_recursive/0113. Path Sum II.py
@@ -1,40 +1,13 @@
-def pathSum(self, root, sum):
-    def get_path_sum(root, cur_sum, sum):
-        if not root:
-            return 
-        if not root.left and not root.right and sum - root.val == 0:
-            cur_sum += [root.val]
-            res.append(cur_sum)
-        get_path_sum(root.left, cur_sum + [root.val], sum - root.val)
-        get_path_sum(root.right, cur_sum + [root.val], sum - root.val)
-
-    res = []
-    get_path_sum(root, [], sum)
-    return res
-
-###
-
 def pathSum(self, root, targetSum):
     res = []
-    path = [root.val]
-    self.dfs(root, 0, targetSum, path, res)
+
+    def get_path_sum(root, targetSum, path):
+        if not root:
+            return
+        if not root.left and not root.right and root.val == targetSum:
+            res.append(path + [root.val])
+        get_path_sum(root.left, targetSum - root.val, path + [root.val])
+        get_path_sum(root.right, targetSum - root.val, path + [root.val])
+    
+    get_path_sum(root, targetSum, [])
     return res
-
-def dfs(self, node, currSum, targetSum, path, res)
-    if not root:
-        return
-
-    currSum += node.val
-    if not node.left and not node.right and currSum == targetSum: #出口
-        res.append(list(path))
-        return 
-
-    if node.left:
-        path.append(node.left.val)
-        self.dfs(node.left, currSum, targetSum, path, res)
-        path.pop()
-
-    if node.right:
-        path.append(node.right.val)
-        self.dfs(node.right, currSum, targetSum, path, res)
-        path.pop()    
