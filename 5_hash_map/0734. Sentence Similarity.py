28d9f102bfd0934bdc9e6e3149bebffb6f7df927
Violet Luo <luo.violeta@gmail.com>
2023-01-08 17:53:06 -0800

    Update 0734. Sentence Similarity.py

--- 5_hash_map/0734. Sentence Similarity.py
+++ 5_hash_map/0734. Sentence Similarity.py
@@ -1,3 +1,20 @@
+def areSentencesSimilar(self, sentence1, sentence2, similarPairs):
+    if len(sentence1) != len(sentence2):
+        return False
+    
+    dic = collections.defaultdict(set)
+    for p in similarPairs:
+        dic[p[0]].add(p[1])
+        dic[p[1]].add(p[0])
+    
+    for i in range(len(sentence1)):
+        w1, w2 = sentence1[i], sentence2[i]
+        if w1 != w2 and w2 not in dic[w1]:
+            return False
+    return True
+
+##
+
 def areSentencesSimilar(sentence1, sentence2, similarPairs):
     if len(sentence1) != len(sentence2):
         return False
