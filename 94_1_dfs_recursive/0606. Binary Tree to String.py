3c00033fa990f81744ae4a19bcfa1958cb76cc4d
Violet Luo <luo.violeta@gmail.com>
2023-01-09 15:22:42 -0800

    Update 0606. Binary Tree to String.py

--- 94_1_dfs_recursive/0606. Binary Tree to String.py
+++ 94_1_dfs_recursive/0606. Binary Tree to String.py
@@ -1,8 +1,11 @@
 def tree2str(self, root):
     if not root:
-        return ""
+        return ''
+    # 4
     if not root.left and not root.right:
         return str(root.val)
+    # 2(4)
     if not root.right:
-        return str(root.val) + "(" + self.tree2str(root.left) + ")"
-    return str(root.val) + "(" + self.tree2str(root.left) + ")(" + self.tree2str(root.right) + ")"
+        return str(root.val) + '(' + self.tree2str(root.left) + ')'
+    # 1(2)(3)
+    return str(root.val) + '(' + self.tree2str(root.left) + ')(' + self.tree2str(root.right) + ')'
