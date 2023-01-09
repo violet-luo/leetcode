9387fff0610533f312109db7a7342f96af55d017
Violet Luo <luo.violeta@gmail.com>
2023-01-09 15:32:12 -0800

    Update 0111. Min Depth of Binary Tree.py

--- 94_1_dfs_recursive/0111. Min Depth of Binary Tree.py
+++ 94_1_dfs_recursive/0111. Min Depth of Binary Tree.py
@@ -1,3 +1,15 @@
+def minDepth(self, root):
+    if not root:
+        return 0
+    leftDepth = self.minDepth(root.left)
+    rightDepth = self.minDepth(root.right)
+    if root.left and root.right:
+        return min(leftDepth, rightDepth) + 1
+    else:
+        return max(leftDepth, rightDepth) + 1
+    
+###
+
 def minDepth(self, root: Optional[TreeNode]) -> int:
     if not root:
         return 0
