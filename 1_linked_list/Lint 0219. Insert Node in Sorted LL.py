f8a95ba22d2e8a251d31f6892e3e8dd7be1618f8
Violet Luo <luo.violeta@gmail.com>
2023-01-08 22:50:03 -0800

    Update Lint 0219. Insert Node in Sorted LL.py

--- 1_linked_list/Lint 0219. Insert Node in Sorted LL.py
+++ 1_linked_list/Lint 0219. Insert Node in Sorted LL.py
@@ -1,11 +1,11 @@
 def insertNode(self, head, val):
-    dummy = ListNode(0)
-    dummy.next = head
+    dummy = ListNode(0, head)
     cur = dummy
 
-    # 行进到插入点
+    # 行进到插入点，cur.next刚刚大于val
     while cur.next and cur.next.val < val:
         cur = cur.next
+        
     # 先定义node, 再插入
     node = ListNode(val, cur.next)
     cur.next = node
