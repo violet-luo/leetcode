f46979aca7f5080c73e169519a583d63dd49cc74
Violet Luo <luo.violeta@gmail.com>
2023-01-09 17:32:58 -0800

    Update 0129. Sum Root to Leaf Numbers.py

--- 94_1_dfs_recursive/0129. Sum Root to Leaf Numbers.py
+++ 94_1_dfs_recursive/0129. Sum Root to Leaf Numbers.py
@@ -1,4 +1,17 @@
 def sumNumbers(self, root):
+    if not root:
+        return 0
+    if not root.left and not root.right:
+        return root.val
+    if root.left:
+        root.left.val = root.val * 10 + root.left.val
+    if root.right:
+        root.right.val = root.val * 10 + root.right.val
+    return self.sumNumbers(root.left) + self.sumNumbers(root.right)
+
+###
+
+def sumNumbers(self, root):
     return self.helper(root, 0)
 
 # 因为需要上一个节点的信息，所以需要重新建立一个helper
