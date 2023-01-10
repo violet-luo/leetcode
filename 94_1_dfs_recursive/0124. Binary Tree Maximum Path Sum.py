0ac012241b6ddb74f69d3f46555df0058ec748f2
Violet Luo <luo.violeta@gmail.com>
2023-01-09 16:25:57 -0800

    Update 0124. Binary Tree Maximum Path Sum.py

--- 94_1_dfs_recursive/0124. Binary Tree Maximum Path Sum.py
+++ 94_1_dfs_recursive/0124. Binary Tree Maximum Path Sum.py
@@ -1,4 +1,22 @@
 def maxPathSum(self, root):
+    max_sum = float('-inf')
+    
+    def dfs(root):
+        if not root: 
+            return 0
+        
+        left = dfs(root.left)
+        right = dfs(root.right)
+        nonlocal max_sum
+        max_sum = max(max_sum, root.val, root.val + left, root.val + right, root.val + left + right)
+        return max(root.val, root.val + left, root.val + right)
+
+    dfs(root)
+    return max_sum 
+
+###
+
+def maxPathSum(self, root):
     self.max_sum = float('-inf')
     self.helper(root)
     return self.max_sum 
