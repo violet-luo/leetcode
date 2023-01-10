b0fcfcee3f736eeebe6c44223e833366cdb9135d
Violet Luo <luo.violeta@gmail.com>
2023-01-09 23:18:52 -0800

    Update 0257. Binary Tree Paths.py

--- 94_1_dfs_recursive/0257. Binary Tree Paths.py
+++ 94_1_dfs_recursive/0257. Binary Tree Paths.py
@@ -1,18 +1,19 @@
 def binaryTreePaths(self, root):
-    def construct_paths(root, path):
-        if root:
-            path += str(root.val)
-            if not root.left and not root.right:  # 当前节点是叶子节点
-                paths.append(path) 
-            else:
-                path += '->'  # 当前节点不是叶子节点，继续递归遍历
-                construct_paths(root.left, path)
-                construct_paths(root.right, path)
-
     paths = []
-    construct_paths(root, '')
-    return paths
 
+    def get_path(root, path):
+        if not root:
+            return
+        path += str(root.val)
+        if not root.left and not root.right: # 当前节点是叶子节点
+            paths.append(path)
+        else:
+            path += '->' # 当前节点不是叶子节点，继续递归遍历
+            get_path(root.left, path)
+            get_path(root.right, path)
+    
+    get_path(root, '')
+    return paths
 ###
 
 def binaryTreePaths(self, root):
