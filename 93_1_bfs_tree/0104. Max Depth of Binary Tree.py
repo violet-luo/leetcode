8f29de81b2720928d5b5ba7abc280c6548109e55
Violet Luo <luo.violeta@gmail.com>
2023-01-10 19:30:47 -0800

    Create 0104. Max Depth of Binary Tree.py

---
+++ 93_1_bfs_tree/0104. Max Depth of Binary Tree.py
@@ -0,0 +1,17 @@
+def maxDepth(self, root):
+    if not root:
+        return 0
+    queue = collections.deque([(root, 1)])
+    res = 0
+
+    while queue:
+        n = len(queue)
+        for i in range(n):
+            node, level = queue.popleft()
+            if node.left:
+                queue.append((node.left, level + 1))
+            if node.right:
+                queue.append((node.right, level + 1))
+        res += 1
+        
+    return res
