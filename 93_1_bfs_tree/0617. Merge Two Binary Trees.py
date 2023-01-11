50b40e5c423abf5452b77e462b0a01e4f3dffe4b
Violet Luo <luo.violeta@gmail.com>
2023-01-10 19:49:33 -0800

    Update 0617. Merge Two Binary Trees.py

--- 93_1_bfs_tree/0617. Merge Two Binary Trees.py
+++ 93_1_bfs_tree/0617. Merge Two Binary Trees.py
@@ -6,16 +6,16 @@ def mergeTrees(self, root1, root2):
 
     queue = collections.deque([(root1,root2)])
     while queue:
-        r1,r2 = queue.popleft()
-        r1.val += r2.val
-        # 如果r1和r2的左子树都不为空，就放到队列中
-        # 如果r1的左子树为空，就把r2的左子树挂到r1的左子树上
-        if r1.left and r2.left:
-            queue.append((r1.left,r2.left))
-        elif not r1.left:
-            r1.left = r2.left
-        if r1.right and r2.right:
-            queue.append((r1.right,r2.right))
-        elif not r1.right:
-            r1.right = r2.right
+        l, r = queue.popleft()
+        l.val += r.val
+        
+        # 顺序可变    
+        if l.left and r.left: # 如果l和r的左子树都不为空，就放到队列中
+            queue.append((l.left, r.left))
+        if l.right and r.right:
+            queue.append((l.right, r.right))
+        if not l.left: # 如果l的左子树为空，就把r的左子树挂到l的左子树上
+            l.left = r.left
+        if not l.right:
+            l.right = r.right
     return root1
