e366362f1c41c8578393cbb6d8c0929e9ad21758
Violet Luo <luo.violeta@gmail.com>
2023-01-10 19:20:11 -0800

    Update 0111. Min Depth of Binary Tree.py

--- 93_1_bfs_tree/0111. Min Depth of Binary Tree.py
+++ 93_1_bfs_tree/0111. Min Depth of Binary Tree.py
@@ -1,12 +1,13 @@
 def minDepth(self, root):
     if not root:
         return 0
+        
     queue = collections.deque([(root, 1)])
     while queue:
         node, level = queue.popleft()
-        if node:
-            if not node.left and not node.right:
-                return level
-            else:
-                queue.append((node.left, level+1))
-                queue.append((node.right, level+1))
+        if not node.left and not node.right: # 走到第一个叶子节点
+            return level
+        if node.left:
+            queue.append((node.left, level + 1))
+        if node.right:
+            queue.append((node.right, level + 1))
