7ff69c47c191f91a8f454bc305acc557a83a8f1f
Violet Luo <luo.violeta@gmail.com>
2023-02-04 11:18:32 -0800

    Update 0186. Reverse Words in a String II.py

--- 91_2p/0186. Reverse Words in a String II.py
+++ 91_2p/0186. Reverse Words in a String II.py
@@ -1,15 +1,17 @@
-def reverseWords(self, s):
-    self.reverse(s, 0, len(s) - 1)
+def reverseWords(s):
+    reverse(s, 0, len(s) - 1) # [eulb si yks eht]
 
     first_idx = 0 # first index of the current word
     for cur_idx in range(len(s)):
-        if s[cur_idx] == ' ': # reverse word
-            self.reverse(s, first_idx, cur_idx - 1)
+        # reverse words but the last
+        if s[cur_idx] == ' ':
+            reverse(s, first_idx, cur_idx - 1)
             first_idx = cur_idx + 1
-        elif cur_idx == len(s) - 1: # reverse the last word, cause there's no space after 
-            self.reverse(s, first_idx, cur_idx)
+        # revesrse the last word
+        elif cur_idx == len(s) - 1: 
+            reverse(s, first_idx, cur_idx)
 
-def reverse(self, s, l, r):
+def reverse(s, l, r):
     while l < r:
         s[l], s[r] = s[r], s[l]
         l += 1
