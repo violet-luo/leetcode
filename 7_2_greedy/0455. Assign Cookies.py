0d048a23c888509ed807f429d3b8db63ad822341
Violet Luo <luo.violeta@gmail.com>
2023-02-04 20:19:53 -0800

    Update 0455. Assign Cookies.py

--- 7_2_greedy/0455. Assign Cookies.py
+++ 7_2_greedy/0455. Assign Cookies.py
@@ -1,23 +1,15 @@
-def findContentChildren(self, g: List[int], s: List[int]) -> int:
+def findContentChildren(self, g, s):
     g.sort()
     s.sort()
-    cnt = 0
-    
-    for i in range(len(s)):
-        if g[cnt] <= s[i]:
-            cnt += 1
-        if cnt >= len(g):
-            break
-    return cnt 
-  
-  ###
-  
-  def findContentChildren(self, g, s):
-    g.sort()
-    s.sort()
-    cnt = 0
+    m, n = len(g), len(s)
+    g_idx, s_idx, count = 0, 0, 0
 
-    for i in range(len(s)):
-        if cnt < len(g) and s[i] >= g[cnt]:
-            cnt += 1
-    return cnt
+    while g_idx < m and s_idx < n:
+        if s[s_idx] >= g[g_idx]: # 当前饼干可以满足孩子
+            count += 1
+            g_idx += 1
+            s_idx += 1
+        else:
+            s_idx += 1
+    
+    return count
