ab421c6320a1dc98771962ffdbec6176e925d3ce
Violet Luo <luo.violeta@gmail.com>
2023-02-05 10:47:23 -0800

    Update 0047. Permutations II.py

--- 94_3_backtracking/0047. Permutations II.py
+++ 94_3_backtracking/0047. Permutations II.py
@@ -1,4 +1,28 @@
 def permuteUnique(self, nums):
+    res, subset = [], []
+    nums.sort()
+    seen = [False] * len(nums)
+
+    def backtrack(nums, seen, res, subset):
+        if len(subset) == len(nums):
+            return res.append(subset[:])
+            return
+
+        for i in range(len(nums)):
+            if seen[i] or (i > 0 and nums[i] == nums[i-1] and not seen[i-1]): # 元素已访问过或是重复元素
+                continue
+            seen[i] = True
+            subset.append(nums[i])
+            backtrack(nums, seen, res, subset)
+            subset.pop()
+            seen[i] = 0
+
+    backtrack(nums, seen, res, subset)
+    return res
+
+###
+
+def permuteUnique(self, nums):
     if not nums: 
         return []
     res, subset = [], []
