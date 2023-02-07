9e4b3119a79f9f30115a9abd9de80373e925c9d6
Violet Luo <luo.violeta@gmail.com>
2023-02-07 10:12:14 -0800

    Update 0055. Jump Game.py

--- 96_dp/0055. Jump Game.py
+++ 96_dp/0055. Jump Game.py
@@ -1,23 +1,13 @@
 def canJump(A):
-    if not A:
-        return False
-
     n = len(A)
-    # 1. state: dp[i] 代表能否跳到坐标 i
-    dp = [False] * n 
-
-    # 2. 初始化：一开始站在0
+    dp = [False] * n # dp[i] 代表能否跳到坐标 i
     dp[0] = True 
 
-    # 3. function
     for i in range(1, n):
         for j in range(i): # for 循环上一步
-            # 高效的写法
-            if dp[j] and j + A[j] >= i: #上一步j是True && j + j的步数大于等于到i的距离
+            # 如果存在某点j, dp[j]为true且j可以跳到i, 那么dp[i]为true
+            if dp[j] and j + A[j] >= i:
                 dp[i] = True
                 break
-            # 偷懒的写法
-            dp[i] = dp[i] or dp[j] and (j + A[j] >= i)
 
-    # 4. answer
     return dp[n - 1]
