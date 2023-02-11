b323fcb19a2f3ed781f112fae22cb142b0c4e9e8
Violet Luo <luo.violeta@gmail.com>
2023-02-11 10:08:12 -0800

    Update 0121. Best Time to Buy and Sell Stock.py

--- 96_dp/0121. Best Time to Buy and Sell Stock.py
+++ 96_dp/0121. Best Time to Buy and Sell Stock.py
@@ -1,13 +1,10 @@
-"""
+def maxProfit(self, prices):
+    n = len(prices)
+    dp = [0] * n
+    min_price = prices[0] 
 
-Runtime: 60 ms, faster than 69.79% of Python3 online submissions for Best Time to Buy and Sell Stock.
-Memory Usage: 15.1 MB, less than 9.72% of Python3 online submissions for Best Time to Buy and Sell Stock.
+    for i in range(1, n):
+        min_price = min(min_price, prices[i])
+        dp[i] = max(dp[i - 1], prices[i] - min_price)
 
-"""
-
-def maxProfit(self, prices: List[int]) -> int:
-    profit, min_buy = 0, float('inf')
-    for price in prices:
-        min_buy = min(min_buy, price)
-        profit = max(profit, price - min_buy)
-    return profit
+    return dp[n-1]
