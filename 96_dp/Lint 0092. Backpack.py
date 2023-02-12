53b44acaa1cce3c43d1751d70617dd6db4c5dca1
Violet Luo <luo.violeta@gmail.com>
2023-02-11 19:16:09 -0800

    Update Lint 0092. Backpack.py

--- 96_dp/Lint 0092. Backpack.py
+++ 96_dp/Lint 0092. Backpack.py
@@ -1,41 +1,40 @@
 ### 1.
 def backPack(self, m, A):
     n = len(A)
-    # state: dp[i][j] 代表前 i 个数里能否挑出若干个数组成 j 的和
-    # 注意这是一个 (n+1) * (m+1) 的二维数组而不是 n*m 的二维数组
     dp = [[False] * (m + 1) for _ in range(n + 1)]
-    
-    # initiazlize: 前0个数能凑出0的和，其他的非0和都凑不出来
     dp[0][0] = True
     
-    # function
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         dp[i][0] = True
-        for j in range(1, m+1):
+        for j in range(1, m + 1):
+            # 背包空间大于A[i]
             if j >= A[i-1]:
-                dp[i][j] = dp[i-1][j] or dp[i-1][j-A[i-1]]
+                # dp[i-1][j]: 前i-1个数已经凑出了j的和
+                # dp[i-1][j-A[i-1]: 前i-1个数已经凑出了 j - Ai 的和
+                dp[i][j] = dp[i-1][j] or dp[i-1][j - A[i-1]]
+            # 背包空间小于A[i]
             else:
                 dp[i][j] = dp[i-1][j]
-    
-    # answer
+   
     for i in range(m, -1, -1):
-        if dp[n][i]:
+        if dp[n][i]: # return最满
             return i
     return -1
-  
-  ### 2.
-  def backPack(self, m, A):
+
+### 2.
+def backPack(self, m, A):
     n = len(A)
-    # state: dp[i][j] 代表前 i 个数里能否挑出 <= j 的最大和
-    # 注意这是一个 (n+1) * (m+1) 的二维数组而不是 n*m 的二维数组
-    dp = [[0] * (m+1) for _ in range(n + 1)]
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
     
-    # function
-    for i in range(1, n+1):
-        for j in range(0, m+1):
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            # 背包容量大于第i件物品
             if j >= A[i-1]:
+                # dp[i-1][j]:前i-1个数可以组成的最靠近j的和，不需要第i个数
+                # dp[i-1][j-A[i]]: 前i-1个数可以组成的最靠近 j - A[i-1], 加上A[i-1]才会更靠近j
                 dp[i][j] = max(dp[i-1][j], dp[i-1][j-A[i-1]] + A[i-1])
+            # 背包容量小于第i件物品，不取
             else:
                 dp[i][j] = dp[i-1][j]
-    # answer
+
     return dp[n][m]
