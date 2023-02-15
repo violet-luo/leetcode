d8a3f3e36840427fcbf5421217a18a4dd5d12f7a
Violet Luo <luo.violeta@gmail.com>
2023-02-15 09:54:14 -0800

    Update 0072. Edit Distance.py

--- 96_dp/0072. Edit Distance.py
+++ 96_dp/0072. Edit Distance.py
@@ -9,10 +9,9 @@ def minDistance(self, word1, word2):
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
-            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1] + 1)
             if word2[j - 1] == word1[i - 1]:
-                dp[i][j] = min(dp[i][j], dp[i - 1][j - 1])
+                dp[i][j] = dp[i - 1][j - 1]
             else:
-                dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1)
+                dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1)
 
     return dp[n][m]
