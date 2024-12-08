903b1d14194dbf95fb86826c4a34dc868e47a250
Violet Luo <luo.violeta@gmail.com>
2024-12-08 11:31:02 -0800

    Update 0033. Search in Rotated Sorted Array.py

--- 92_binary_search/0033. Search in Rotated Sorted Array.py
+++ 92_binary_search/0033. Search in Rotated Sorted Array.py
@@ -1,29 +1,61 @@
-"""
-
-Runtime: 36 ms, faster than 88.29% of Python3 online submissions for Search in Rotated Sorted Array.
-Memory Usage: 14.3 MB, less than 16.47% of Python3 online submissions for Search in Rotated Sorted Array.
-
-"""
-
+# 1. Two binary searches
 def search(self, nums: List[int], target: int) -> int:
-    start, end = 0, len(nums) - 1
+    # 1. find index of min
+    n = len(nums)
+    l, r = 0, n - 1
+    while l + 1 < r:
+        mid = (l + r) // 2
+        if nums[mid] > nums[r]:
+            l = mid
+        else:
+            r = mid
+    num_min_idx = l if nums[l] < nums[r] else r
 
-    while start <= end:
-        mid = start + (end - start) // 2
+    # 2.
+    if target <= nums[-1]:
+        return self.binary_search(nums, num_min_idx, n - 1, target)
+    else:
+        return self.binary_search(nums, 0, num_min_idx - 1, target)
 
+def binary_search(self, nums, l, r, target):
+    while l + 1 < r:
+        mid = (l + r) // 2
         if nums[mid] == target:
             return mid
+        elif nums[mid] < target:
+            l = mid 
+        else:
+            r = mid
+    
+    if nums[l] == target:
+        return l
+    if nums[r] == target:
+        return r
+        return -1
 
-        # determine it's left rotated or right rotated
-        if nums[mid] >= nums[start]: # left rotated, mid >= left, [3,4,5,6,7,1,2]
-            if nums[start] <= target and target <= nums[mid]: # search in the left side [3,4,5]
-                end = mid - 1
-            else:
-                start = mid + 1
-        else: # right rotated, mid < left, [6,7,1,2,3,4,5]
-            if nums[mid] <= target <= nums[end]: # search in the right side [2,3,4,5]
-                start = mid + 1
-            else:
-                end = mid - 1
+# 2. One binary search
+def search(self, nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
 
+    while l + 1 < r:
+        mid = (l + r) // 2
+        if nums[mid] > nums[r]: # 在左边
+            # 不能是 nums[mid] > target
+            # [4, 5, 6, 7, 0, 1, 2]，nums[mid] might be 7
+            # nums[mid] > target always hold true, 不知道是往左还是往右
+            # 要带等号，array没有重复值，但是target可能会相等
+            if nums[l] <= target <= nums[mid]:
+                r = mid
+            else:
+                l = mid
+        else: # 在右边
+            if nums[mid] <= target <= nums[r]:
+                l = mid
+            else:
+                r = mid
+    
+    if nums[l] == target:
+        return l
+    if nums[r] == target:
+        return r
     return -1
