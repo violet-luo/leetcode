dc8a4bc4d66771ca253891afb17f4ece5460b4bc
Violet Luo <luo.violeta@gmail.com>
2024-12-08 14:21:23 -0800

    Update 0240. Search a 2D Matrix II.py

--- 92_binary_search/0240. Search a 2D Matrix II.py
+++ 92_binary_search/0240. Search a 2D Matrix II.py
@@ -1,3 +1,21 @@
+# 从右上出发
+def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    n, m = len(matrix), len(matrix[0])
+    i, j = 0, m - 1 # 从右上角开始
+    
+    while i < n and j >= 0: # 还有剩余元素
+        if matrix[i][j] == target:
+            return True
+        # 先排除行再排除列
+        elif matrix[i][j] < target:
+            i += 1 # 这一行剩余元素全部小于 target，排除
+        else:
+            j -= 1 # 这一列剩余元素全部大于 target，排除
+    
+    return False
+
+###
+
 def searchMatrix(matrix, target):
     if not matrix or not matrix[0]:
         return 0
@@ -40,13 +58,12 @@ def searchMatrix(matrix, target):
     return False
 
 ###
-
+# 从左下出发
 def searchMatrix(matrix, target):
     if not matrix or not matrix[0]:
         return False
         
     n, m = len(matrix), len(matrix[0])
-    # 从左下出发
     i, j = n - 1, 0
     while i >= 0 and j < m:
         if matrix[i][j] == target:
