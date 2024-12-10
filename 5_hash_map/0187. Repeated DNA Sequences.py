298c01677780a74f60e1e689f5a8148ef8764ffd
Violet Luo <luo.violeta@gmail.com>
2024-12-09 21:28:03 -0800

    Update 0187. Repeated DNA Sequences.py

--- 5_hash_map/0187. Repeated DNA Sequences.py
+++ 5_hash_map/0187. Repeated DNA Sequences.py
@@ -1,3 +1,18 @@
+def findRepeatedDnaSequences(self, s: str) -> List[str]:
+    seen = set()
+    res = []
+
+    for i in range(len(s) - 9): # s长度为11,range(0,1)
+        substr = s[i:i+10]
+        if substr in seen and substr not in res:
+            res.append(substr)
+        else:
+            seen.add(substr)
+    
+    return res
+
+###
+
 def findRepeatedDnaSequences(self, s):
     seen = set()
     res = set()
