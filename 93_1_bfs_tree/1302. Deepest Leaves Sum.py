51e78d0fd9d60ca8ec9d2ced0199e4a8757aac13
Violet Luo <luo.violeta@gmail.com>
2024-12-09 20:31:47 -0800

    Update 1302. Deepest Leaves Sum.py

--- 93_1_bfs_tree/1302. Deepest Leaves Sum.py
+++ 93_1_bfs_tree/1302. Deepest Leaves Sum.py
@@ -1,3 +1,21 @@
+def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+    res = collections.defaultdict(list)
+    q = collections.deque([(root, 0)])
+
+    while q:
+        for i in range(len(q)):
+            node, depth = q.popleft()
+            res[depth].append(node.val)
+            if node.left:
+                q.append((node.left, depth + 1))
+            if node.right:
+                q.append((node.right, depth + 1))
+
+    max_depth = max(res.keys())
+    return sum(res[max_depth])
+        
+###
+
 def deepestLeavesSum(self, root):
     queue = collections.deque([root])
     while queue:
