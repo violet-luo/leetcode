96f6a5d10a018a668d6afe88722a6ba03025dd47
Violet Luo <luo.violeta@gmail.com>
2024-12-10 11:18:58 -0800

    Update 0200. Number of Islands.py

--- 93_2_bfs_matrix/0200. Number of Islands.py
+++ 93_2_bfs_matrix/0200. Number of Islands.py
@@ -1,3 +1,29 @@
+def numIslands(self, grid: List[List[str]]) -> int:
+    if not grid or not grid[0]:
+        return 0
+    
+    n, m = len(grid), len(grid[0])
+    islands = 0
+
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == "1":
+                q = collections.deque([(i, j)])
+                grid[i][j] = '0'
+                while q:
+                    x, y = q.popleft()
+                    for dx, dy in [(1, 0), (0, -1), (-1, 0), (0, 1)]:
+                        new_x, new_y = x + dx, y + dy
+                        if 0 <= new_x < n and 0 <= new_y < m and grid[new_x][new_y] == '1':
+                            grid[new_x][new_y] = '0'
+                            q.append((new_x, new_y))
+                
+                islands += 1
+    
+    return islands
+    
+###
+
 def numIslands(grid):
     if not grid or not grid[0]: return 0
     n, m = len(grid), len(grid[0])
