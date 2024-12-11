b1435a7e0015006817ce45c8cb136a8c21141284
Violet Luo <luo.violeta@gmail.com>
2024-12-11 13:08:02 -0800

    Update 0965. Univalued Binary Tree.py

--- 94_1_dfs_recursive/0965. Univalued Binary Tree.py
+++ 94_1_dfs_recursive/0965. Univalued Binary Tree.py
@@ -1,4 +1,33 @@
 def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+    uni_value = root.val
+
+    def traverse(node):
+        if not node:
+            return True
+        if node.val != uni_value:
+            return False
+        return traverse(node.left) and traverse(node.right)
+    
+    return traverse(root)
+        
+###
+
+def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+    res = []
+
+    def traverse(node):
+        if not node:
+            return
+        res.append(node.val)
+        traverse(node.left)
+        traverse(node.right)
+    
+    traverse(root)
+    return len(set(res)) == 1
+
+###
+
+def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
     if not root:
         return True
     uni_value = root.val
