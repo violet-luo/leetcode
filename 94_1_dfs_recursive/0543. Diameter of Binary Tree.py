b123beec37e6fe9b066248d13d2c9488d7e5eb44
Violet Luo <luo.violeta@gmail.com>
2024-12-11 15:36:43 -0800

    Update 0543. Diameter of Binary Tree.py

--- 94_1_dfs_recursive/0543. Diameter of Binary Tree.py
+++ 94_1_dfs_recursive/0543. Diameter of Binary Tree.py
@@ -1,9 +1,14 @@
-def diameterOfBinaryTree(self, root):
-    return self.getHeight(root.left) + self.getHeight(root.right)
-    
-def getHeight(self, node):
-    if not node:
-        return 0
-    leftHeight = self.getHeight(node.left)
-    rightHeight = self.getHeight(node.right)
-    return max(leftHeight, leftHeight) + 1
+def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+    max_diameter = 0
+
+    def get_height(node):
+        if not node:
+            return 0
+        nonlocal max_diameter
+        left_height = get_height(node.left)
+        right_height = get_height(node.right)
+        max_diameter = max(max_diameter, left_height + right_height)
+        return max(left_height, right_height) + 1
+
+    get_height(root)
+    return max_diameter
