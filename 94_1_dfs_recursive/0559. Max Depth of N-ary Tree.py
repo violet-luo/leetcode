45452a58aac57c9dacea5f7e9b9948401a0826ce
Violet Luo <luo.violeta@gmail.com>
2024-12-11 15:12:32 -0800

    Update 0559. Max Depth of N-ary Tree.py

--- 94_1_dfs_recursive/0559. Max Depth of N-ary Tree.py
+++ 94_1_dfs_recursive/0559. Max Depth of N-ary Tree.py
@@ -1,4 +1,28 @@
 def maxDepth(self, root):
+    def get_height(node):
+        if node is None: 
+            return 0
+        elif not node.children:
+            return 1
+        else:
+            height = [get_height(child) for child in node.children]
+            return max(height) + 1
+    return get_height(root)
+
+###
+
+def maxDepth(self, root):
+    if root is None: 
+        return 0 
+    elif root.children == []:
+        return 1
+    else: 
+        height = [self.maxDepth(c) for c in root.children]
+        return max(height) + 1 
+    
+###
+
+def maxDepth(self, root):
     if not root:
         return 0
     depth = 0
