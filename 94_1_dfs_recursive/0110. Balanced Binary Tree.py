9a101ef41f10bb677f0752dcdcd0e78859e9d119
Violet Luo <luo.violeta@gmail.com>
2024-12-11 20:53:14 -0800

    Update 0110. Balanced Binary Tree.py

--- 94_1_dfs_recursive/0110. Balanced Binary Tree.py
+++ 94_1_dfs_recursive/0110. Balanced Binary Tree.py
@@ -1,3 +1,22 @@
+def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    res = True
+    def get_heights(node):
+        if not node:
+            return 0
+        nonlocal res
+
+        left_height = get_heights(node.left)
+        right_height = get_heights(node.right)
+        diff = abs(left_height - right_height)
+        if diff > 1:
+            res = False # 不能直接return False here因为和下面的return type不一样
+        return max(left_height, right_height) + 1
+    
+    get_heights(root)
+    return res
+    
+###
+
 def isBalanced(self, root):
     if not root:
         return True
