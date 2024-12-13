6f0a8046a36e569c91f6f69080a20d36e2cf2dbd
Violet Luo <luo.violeta@gmail.com>
2024-12-13 15:33:06 -0800

    Update 0112. Path Sum.py

--- 94_1_dfs_recursive/0112. Path Sum.py
+++ 94_1_dfs_recursive/0112. Path Sum.py
@@ -1,3 +1,23 @@
+def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+    res = []
+
+    def traverse(node, path_sum):
+        nonlocal res
+        if not node:
+            return None
+
+        path_sum += node.val
+        if not node.left and not node.right:
+            res.append(path_sum)
+        else:
+            traverse(node.left, path_sum)
+            traverse(node.right, path_sum)
+
+    traverse(root, 0)
+    return True if targetSum in res else False
+
+###
+
 def hasPathSum(self, root, sum):
     if not root: # 出口2，这里加not sum会死循环，报错
         return False
