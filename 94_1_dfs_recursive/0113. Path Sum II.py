7318088ba7941260ca7b5fc2c39dadabf1e405d9
Violet Luo <luo.violeta@gmail.com>
2024-12-13 15:54:20 -0800

    Update 0113. Path Sum II.py

--- 94_1_dfs_recursive/0113. Path Sum II.py
+++ 94_1_dfs_recursive/0113. Path Sum II.py
@@ -1,3 +1,27 @@
+def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+    res = []
+
+    def traverse(node, path, path_sum):
+        if not node:
+            return None
+
+        # 不能是 path.append(node.val)
+        # When you call path.append(node) in one recursive branch
+        # it modifies the same list path used in other branches of the recursion.
+        # 会需要backtrack
+        new_path = path + [node.val]
+        new_path_sum = path_sum + node.val
+        if not node.left and not node.right and new_path_sum == targetSum:
+            res.append(new_path)
+        else:
+            traverse(node.left, new_path, new_path_sum)
+            traverse(node.right, new_path, new_path_sum)
+
+    traverse(root, [], 0)
+    return res
+    
+###
+
 def pathSum(self, root, targetSum):
     res = []
 
