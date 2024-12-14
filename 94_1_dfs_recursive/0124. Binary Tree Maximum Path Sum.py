ec987bc9cbc4b7350c5d4c589849c5a90bfca56f
Violet Luo <luo.violeta@gmail.com>
2024-12-13 17:26:00 -0800

    Update 0124. Binary Tree Maximum Path Sum.py

--- 94_1_dfs_recursive/0124. Binary Tree Maximum Path Sum.py
+++ 94_1_dfs_recursive/0124. Binary Tree Maximum Path Sum.py
@@ -1,3 +1,23 @@
+def maxPathSum(self, root: Optional[TreeNode]) -> int:
+    max_sum = float('-inf')
+
+    def traverse(node) -> int:
+        nonlocal max_sum
+        if not node:
+            return 0
+        
+        left_max_sum = max(traverse(node.left), 0) # ignore paths with negative sums
+        right_max_sum = max(traverse(node.right), 0)
+
+        curr_sum = left_max_sum + right_max_sum + node.val
+        max_sum = max(max_sum, curr_sum)
+
+        return node.val + max(left_max_sum, right_max_sum) # return the max sum path ending at the current node
+
+    traverse(root)
+    return max_sum
+###
+
 def maxPathSum(self, root):
     max_sum = float('-inf')
     
