a02129e49b3aebbc55137cca21081ddbd680ac81
Violet Luo <luo.violeta@gmail.com>
2024-12-14 10:41:16 -0800

    Update 0938. Range Sum of BST.py

--- 94_1_dfs_recursive/0938. Range Sum of BST.py
+++ 94_1_dfs_recursive/0938. Range Sum of BST.py
@@ -7,3 +7,17 @@ def rangeSumBST(self, root, low, high):
         return self.rangeSumBST(root.left, low, high)
     else: # low < root.val < high
         return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+###
+
+def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    if not root:
+        return 0
+    sum = 0
+    if root.val > low: # 需要访问左子树
+        sum += self.rangeSumBST(root.left, low, high)
+    if root.val < high:
+        sum += self.rangeSumBST(root.right, low, high)
+    if low <= root.val <= high:
+        sum += root.val     
+    return sum
