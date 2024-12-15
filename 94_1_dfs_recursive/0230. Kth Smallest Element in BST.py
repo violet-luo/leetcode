59375652007a90b26128ac9c52fc28b54d849480
Violet Luo <luo.violeta@gmail.com>
2024-12-15 12:27:18 -0800

    Update 0230. Kth Smallest Element in BST.py

--- 94_1_dfs_recursive/0230. Kth Smallest Element in BST.py
+++ 94_1_dfs_recursive/0230. Kth Smallest Element in BST.py
@@ -1,3 +1,22 @@
+def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    res = None
+    def traverse(node):
+        nonlocal res, k
+        if not node:
+            return
+        
+        traverse(node.left)
+        k -= 1
+        if k == 0:
+            res = node.val
+            return
+        traverse(node.right)
+    
+    traverse(root)
+    return res
+
+###
+
 def kthSmallest(self, root, k):
     arr = self.inorder(root)
     return arr[k - 1]
