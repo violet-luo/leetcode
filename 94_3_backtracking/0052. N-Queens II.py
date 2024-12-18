306d6871324bae94098d0b8c3358140bb292e38e
Violet Luo <luo.violeta@gmail.com>
2024-12-18 13:50:18 -0800

    Update 0052. N-Queens II.py

--- 94_3_backtracking/0052. N-Queens II.py
+++ 94_3_backtracking/0052. N-Queens II.py
@@ -1,4 +1,34 @@
 def totalNQueens(self, n: int) -> int:
+    count = 0
+    queen_columns = []
+
+    def dfs(n, queen_columns):
+        nonlocal count
+        def is_valid(queen_columns, row, col):
+            for r, c in enumerate(queen_columns):
+                if c == col: # 有其他皇后在同一列
+                    return False 
+                if abs(r - row) == abs(c - col): #有其他皇后在同一斜线
+                    return False
+            return True
+        
+        row = len(queen_columns)
+        if row == n: # 递归出口, 所有行都有皇后了
+            count += 1
+            return
+        
+        for col in range(n):
+            if is_valid(queen_columns, row, col):
+                queen_columns.append(col)
+                dfs(n, queen_columns)
+                # 找到第一个solution之后backtrack到上一个valid column找其他的solution
+                queen_columns.pop() 
+    dfs(n, queen_columns)
+    return count
+    
+###
+
+def totalNQueens(self, n: int) -> int:
     self.count = 0
     subset = []
     self.backtrack(n, subset)
