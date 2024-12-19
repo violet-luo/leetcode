6b542791b2211dba6dd3bf4bbb06ea2d09d0c324
Violet Luo <luo.violeta@gmail.com>
2024-12-19 12:14:31 -0800

    Update 0444. Sequence Reconstruction.py

--- 93_4_topology/0444. Sequence Reconstruction.py
+++ 93_4_topology/0444. Sequence Reconstruction.py
@@ -1,3 +1,53 @@
+def sequenceReconstruction(self, nums: List[int], sequences: List[List[int]]) -> bool:
+    graph = self.build_graph(nums, sequences)
+    order = self.topological_sort(graph, nums)
+    return order == nums
+
+def build_graph(self, nums, sequences):
+    graph = defaultdict(set)
+    for num in nums:
+        graph[num] # {1: set(), 2: set(), 3: set()}
+
+    for seq in sequences:
+        for i in range(1, len(seq)):
+            # graph存储每个点对应的后序点
+            graph[seq[i - 1]].add(seq[i]) # {1: {2, 3}, 2: set(), 3: set()}
+
+    return graph
+
+# 标准拓扑排序模板
+def topological_sort(self, graph, nums):
+    indegree = self.get_indegree(graph)
+    queue = collections.deque([num for num in nums if indegree[node] == 0 ])
+    order = []
+    
+    while queue:
+        # 优化：比模板多了这个判断
+        # 如果queue里有超过一个节点，说明排序的结果有多种可能
+        # e.g. nums = [1,2,3], seq = [[1,2],[1,3]]
+        # 处理节点1时，queue = [2, 3], 排序不唯一
+        if len(queue) > 1:
+            return None
+        node = queue.popleft()
+        order.append(node)
+        for neighbor in graph[node]:
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                queue.append(neighbor)
+
+    if len(order) == len(graph): # 比模板多了这个判断
+        return order
+    return None
+
+def get_indegree(self, graph):
+    indegree = { node: 0 for node in graph }
+    for node in graph:
+        for neighbor in graph[node]:
+            indegree[neighbor] += 1
+    return indegree
+
+### Lintcode
+
 def sequenceReconstruction(self, org, seqs):
     graph = self.build_graph(seqs)
     order = self.topological_sort(graph)
