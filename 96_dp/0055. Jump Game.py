35f893c087f769e385c699afd4cd5dc4133f91d0
Violet Luo <luo.violeta@gmail.com>
2024-12-22 14:15:44 -0800

    Update 0055. Jump Game.py

--- 96_dp/0055. Jump Game.py
+++ 96_dp/0055. Jump Game.py
@@ -1,13 +1,25 @@
-def canJump(A):
-    n = len(A)
-    dp = [False] * n # dp[i] 代表能否跳到坐标 i
-    dp[0] = True 
+def canJump(self, nums: List[int]) -> bool:
+    n = len(nums)
 
+    # state: dp[i] 代表能否跳到坐标 i
+    dp = [False] * n
+
+    # initialization: 一开始站在0这个位置
+    dp[0] = True
+
+    # function
+    # loop thru each index from 1 to n - 1 to see if it's reachable
     for i in range(1, n):
-        for j in range(i): # for 循环上一步
-            # 如果存在某点j, dp[j]为true且j可以跳到i, 那么dp[i]为true
-            if dp[j] and j + A[j] >= i:
+        # for each index, loos thru all previous indices j from 0 to i - 1
+        for j in range(i):
+            # 高效的写法
+            # dp[j]: can we reach index j?
+            # j + nums[j] >= i: from index j, can we reach i?
+            if dp[j] and j + nums[j] >= i:
                 dp[i] = True
                 break
-
+            
+            # 偷懒的写法
+            dp[i] = dp[i] or dp[j] and (j + nums[j] >= i)
+    
     return dp[n - 1]
