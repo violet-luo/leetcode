eaa39f86a36afbb87b66444037b0f70651407394
Violet Luo <luo.violeta@gmail.com>
2024-12-21 19:46:52 -0800

    Update 0701. Insert Node in BST.py

--- 94_1_dfs_recursive/0701. Insert Node in BST.py
+++ 94_1_dfs_recursive/0701. Insert Node in BST.py
@@ -1,3 +1,24 @@
+def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    if root is None:
+        return TreeNode(val)
+    def traverse(node) -> None:
+        if node is None:
+            return None
+        if node.val < val: # no need to consider = because of BST
+            if node.right is None:
+                node.right = TreeNode(val)
+            else:
+                traverse(node.right)
+        else:
+            if node.left is None:
+                node.left = TreeNode(val)
+            else:
+                traverse(node.left)
+    traverse(root)
+    return root
+    
+###
+
 def insertIntoBST(self, root, val):
     if not root:
         return TreeNode(val)
