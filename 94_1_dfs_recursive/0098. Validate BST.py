10eb2ee590e1b32c29ada5fd4f54c5a06c25600b
Violet Luo <luo.violeta@gmail.com>
2024-12-21 22:55:33 -0800

    Update 0098. Validate BST.py

--- 94_1_dfs_recursive/0098. Validate BST.py
+++ 94_1_dfs_recursive/0098. Validate BST.py
@@ -1,3 +1,24 @@
+def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    def validate(node) -> (bool, min: int, max: int):
+        if node is None:
+            return True, float('inf'), float('-inf')
+        
+        left_is_valid, l_min, l_max = validate(node.left)
+        right_is_valid, r_min, r_max = validate(node.right)
+        
+        if not (left_is_valid and right_is_valid and l_max < node.val < r_min):
+            return False, 0, 0
+        
+        subtree_min = min(l_min, node.val)
+        subtree_max = max(r_max, node.val)
+        
+        return True, subtree_min, subtree_max
+    
+    is_valid, _, _ = validate(root)
+    return is_valid
+
+###
+
 def isValidBST(self, root):
     return self.check_bst(float('-inf'), root, float("inf"))
 
