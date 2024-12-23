c7ab7f94679b59e170b8c209e148603d047fd398
Violet Luo <luo.violeta@gmail.com>
2024-12-22 19:47:27 -0800

    Update 0152. Max Product Subarray.py

--- 96_dp/0152. Max Product Subarray.py
+++ 96_dp/0152. Max Product Subarray.py
@@ -1,3 +1,24 @@
+def maxProduct(self, nums: List[int]) -> int:
+    if not nums:
+        return None
+
+    global_max = prev_max = prev_min = nums[0]
+
+    for num in nums[1:]:
+        if num > 0:
+            cur_max = max(num, prev_max * num)
+            cur_min = min(num, prev_min * num)
+        else:
+            cur_max = max(num, prev_min * num)
+            cur_min = min(num, prev_max * num)
+
+        global_max = max(global_max, cur_max)
+        prev_max, prev_min = cur_max, cur_min
+
+    return global_max
+
+###
+
 def maxProduct(self, nums):
     res = nums[0]
     min_p = max_p = nums[0]
