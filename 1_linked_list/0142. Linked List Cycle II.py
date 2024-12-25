216dbf816bdd70ae5962a15847813569d11ecc95
Violet Luo <luo.violeta@gmail.com>
2024-12-24 17:04:26 -0800

    Update 0142. Linked List Cycle II.py

--- 1_linked_list/0142. Linked List Cycle II.py
+++ 1_linked_list/0142. Linked List Cycle II.py
@@ -2,6 +2,25 @@
 1. Two Pointers
 
 """
+def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    slow = fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    else:
+        return None
+
+    slow = head
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+    
+    return slow
+
+###
 
 def detectCycle(self, head):     
     if not head or not head.next:
