c8a1f3f0d2fb1c6383aa034eb8e09633581a8d2c
Violet Luo <luo.violeta@gmail.com>
2024-12-25 21:01:52 -0800

    Update 1836. Remove Dupes from Unsorted List.py

--- 1_linked_list/1836. Remove Dupes from Unsorted List.py
+++ 1_linked_list/1836. Remove Dupes from Unsorted List.py
@@ -1,3 +1,24 @@
+def deleteDuplicatesUnsorted(self, head: ListNode) -> ListNode:
+    dic = collections.defaultdict(int)
+    cur = head
+    while cur:
+        dic[cur.val] += 1
+        cur = cur.next
+    
+    dummy = ListNode(0)
+    dummy.next = head
+    cur = dummy
+
+    while cur.next:
+        if dic[cur.next.val] > 1:
+            cur.next = cur.next.next  # Skip the duplicate
+        else:
+            cur = cur.next  # Move to the next node
+    
+    return dummy.next
+        
+###
+
 def deleteDuplicatesUnsorted(self, head):
     dic = collections.defaultdict(int)
     cur = head
