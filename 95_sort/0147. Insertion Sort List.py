95c113d6f1a7e657129828fab0b6321280a5c932
Violet Luo <luo.violeta@gmail.com>
2024-12-30 13:04:21 -0800

    Update 0147. Insertion Sort List.py

--- 95_sort/0147. Insertion Sort List.py
+++ 95_sort/0147. Insertion Sort List.py
@@ -1,20 +1,26 @@
-def insertionSortList(self, head):
+def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
     if not head:
         return head
     dummy = ListNode(0, head)
     cur = head
 
-    while cur.next:
-        if cur.val <= cur.next.val:
+    while cur.next: # 循环里需要cur.next.val
+		    # 1. 找一个比前一个数小的数，cur为大数，cur.next为小数
+		    # cur = 5, cur.next = 2
+        if cur.val <= cur.next.val: 
             cur = cur.next
         else:
-            start = dummy #从头开始寻找插入位置，最终在start的后面
+            insertion_point = dummy
             node_to_insert = cur.next
-            while start.next.val <= node_to_insert.val: #当start的next的值比待排的大
-                start = start.next
-
-            cur.next = node_to_insert.next
-            node_to_insert.next = start.next
-            start.next = node_to_insert
+            # 2. 找insertion_point < node < insertion_point.next
+            # 一直走到insertion_point.next的值比待排的大
+            # insertion_point = 1, insertion_point.next = 3
+            while insertion_point.next.val <= node_to_insert.val: 
+                insertion_point = insertion_point.next
+						
+						# 3. 把node插入insertion和next之间
+            cur.next = node_to_insert.next # 把待插入节点从链表中移除
+            node_to_insert.next = insertion_point.next # 插入节点的后继设为 start.next
+            insertion_point.next = node_to_insert # 完成插入，将 start 的 next 指向插入节点
 
     return dummy.next
