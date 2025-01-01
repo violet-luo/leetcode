26aafcef9a74fc795073f9fc0c032b4d2c5f97cc
Violet Luo <luo.violeta@gmail.com>
2025-01-01 14:31:35 -0800

    Update 0249. Group Shifted Strings.py

--- 5_hash_map/0249. Group Shifted Strings.py
+++ 5_hash_map/0249. Group Shifted Strings.py
@@ -5,12 +5,18 @@
 #          (): ['a', 'z']
 # }
 
-def groupStrings(self, strings):
-    dic = {} # key: a tuple of diff between adjacent char
+def groupStrings(self, strings: List[str]) -> List[List[str]]:
+    shift_to_strings = collections.defaultdict(list)
+
     for s in strings:
-        key = ()
-        for i in range(len(s) - 1):
-            circular_difference = 26 + ord(s[i+1]) - ord(s[i])
-            key += (circular_difference % 26,)
-        dic[key] = dic.get(key, []) + [s]
-    return list(dic.values())
+        shift = ()
+        for i in range(1, len(s)):
+            diff = (ord(s[i]) - ord(s[i - 1])) % 26
+            # Tuples are immutable, so cannot modify directly (e.g., by appending elements)
+            # Instead, can create a new tuple by concatenating an existing tuple with another tuple
+            # (diff,) creates a single-element tuple containing diff
+            # e.g. diff = 1, x = (diff,) = (1, )
+            shift += (diff, )
+        shift_to_strings[shift].append(s)
+    
+    return list(shift_to_strings.values())
