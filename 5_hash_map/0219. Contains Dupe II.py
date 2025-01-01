51eb78a83fe26a63c134f57fc91e9fd60ed9d67c
Violet Luo <luo.violeta@gmail.com>
2024-12-31 19:33:18 -0800

    Update 0219. Contains Dupe II.py

--- 5_hash_map/0219. Contains Dupe II.py
+++ 5_hash_map/0219. Contains Dupe II.py
@@ -1,8 +1,9 @@
-def containsNearbyDuplicate(self, nums, k):
-    dic = {}
+def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    num_to_idx = collections.defaultdict(int)
 
     for i in range(len(nums)):
-        if nums[i] in dic and abs(i - dic[nums[i]]) <= k:
+        if nums[i] in num_to_idx and abs(i - num_to_idx[nums[i]]) <= k:
             return True
-        dic[nums[i]] = i
+        num_to_idx[nums[i]] = i
+    
     return False
