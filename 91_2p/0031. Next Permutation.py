85ae42a57d5f24d57f1c8c7cc99ef347b937106e
Violet Luo <luo.violeta@gmail.com>
2025-01-02 17:57:30 -0800

    Update 0031. Next Permutation.py

--- 91_2p/0031. Next Permutation.py
+++ 91_2p/0031. Next Permutation.py
@@ -1,3 +1,25 @@
+def nextPermutation(self, nums: List[int]) -> None:
+    n = len(nums)
+
+    # 1. 从后往前找第一个降序元素 A[i]
+    for i in range(n - 2, -1, -1):
+        if nums[i + 1] > nums[i]:
+            break
+    else:
+        nums.reverse()
+        return nums
+
+    # 2. 从后往前找第一个比 A[i] 大的元素 A[k]
+    for j in range(n - 1, i, -1):
+        if nums[j] > nums[i]:
+            nums[j], nums[i] = nums[i], nums[j]
+            break
+    
+    # 3. reverse A[i+1:]
+    nums[i+1::] = nums[i+1:][::-1] # 不能只是nums[i+1:][::-1], 这个只是copy, 没有真的改变
+    
+###
+
 def nextPermutation(self, nums):
     n = len(nums)
     for i in range(n - 1, 0, -1):
