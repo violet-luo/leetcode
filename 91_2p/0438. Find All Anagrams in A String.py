0924502ffb2a683dd88e45717e2963dabfafe560
Violet Luo <luo.violeta@gmail.com>
2025-01-03 12:48:38 -0800

    Update 0438. Find All Anagrams in A String.py

--- 91_2p/0438. Find All Anagrams in A String.py
+++ 91_2p/0438. Find All Anagrams in A String.py
@@ -1,29 +1,17 @@
-def findAnagrams(self, s, p):
-    if len(p) > len(s):
-        return []
-    if p == s:
-        return [0]
-        
-    p_hash = {}
-    s_hash = {}
+def findAnagrams(self, s: str, p: str) -> List[int]:
+    p_dic = collections.Counter(p)
+    s_dic = collections.defaultdict(int)
+    res = []
+
+    l = 0
+    for r in range(0, len(s)):
+        s_dic[s[r]] += 1
+        if r - l + 1 == len(p):
+            if s_dic == p_dic:
+                res.append(l)
+            s_dic[s[l]] -= 1
+            if s_dic[s[l]] == 0:
+                del s_dic[s[l]]
+            l += 1
     
-    ans = [] 
-    for i in range(len(p)):
-        p_hash[p[i]] = p_hash.get(p[i],0) + 1 
-        s_hash[s[i]] = s_hash.get(s[i],0) + 1 
-        
-    if s_hash == p_hash:
-        ans.append(0)    
-       
-    for window_start in range(1, len(s) - len(p) + 1):
-        window_end = window_start + len(p) - 1 
-        s_hash[s[window_end]] = s_hash.get(s[window_end], 0) + 1 
-        
-        s_hash[s[window_start -1]] -= 1 
-        if s_hash[s[window_start -1]] == 0:
-            del s_hash[s[window_start -1]]
-        
-        if s_hash == p_hash:
-            ans.append(window_start)
-            
-    return ans
+    return res
