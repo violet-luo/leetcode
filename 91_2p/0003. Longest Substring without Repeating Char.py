a7833a9dc720a85395b398e305587418f02fd838
Violet Luo <luo.violeta@gmail.com>
2025-01-03 11:37:56 -0800

    Update 0003. Longest Substring without Repeating Char.py

--- 91_2p/0003. Longest Substring without Repeating Char.py
+++ 91_2p/0003. Longest Substring without Repeating Char.py
@@ -1,14 +1,14 @@
-def lengthOfLongestSubstring(s):
-    max_len, seen = 0, set()
-    n = len(s)
-    right = 0
+def lengthOfLongestSubstring(self, s: str) -> int:
+    max_len = 0
+    l = 0
+    seen = set()
 
-    for left in range(n):
-        while right < n and s[right] not in seen:
-            seen.add(s[right])
-            right += 1
-        # 走到第一个重复
-        max_len = max(max_len, right - left)
-        seen.remove(s[left])
+    for r in range(len(s)):
+        while s[r] in seen: # Remove characters until the duplicate is gone
+            seen.remove(s[l])
+            l += 1
+        
+        max_len = max(max_len, r - l + 1)
+        seen.add(s[r])
     
     return max_len
