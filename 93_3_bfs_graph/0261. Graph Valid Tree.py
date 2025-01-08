b79fd7498345e6c19fdb6a13c5dbbb1087af62b0
Violet Luo <luo.violeta@gmail.com>
2025-01-07 16:23:16 -0800

    Update 0261. Graph Valid Tree.py

--- 93_3_bfs_graph/0261. Graph Valid Tree.py
+++ 93_3_bfs_graph/0261. Graph Valid Tree.py
@@ -1,23 +1,22 @@
 def validTree(self, n: int, edges: List[List[int]]) -> bool:
-    if len(edges) != n - 1:
+    if len(edges) != n - 1: # 有环
         return False
-
-    # 每个点和它的邻居
+    
     neighbors = collections.defaultdict(list)
-    # {0: [1, 2, 3], 1: [0, 4], 2: [0], 3: [0], 4: [1]}
     for u, v in edges:
         neighbors[u].append(v)
         neighbors[v].append(u)
+    
+    q = collections.deque([0]) # start from 0
+    visited = {0}
 
-    visited = set()
-    queue = collections.deque([0])
-
-    while queue:
-        node = queue.popleft() #0
-        visited.add(node) # {0: True}
+    while q:
+        node = q.popleft()
         for neighbor in neighbors[node]:
             if neighbor not in visited:
-                visited.add(neighbor) # (0, 1, 2, 3, 4)
-                queue.append(neighbor) # ([1,2,3])
-
+                q.append(neighbor) # ([1,2,3])
+                visited.add(neighbor)
+    
+    # 如果能遍历完所有的点，且没有环存在，那么说明这个无向图是树
+    # if len(visited) < n: 有没有遍历的点
     return len(visited) == n
