3ad0e993f97434bc8a37296686d0efe79cae9bff
Violet Luo <luo.violeta@gmail.com>
2025-01-10 14:48:48 -0800

    Update 0247. Strobogrammatic Number II.py

--- 94_1_dfs_recursive/0247. Strobogrammatic Number II.py
+++ 94_1_dfs_recursive/0247. Strobogrammatic Number II.py
@@ -1,5 +1,5 @@
 def findStrobogrammatic(self, n: int) -> List[str]:
-    pairs = [('0', '0'), ('1', '1'), ('6', '9'), ('8', '8'), ('9', '6')]
+    dic = {'0':'0', '1':'1', '6':'9', '8':'8', '9':'6'}
 
     def traverse(x: int) -> List[str]:
         # base case
@@ -12,12 +12,9 @@ def findStrobogrammatic(self, n: int) -> List[str]:
         res = []
 
         for num in smaller_numbers:
-            for pair in pairs:
-                # Avoid leading zeros when x == n
-                if x == n and pair[0] == '0':
+            for key, val in dic.items():
+                if x == n and key == '0':
                     continue
-                res.append(pair[0] + num + pair[1])
+                res.append(key + num + val)
         
         return res
-    
-    return traverse(n)
