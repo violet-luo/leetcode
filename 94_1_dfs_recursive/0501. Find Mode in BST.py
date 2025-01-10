8cf42697c92376f735a2c65c4a8384fb82f7d693
Violet Luo <luo.violeta@gmail.com>
2025-01-10 14:00:39 -0800

    Update 0501. Find Mode in BST.py

--- 94_1_dfs_recursive/0501. Find Mode in BST.py
+++ 94_1_dfs_recursive/0501. Find Mode in BST.py
@@ -1,3 +1,35 @@
+def findMode(self, root: Optional[TreeNode]) -> List[int]:
+    current_val = None
+    current_count = 0
+    max_count = 0
+    modes = []
+
+    def traverse(node):
+        nonlocal current_val, current_count, max_count, modes
+        if not node:
+            return
+        
+        traverse(node.left)
+
+        if node.val == current_val:
+            current_count += 1
+        else:
+            current_count = 1
+            current_val = node.val
+        
+        if current_count == max_count:
+            modes.append(node.val)
+        elif current_count > max_count:
+            max_count = current_count
+            modes = [node.val]
+        
+        traverse(node.right)
+    
+    traverse(root)
+    return modes
+    
+###
+
 def findMode(self, root):
     nums = self.inorder(root)
     counter = collections.Counter(nums)
