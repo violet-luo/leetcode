9730fbb2484ef6fc05f20624e35c8ee36a2bcad8
Violet Luo <luo.violeta@gmail.com>
2025-01-10 12:46:41 -0800

    Update 0106. Inorder and Postorder Traversal to Binary Tree.py

--- 94_1_dfs_recursive/0106. Inorder and Postorder Traversal to Binary Tree.py
+++ 94_1_dfs_recursive/0106. Inorder and Postorder Traversal to Binary Tree.py
@@ -1,8 +1,11 @@
-def buildTree(self, inorder, postorder):
+def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
     if not inorder:
-        return   
-    root = TreeNode(postorder[-1]) # 后序的最后一个为根
-    root_idx = inorder.index(postorder[-1]) # 在中序中找到根的位    
+        return None
+    
+    root_val = postorder[-1]
+    root = TreeNode(root_val) # 后序的最后一个为根
+    root_idx = inorder.index(root_val) # 在中序中找到根的位，不知道为什么这里改成root就会报错    
+    
     root.left = self.buildTree(inorder[ : root_idx], postorder[ : root_idx])
     root.right = self.buildTree(inorder[root_idx + 1 : ], postorder[root_idx : -1]) 
     return root
