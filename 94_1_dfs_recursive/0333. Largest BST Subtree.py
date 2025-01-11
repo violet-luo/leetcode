44f1632ee3a89d4a7fc60db3aeec54aa430341f3
Violet Luo <luo.violeta@gmail.com>
2025-01-11 15:23:19 -0800

    Update 0333. Largest BST Subtree.py

--- 94_1_dfs_recursive/0333. Largest BST Subtree.py
+++ 94_1_dfs_recursive/0333. Largest BST Subtree.py
@@ -1,3 +1,26 @@
+def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
+    max_cnt = 0
+
+    def traverse(node):
+        nonlocal max_cnt
+        if node is None:
+            return True, float('inf'), float('-inf'), 0
+        
+        l_bst, l_min, l_max, l_cnt = traverse(node.left)
+        r_bst, r_min, r_max, r_cnt  = traverse(node.right)
+        cnt = l_cnt + r_cnt + 1
+
+        if l_bst and r_bst and l_max < node.val < r_min:
+            max_cnt = max(max_cnt, cnt)
+            return True, min(node.val, l_min), max(node.val, r_max), cnt # 不能直接写 True, l_min, r_max 因为l_min刚开始是float('-inf')
+        else:
+            return False, 0, 0, 0
+
+    traverse(root)
+    return max_cnt
+
+###
+
 def largestBSTSubtree(self, root: TreeNode) -> int:
     if root is None:
         return 0
