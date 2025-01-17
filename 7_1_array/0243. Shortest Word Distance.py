c3ddab50d88a7798969bef65ad9dc04297f69dd6
Violet Luo <luo.violeta@gmail.com>
2025-01-16 17:21:55 -0800

    Update 0243. Shortest Word Distance.py

--- 7_1_array/0243. Shortest Word Distance.py
+++ 7_1_array/0243. Shortest Word Distance.py
@@ -1,7 +1,26 @@
+def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+    n = len(wordsDict)
+    i1, i2 = n, n
+    min_dist = len(wordsDict)
+
+    for i, word in enumerate(wordsDict):
+        if word == word1:
+            i1 = i
+            if i2 != n:
+                min_dist = min(min_dist, abs(i1 - i2))
+        if word == word2:
+            i2 = i
+            if i1 != n:
+                min_dist = min(min_dist, abs(i2 - i1))
+    
+    return min_dist
+
+###
+
 def shortestDistance(wordsDict, word1, word2):
 	length = res = len(wordsDict)
 	index1 = index2 = length # 不能init成0
-
+	
 	for i in range(length):
 		if wordsDict[i] == word1:
 			index1 = i 
@@ -9,5 +28,5 @@ def shortestDistance(wordsDict, word1, word2):
 		elif wordsDict[i] == word2:
 			index2 = i
 			res = min(res, abs(index1 - index2))
-
+	
 	return res
