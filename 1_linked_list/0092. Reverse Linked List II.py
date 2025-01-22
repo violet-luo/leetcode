7fcc79d29979c1129dfa56e11caae09a5ab093c7
Violet Luo <luo.violeta@gmail.com>
2025-01-22 13:45:10 -0800

    Update 0092. Reverse Linked List II.py

--- 1_linked_list/0092. Reverse Linked List II.py
+++ 1_linked_list/0092. Reverse Linked List II.py
@@ -26,6 +26,33 @@ def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Opt
 
 ###
 
+def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+    dummy = ListNode(0, head)
+    cur = head
+    prev = dummy
+
+    # prev走到换的left前, cur走到left
+    # prev在1，cur在2
+    for _ in range(left - 1):
+        prev = prev.next
+        cur = cur.next
+    
+    for _ in range(right - left): # range(2), swap twice
+        # First swap
+        # prev = 1, cur = 2
+        # 1 -> 3 -> 2 -> 4 -> 5
+        # Second swap 
+        # prev = 1, cur = 2
+        # 1 -> 4 -> 3 -> 2 -> 5
+        temp = cur.next # 3
+        cur.next = temp.next # 2 -> 4
+        temp.next = prev.next # 3 -> 2
+        prev.next = temp # 1 -> 3
+
+    return dummy.next
+
+###
+
 def reverseBetween(self, head, left, right):
     dummy = ListNode(0)
     dummy.next = head
