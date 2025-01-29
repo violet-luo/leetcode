562bf9ab1891a7f6f426b572decb7be5428fd299
Violet Luo <luo.violeta@gmail.com>
2025-01-28 16:06:43 -0800

    Update 0129. Sum Root to Leaf Numbers.py

--- 94_2_dfs_iterative/0129. Sum Root to Leaf Numbers.py
+++ 94_2_dfs_iterative/0129. Sum Root to Leaf Numbers.py
@@ -1,3 +1,42 @@
+def sumNumbers(self, root: Optional[TreeNode]) -> int:
+    res = 0
+    q = collections.deque([(root, 0)])
+
+    while q:
+        node, total = q.pop()
+        total = total * 10 + node.val
+        if not node.left and not node.right:
+            res += total
+        if node.left:
+            q.append((node.left, total))
+        if node.right:
+            q.append((node.right, total))
+    
+    return res
+
+###
+
+def sumNumbers(self, root: Optional[TreeNode]) -> int:
+    paths = []
+
+    q = [(root, str(root.val))]
+    while q:
+        cur, path = q.pop()
+        if not cur.left and not cur.right:
+            paths.append(path)
+        if cur.left:
+            q.append([cur.left, path + str(cur.left.val)])
+        if cur.right:
+            q.append([cur.right, path + str(cur.right.val)])
+    
+    res = 0
+    for p in paths:
+        res += int(p)
+    
+    return res
+
+###
+
 def sumNumbers(self, root):
     stack = [[root, ""]]
     result = 0
