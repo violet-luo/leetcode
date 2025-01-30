e2da7aadb9cc9dadd09e52742c82a62f56ef52c6
Violet Luo <luo.violeta@gmail.com>
2025-01-29 19:54:35 -0800

    Update 0530. Min Abs Diff in BST.py

--- 94_2_dfs_iterative/0530. Min Abs Diff in BST.py
+++ 94_2_dfs_iterative/0530. Min Abs Diff in BST.py
@@ -1,3 +1,26 @@
+def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+    min_val = float('inf')
+    stack, res = [], []
+
+    while root:
+        stack.append(root)
+        root = root.left
+    
+    while stack:
+        node = stack.pop()
+        if res: # 与中序遍历唯一不同
+            min_val = min(min_val, abs(node.val - res[-1]))
+        res.append(node.val)
+        if node.right:
+            node = node.right
+            while node:
+                stack.append(node)
+                node = node.left
+    
+    return min_val
+   
+###
+
 def getMinimumDifference(self, root):
    stack = []
    node = root
