f46b05236efbc0ca3d0edc1c019a1cf34cbed0ef
Violet Luo <luo.violeta@gmail.com>
2025-03-06 19:38:56 -0800

    Create 0014. Longest Common Prefix.py

---
+++ 7_1_array/0014. Longest Common Prefix.py
@@ -0,0 +1,10 @@
+def longestCommonPrefix(self, strs: List[str]) -> str:
+    prefix = strs[0]
+
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if prefix == "":
+                return ""
+    
+    return prefix
